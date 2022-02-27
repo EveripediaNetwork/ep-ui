c6ff00ba44d91502cc9c29f584b4d603824a2e3b
Abraham <anubra266@gmail.com>
2022-02-27 22:21:32 +0100

    Add Top bar filter

--- src/components/Profile/TopBarFilter.tsx
+++ src/components/Profile/TopBarFilter.tsx
@@ -2,7 +2,9 @@ import {
   COLLECTIONS_DISPLAY_SIZES,
   useProfileContext,
 } from '@/components/Profile/utils'
+import { ChevronDownIcon } from '@chakra-ui/icons'
 import {
+  Button,
   ButtonGroup,
   Flex,
   HTMLChakraProps,
@@ -11,6 +13,10 @@ import {
   Input,
   InputGroup,
   InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
 } from '@chakra-ui/react'
 import React from 'react'
 import { BsFillGrid3X3GapFill, BsFillGridFill } from 'react-icons/bs'
@@ -26,7 +32,7 @@ export const TopBarFilter = () => {
     },
   }
   return (
-    <Flex mt="4" gap="2">
+    <Flex mt="4" gap="2" wrap={{ base: 'wrap', md: 'nowrap' }}>
       <InputGroup>
         <InputLeftElement h="full" left="2">
           <Icon
@@ -36,8 +42,60 @@ export const TopBarFilter = () => {
             pointerEvents="none"
           />
         </InputLeftElement>
-        <Input h="12" placeholder="Search" ml="2" rounded="lg" />
+        <Input h="12" placeholder="Search" rounded="lg" />
       </InputGroup>
+
+      <Menu>
+        <MenuButton
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          variant="outline"
+          size="lg"
+          w="56"
+          minW="56"
+          flex="auto"
+          textAlign="start"
+          fontWeight="normal"
+          px="3"
+          fontSize="md"
+        >
+          All items
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Recentyl Listed</MenuItem>
+          <MenuItem>Recentyl Created</MenuItem>
+          <MenuItem>Recentyl Sold</MenuItem>
+          <MenuItem>Recentyl Received</MenuItem>
+          <MenuItem>Ending Soon</MenuItem>
+          <MenuItem>Price: Low to High</MenuItem>
+          <MenuItem>Price: High to Low</MenuItem>
+          <MenuItem>Highest Lost Sale</MenuItem>
+          <MenuItem>Most Viewed</MenuItem>
+          <MenuItem>Most Favorited</MenuItem>
+          <MenuItem>Oldest</MenuItem>
+        </MenuList>
+      </Menu>
+      <Menu>
+        <MenuButton
+          order={{ base: 5, md: 'initial' }}
+          as={Button}
+          rightIcon={<ChevronDownIcon />}
+          variant="outline"
+          size="lg"
+          w={{ base: 'full', md: '56' }}
+          minW="56"
+          textAlign="start"
+          fontWeight="normal"
+          px="3"
+          fontSize="md"
+        >
+          Recently Listed
+        </MenuButton>
+        <MenuList>
+          <MenuItem>Single items</MenuItem>
+          <MenuItem>Bundles</MenuItem>
+        </MenuList>
+      </Menu>
       <ButtonGroup size="lg" isAttached variant="outline">
         <IconButton
           aria-label="Large display"
