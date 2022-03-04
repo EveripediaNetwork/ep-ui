48d779855cde6961d7faf8f4645e46b6def1e388
Drebakare <bakaredmlr@gmail.com>
2022-03-04 13:27:23 +0100

    fixing dropzone type error

--- src/components/Layout/Editor/Highlights/Highlights.tsx
+++ src/components/Layout/Editor/Highlights/Highlights.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useMemo, useState } from 'react'
+import React, { ChangeEvent, useState } from 'react'
 import {
   Flex,
   Text,
@@ -47,13 +47,11 @@ const Highlights = () => {
     })
   }
 
-  const dropZoneActions = useMemo(()=> {
-    return {
-      setImage: handleSetImage,
-      setHideImageInput,
-      deleteImage: handleDeleteImage,
-    }
-  }, [])
+  const dropZoneActions = {
+    setImage: handleSetImage,
+    setHideImageInput,
+    deleteImage: handleDeleteImage,
+  }
 
   return (
     <Flex
