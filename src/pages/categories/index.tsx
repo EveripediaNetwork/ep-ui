da319d513882d44313c1a469f2e71b5f6d21d051
Royal-lobster <srujangs8@gmail.com>
2022-03-08 21:48:12 +0530

    added fetching data logic from graphql api in categories page

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,12 +1,44 @@
 import React from 'react'
-import { NextPage } from 'next'
+import { GetServerSideProps, NextPage } from 'next'
 import { Divider, Box, Heading, SimpleGrid } from '@chakra-ui/react'
 import { Image } from '@/components/Elements/Image/Image'
 import ToggleText from '@/components/Elements/ToggleText/ToggleText'
 import CategoryCard from '@/components/Categories/CategoryCard'
 import { sampleCategories } from '@/data/CategoriesData'
+import { CategoryDataType } from '@/types/CategoryDataTypes'
+import { request, gql } from 'graphql-request'
 
-const Categories: NextPage = () => (
+export const getServerSideProps: GetServerSideProps = async () => {
+  const query = gql`
+    {
+      categories {
+        id
+        title
+        description
+        cardImage
+        heroImage
+        icon
+      }
+    }
+  `
+
+  const res = request('http://api.dev.braindao.org/graphql', query)
+  const { categories } = await res
+
+  return {
+    props: {
+      categories,
+    },
+  }
+}
+
+interface CategoriesPageProps {
+  categories: CategoryDataType
+}
+
+const Categories: NextPage<CategoriesPageProps> = ({
+  categories,
+}: CategoriesPageProps) => (
   <Box mt="-12" bgColor="pageBg" pb={12}>
     <Image src="/images/categories-backdrop.png" height="250px" />
     <Heading fontSize={40} textAlign="center" mt={12}>
@@ -28,14 +60,14 @@ const Categories: NextPage = () => (
         my={12}
         gap={8}
       >
-        {sampleCategories.map(category => (
+        {categories.map((category, i) => (
           <CategoryCard
-            key={category.slug}
-            imageCard={category.imageCard}
-            title={category.title}
-            brief={category.description}
-            categoryUrl={category.slug}
-            coverIcon={category.icon}
+            key={category.slug || sampleCategories[i].slug}
+            imageCard={category.imageCard || sampleCategories[i].imageCard}
+            title={category.title || sampleCategories[i].title}
+            brief={category.description || sampleCategories[i].description}
+            categoryUrl={category.slug || sampleCategories[i].slug}
+            coverIcon={category.icon || sampleCategories[i].icon}
           />
         ))}
       </SimpleGrid>
