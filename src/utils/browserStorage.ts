8295e7e37aa7098ff94e3b5b5054a0a4666823cd
codiejay <jamesakpan016@gmail.com>
2022-03-16 14:36:34 +0100

    Implementing localstorage expiry

--- src/utils/browserStorage.ts
+++ src/utils/browserStorage.ts
@@ -1,25 +1,33 @@
 import type { RootState } from '@/store/store'
 
 const storageKey = 'serializedState'
+const currentDate = new Date();
+const expiryTimeline = 86400;
 
 export const loadState = () => {
   try {
     const serializedInitialState = localStorage.getItem(storageKey)
+    const setExpiry = JSON.parse(localStorage.getItem(storageKey) || "{}");
     if (!serializedInitialState) return undefined
+
+    if(currentDate.getTime() > setExpiry) { 
+      localStorage.removeItem(storageKey)
+    }
     return JSON.parse(serializedInitialState)
   } catch (e) {
     return undefined
   }
 }
 
-export function saveState(state: RootState) {
+export function saveState(state: RootState) {  
   if (typeof window !== 'undefined') {
     let updatedState = state
     if (state.providerNetwork) {
       const providerNetwork = { detectedProvider: null }
-      updatedState = { ...state, providerNetwork }
-    }
-    const serializedState = JSON.stringify(updatedState)
+      updatedState = { ...state, providerNetwork}
+    } 
+    const preSerializedState = {updatedState, expiry: currentDate.getTime()+expiryTimeline}
+    const serializedState = JSON.stringify(preSerializedState)
     localStorage.setItem(storageKey, serializedState)
   }
 }
