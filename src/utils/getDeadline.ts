918ec1cab9cdf10134074e5ee2a157f413b94a8d
JustinCast <justincastillovalladares@gmail.com>
2022-03-21 08:21:56 -0600

    fix: improvements

--- src/utils/getDeadline.ts
+++ src/utils/getDeadline.ts
@@ -1,5 +1,5 @@
 export const getDeadline = () => {
-  let deadline = new Date()
+  const deadline = new Date()
   deadline.setHours(deadline.getHours() + 1)
 
   return Math.floor(new Date().getTime() / 1000.0)
