6b632d2d160ae95ea281aee2eda958ba4927333f
Drebakare <bakaredmlr@gmail.com>
2022-03-21 19:14:21 +0100

    implements shortenText function

--- src/components/Categories/SubCategoryCard.tsx
+++ src/components/Categories/SubCategoryCard.tsx
@@ -2,23 +2,23 @@ import React from 'react'
 import Image from 'next/image'
 import { Box, Center, Text, Stack } from '@chakra-ui/react'
 import { Content } from '@/types/Wiki'
+import { shortenText } from '@/utils/shortenText'
 
 const SubCategoryCard = ({ wiki }: { wiki: Content }) => {
-  const { updated, title } = wiki
+  const { updated, content, title } = wiki
 
   return (
     <Center py={6}>
-      <Box boxShadow="2xl" rounded="md" p={6} overflow="hidden">
+      <Box minH={390} boxShadow="2xl" rounded="md" p={6} overflow="hidden">
         <Box h={200} mb={3} pos="relative">
           <Image src="/images/sub-category-image.png" layout="fill" />
         </Box>
         <Stack spacing={3}>
           <Text fontSize="2xl" fontWeight="bold">
-            {title}
+              {title}
           </Text>
           <Text color="gray.600" fontSize="md">
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-            nonumy ...
+            {shortenText(content, 65)}
           </Text>
           <Text color="gray.400" fontSize="sm">
             {updated}
