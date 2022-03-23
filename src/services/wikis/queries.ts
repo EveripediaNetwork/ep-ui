d5b30672684ee53a693710b61065de4093b83f20
Drebakare <bakaredmlr@gmail.com>
2022-03-23 01:14:42 +0100

    makes wiki link dynamic

--- src/services/wikis/queries.ts
+++ src/services/wikis/queries.ts
@@ -66,6 +66,7 @@ export const GET_USER_WIKIS_BY_ID = gql`
   query GetUserWikis($id: String!) {
     user(id: $id) {
       contents(first: 10, orderBy: createdAt, orderDirection: desc) {
+        id
         createdAt
         title
         content
