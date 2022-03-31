b11378e9aed408541b886de94cc0f53e1f9d3a52
Royal-lobster <srujangs8@gmail.com>
2022-03-31 17:37:55 +0530

    fixed type error with ipfs on wiki

--- src/types/Wiki.ts
+++ src/types/Wiki.ts
@@ -74,7 +74,7 @@ export const Languages: LanguagesType = {
 
 export interface Wiki {
   id: string
-  ipfs: string
+  ipfs?: string
   title: string
   content: string
   categories: BaseCategory[]
