2ec741888d721270daf062fec926720c6e4d4b14
Royal-lobster <srujangs8@gmail.com>
2022-04-05 20:57:42 +0530

    made heroimage to use url from getWikiImageUrl

--- src/utils/getWikiImageUrl.ts
+++ src/utils/getWikiImageUrl.ts
@@ -2,6 +2,6 @@ import config from '@/config'
 import { WikiTitle } from '@/services/nav-search'
 import { Wiki } from '@/types/Wiki'
 
-export const getWikiImageUrl = (wiki: Partial<Wiki> | WikiTitle) => {
+export const getWikiImageUrl = (wiki?: Partial<Wiki> | WikiTitle) => {
   return `${config.pinataBaseUrl}${wiki?.images ? wiki?.images[0]?.id : ''}`
 }
