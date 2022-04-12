824f2866281360142909024a5703f3e5726aa0ea
Royal-lobster <srujangs8@gmail.com>
2022-04-12 21:30:56 +0530

    added ens slice and implemented caching in useENSData hook

--- src/hooks/useENSData.ts
+++ src/hooks/useENSData.ts
@@ -2,12 +2,23 @@ import { useEffect, useState } from 'react'
 import { BaseProvider, StaticJsonRpcProvider } from '@ethersproject/providers'
 import { AvatarResolver } from '@ensdomains/ens-avatar'
 import config from '@/config'
+import { useAppDispatch, useAppSelector } from '@/store/hook'
+import { addENSAddress } from '@/store/slices/ens-slice'
 
-// TODO: future improvements: cache results
 export const useENSData = (address: string | undefined | null) => {
   const [avatar, setAvatar] = useState<string>()
   const [username, setUsername] = useState<string>()
   const [loading, setLoading] = useState<boolean>(false)
+  const ens = useAppSelector(state => state.ens)
+  const dispatch = useAppDispatch()
+
+  const setENSAddressToCache = (
+    addr: string,
+    name: string | null,
+    avt: string | null,
+  ) => {
+    dispatch(addENSAddress({ address: addr, username: name, avatar: avt }))
+  }
 
   useEffect(() => {
     const getAvatar = async (addrs: string) => {
@@ -21,13 +32,24 @@ export const useENSData = (address: string | undefined | null) => {
         })
         if (avatarURI) {
           setAvatar(avatarURI)
-        }
-      }
+          setENSAddressToCache(addrs, name, avatarURI)
+        } else setENSAddressToCache(addrs, name, null)
+      } else setENSAddressToCache(addrs, null, null)
+
       setLoading(false)
     }
 
     if (!avatar && address) {
-      getAvatar(address)
+      // first look up the ENS in the redux ens slice
+      if (ens[address]) {
+        setAvatar(ens[address].avatar)
+        setUsername(ens[address].username)
+      }
+      // if it's not there, look it up in the blockchain
+      // and save data to the redux slice
+      else {
+        getAvatar(address)
+      }
     }
   }, [address])
 
