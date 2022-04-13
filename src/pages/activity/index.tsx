1d69fcf39480c1cddd1484336498039863ca2bf2
Drebakare <bakaredmlr@gmail.com>
2022-04-13 10:44:06 +0100

    removes console.log

--- src/pages/activity/index.tsx
+++ src/pages/activity/index.tsx
@@ -27,8 +27,7 @@ const Activity = () => {
   const { data: LatestActivityData, isLoading } = useGetLatestActivitiesQuery({
     offset: 0,
   })
-  console.log(LatestActivityData)
-  console.log(LatestActivityData)
+
   const renderActivityCard = (activity: ActivityType, i: number) => (
     <ActivityCard
       id={activity.id}
--- src/utils/getWikiImageUrl.ts
+++ src/utils/getWikiImageUrl.ts
@@ -3,7 +3,5 @@ import { WikiTitle } from '@/services/nav-search'
 import { Wiki } from '@/types/Wiki'
 
 export const getWikiImageUrl = (wiki?: Partial<Wiki> | WikiTitle) => {
-  console.log(wiki)
-  console.log(wiki)
   return `${config.pinataBaseUrl}${wiki?.images ? wiki?.images[0]?.id : ''}`
 }
