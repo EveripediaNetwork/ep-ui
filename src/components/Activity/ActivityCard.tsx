5573e42146b8dfba751d4a947f9f5e24e13fcad6
Drebakare <bakaredmlr@gmail.com>
2022-04-13 11:47:09 +0100

    adds user avatar

--- src/components/Activity/ActivityCard.tsx
+++ src/components/Activity/ActivityCard.tsx
@@ -75,7 +75,7 @@ const ActivityCard = ({
       <Flex justifyContent="space-between" w="full">
         <Box>
           <HStack flex="1">
-            <DisplayAvatar size="xl" mt={1} />
+            <DisplayAvatar address={wiki.user?.id} size="20"/>
             <Text fontSize="14px" color="linkColor">
               <NextLink href={`/account/${editor}`} passHref>
                 <Link href="passRef" color="brand.500" fontWeight="bold">
