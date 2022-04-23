9fc18191bb72c255cd066f3094e81a20aa1c5e8f
Srujan Gurram <srujangs8@gmail.com>
2022-04-24 01:13:48 +0530

    modified history card with experimental design

--- src/components/Wiki/History/HistoryCard.tsx
+++ src/components/Wiki/History/HistoryCard.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
-import {
-  Alert,
-  Box,
-  Button,
-  Flex,
-  HStack,
-  Icon,
-  Link,
-  Tag,
-  Text,
-} from '@chakra-ui/react'
+import { Box, Flex, HStack, Icon, Link, Tag, Text } from '@chakra-ui/react'
 import { TriangleUpIcon } from '@chakra-ui/icons'
 import { useENSData } from '@/hooks/useENSData'
 import shortenAccount from '@/utils/shortenAccount'
 import DisplayAvatar from '@/components/Elements/Avatar/Avatar'
 import { format } from 'date-fns'
-import { RiHistoryLine } from 'react-icons/ri'
 import { shortenText } from '@/utils/shortenText'
 
 interface HistoryCardArrowProps {
@@ -71,7 +60,7 @@ interface HistoryCardProps {
   lastEditedTime?: string
   transactionAddress?: string
   commitMessage?: string
-  wordsChanged?: string
+  // wordsChanged?: string
   percentChanged?: string
   blocksChanged?: string
 }
@@ -83,7 +72,7 @@ export const HistoryCard = ({
   lastEditedTime,
   transactionAddress = '',
   commitMessage,
-  wordsChanged = '0',
+  // wordsChanged = '0',
   percentChanged = '0',
   blocksChanged = '',
 }: HistoryCardProps) => {
@@ -107,42 +96,65 @@ export const HistoryCard = ({
         isFullWidth={isFullWidth}
       />
 
-      {/* Username and Avatar of the last editor */}
-      <HStack>
-        <DisplayAvatar address={lastEditor} />
-        <Link href={`/account/${lastEditor}`} color="brand.500">
-          {username || shortenAccount(lastEditor)}
-        </Link>
-      </HStack>
+      <HStack justify="space-between">
+        {/* Username and Avatar of the last editor */}
+        <HStack>
+          <DisplayAvatar address={lastEditor} />
+          <Link href={`/account/${lastEditor}`} color="brand.500">
+            {username || shortenAccount(lastEditor)}
+          </Link>
+        </HStack>
 
-      {/* Date of the last edit */}
-      {lastEditedTime && (
-        <Text fontSize="sm" color="gray.500" mt={2}>
-          {format(new Date(lastEditedTime), 'MMMM d, yyyy')} at{' '}
-          {format(new Date(lastEditedTime), 'h:mm a')}
-        </Text>
-      )}
-
-      {/* Content change value */}
-      <HStack mt={2}>
-        <Icon as={RiHistoryLine} />
-        <Text fontSize="md" color="text.500" mt={2}>
-          {percentChanged}% content change ({wordsChanged} words)
-        </Text>
+        {/* Date of the last edit */}
+        {lastEditedTime && (
+          <Text fontSize="sm" color="gray.500" mt={2}>
+            {format(new Date(lastEditedTime), 'MMMM d, yyyy')} at{' '}
+            {format(new Date(lastEditedTime), 'h:mm a')}
+          </Text>
+        )}
       </HStack>
 
       {/* Commit message */}
       {commitMessage && (
-        <Text fontSize="sm" color="text.500" my={2}>
-          <Alert
-            colorScheme="pink"
-            borderRadius={4}
-            variant="left-accent"
-            mb={3}
+        <Box
+          pos="relative"
+          py={2}
+          px={4}
+          my={4}
+          bgColor="historyCommentBg"
+          boxShadow="0px 0px 4px rgba(0, 0, 0, 0.25);"
+          borderRadius={4}
+        >
+          <Box
+            w="70px"
+            pos="absolute"
+            top={0}
+            left={6}
+            transform="translateY(-100%)"
           >
+            <Box
+              width="30%"
+              position="relative"
+              overflow="hidden"
+              paddingBottom="21.27%"
+              _before={{
+                content: "''",
+                position: 'absolute',
+                top: '0',
+                left: '0',
+                width: '100%',
+                height: '100%',
+                boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)',
+                backgroundColor: 'historyCommentBg',
+                transformOrigin: '0 100%',
+                transform: 'rotate(45deg)',
+              }}
+            />
+          </Box>
+          <Text fontSize="sm" color="text.500" my={2}>
             {shortenText(commitMessage, 90)}
-          </Alert>
-        </Text>
+          </Text>
+        </Box>
       )}
 
       {/* What Changed tags */}
@@ -153,35 +165,38 @@ export const HistoryCard = ({
               mx="0 !important"
               variant="outline"
               boxShadow="0 0 0 1px rgba(226, 232, 240, 1)"
+              borderRadius={2}
               _dark={{ boxShadow: '0 0 0 1px rgba(255, 255, 255, 0.16)' }}
               color={{ light: 'black', _dark: 'white' }}
               size="sm"
               key={i}
             >
-              {changed}
+              {changed} {changed === 'content' && `${percentChanged}%`}
             </Tag>
           ))}
         </Flex>
       )}
 
       {/* Transaction address and restore button */}
-      <HStack mt={3} justify="space-between">
-        <HStack>
-          <Text fontSize="sm" color="text.500">
-            TX Address:
-          </Text>
-          <Link
-            href={`https://etherscan.io/tx/${transactionAddress}`}
-            color="brand.500"
-            ml={2}
-            isExternal
-          >
-            {shortenAccount(transactionAddress)}
-          </Link>
-        </HStack>
-        <Button size="xs" p={2} fontSize="sm">
-          Restore
-        </Button>
+      <HStack
+        borderTopWidth={1}
+        m={-4}
+        p={2}
+        px={4}
+        mt={3}
+        justify="space-between"
+      >
+        <Text fontSize="sm" color="text.500">
+          Transaction Address:
+        </Text>
+        <Link
+          href={`https://etherscan.io/tx/${transactionAddress}`}
+          color="brand.500"
+          ml={2}
+          isExternal
+        >
+          {shortenAccount(transactionAddress)}
+        </Link>
       </HStack>
     </Box>
   )
