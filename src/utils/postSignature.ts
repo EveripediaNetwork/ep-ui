2a94141a164f14dc5d1133df984006f6250bf4b2
codiejay <jamesakpan016@gmail.com>
2022-05-02 17:48:10 +0100

    Updating postSignature with mutation

--- src/utils/postSignature.ts
+++ src/utils/postSignature.ts
@@ -1,4 +1,5 @@
 import config from '@/config'
+import { GraphQLClient, gql } from 'graphql-request'
 import { Dict } from '@chakra-ui/utils'
 import axios from 'axios'
 
@@ -21,6 +22,57 @@ export const submitVerifiableSignature = async (
       v,
       r,
       s,
+    })
+  } catch (error) {
+    return error as Dict
+  }
+}
+
+async function main() {
+  const endpoint = 'https://api.dev.braindao.org/graphql'
+
+  const graphQLClient = new GraphQLClient(endpoint)
+
+  const mutation = gql`
+    mutation Relayer(
+      $ipfs: String!
+      $userAddr: String!
+      $deadline: Float!
+      $v: String!
+      $r: String!
+      $s: String!
+    ) {
+      relayer(
+        txToRelay: {
+          ipfs: $ipfs
+          userAddr: $userAddr
+          deadline: $deadline
+          v: $v
+          r: $r
+          s: $s
+        }
+      ) {
+        hash
+      }
+    }
+  `
+
+  const variables = {
+    ipfs: '2',
+    userAddr: '2',
+    deadline: 2.0,
+    v: '2',
+    r: '2',
+    s: '2',
+  }
+  try {
+    return await axios.post(`${config.epApiBaseUrl}relayer`, {
+      ipfs: variables.ipfs,
+      userAddr: variables.userAddr,
+      deadline: variables.deadline,
+      v: variables.v,
+      r: variables.r,
+      s: variables.s,
     })
   } catch (error) {
     return error as Dict
