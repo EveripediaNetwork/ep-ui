6e0549691b768008e138309302d637c365dc3c7d
Drebakare <bakaredmlr@gmail.com>
2022-05-03 15:41:38 +0100

    removes account support

--- src/components/About/BlogCard.tsx
+++ src/components/About/BlogCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
 import { Box, Center, Text, Stack } from '@chakra-ui/react'
-import Link from '@/components/Elements/Link/Link'
 
 const BlogCard = ({
   image,
@@ -15,28 +14,32 @@ const BlogCard = ({
   body: string
 }) => {
   return (
-    <Center onClick={()=> window.open("https://everipedia.org/blog")} cursor="pointer" py={6}>
-        <Box
-            maxW="445px"
-            w="full"
-            boxShadow="2xl"
-            rounded="md"
-            p={6}
-            overflow="hidden"
-        >
-            <Box h="210px" mt={-6} mx={-6} mb={6} pos="relative">
-                <Image src={image} layout="fill" />
-            </Box>
-            <Stack>
-                <Text color="gray.400" fontSize="sm">
-                    {date}
-                </Text>
-                <Text fontWeight={600}>{title}</Text>
-                <Text color="gray.600" fontSize="md">
-                    {body}
-                </Text>
-            </Stack>
+    <Center
+      onClick={() => window.open('https://everipedia.org/blog')}
+      cursor="pointer"
+      py={6}
+    >
+      <Box
+        maxW="445px"
+        w="full"
+        boxShadow="2xl"
+        rounded="md"
+        p={6}
+        overflow="hidden"
+      >
+        <Box h="210px" mt={-6} mx={-6} mb={6} pos="relative">
+          <Image src={image} layout="fill" />
         </Box>
+        <Stack>
+          <Text color="gray.400" fontSize="sm">
+            {date}
+          </Text>
+          <Text fontWeight={600}>{title}</Text>
+          <Text color="gray.600" fontSize="md">
+            {body}
+          </Text>
+        </Stack>
+      </Box>
     </Center>
   )
 }
