b69d4cde7d0dce9116825fa89f9b661d4a650043
Srujan Gurram <srujangs8@gmail.com>
2022-05-04 22:47:34 +0530

    added simple ui to citation plugin for adding cite links and desc

--- src/editor-plugins/cite/index.ts
+++ src/editor-plugins/cite/index.ts
@@ -1,5 +1,8 @@
 import { HTMLConvertorMap, ToMdConvertorMap } from '@toast-ui/editor'
-import { PluginCommandMap } from '@toast-ui/editor/dist/toastui-editor-viewer'
+import {
+  PluginCommandMap,
+  PluginContext,
+} from '@toast-ui/editor/dist/toastui-editor-viewer'
 import {
   NodeViewPropMap,
   PluginProp,
@@ -17,7 +20,59 @@ interface PluginInfo {
   toolbarItems?: PluginToolbarItem[]
 }
 
-export default function cite(): PluginInfo {
+export default function cite(context: PluginContext): PluginInfo {
+  const { eventEmitter } = context
+  // ========================
+  //  Toolbar Frame Elements
+  // ========================
+
+  // Frame Container
+  const container = document.createElement('div')
+  container.classList.add('cite__frame')
+
+  //  Input bar for URL
+  const urlLabel = document.createElement('label')
+  urlLabel.textContent = 'Enter URL'
+  urlLabel.htmlFor = 'citeUrlInput'
+
+  const urlInput = document.createElement('input')
+  urlInput.type = 'text'
+  urlInput.placeholder = 'Insert URL'
+  urlInput.id = 'citeUrlInput'
+
+  const urlInputContainer = document.createElement('div')
+  urlInputContainer.appendChild(urlLabel)
+  urlInputContainer.appendChild(urlInput)
+
+  // Input bar for Description
+  const descriptionLabel = document.createElement('label')
+  descriptionLabel.textContent = 'Enter Description'
+  descriptionLabel.htmlFor = 'citeDescriptionInput'
+
+  const descriptionInput = document.createElement('input')
+  descriptionInput.type = 'text'
+  descriptionInput.placeholder = 'Insert Description'
+  descriptionInput.id = 'citeDescriptionInput'
+
+  const descriptionInputContainer = document.createElement('div')
+  descriptionInputContainer.appendChild(descriptionLabel)
+  descriptionInputContainer.appendChild(descriptionInput)
+
+  // Submit button to cite the current cursor position
+  const submitButton = document.createElement('button')
+  submitButton.classList.add('toastui-editor-ok-button')
+  submitButton.textContent = 'Cite'
+
+  submitButton.addEventListener('click', () => {
+    eventEmitter.emit('command', 'cite', {
+      url: urlInput.value,
+      desc: descriptionInput.value,
+    })
+  })
+  container.appendChild(urlInputContainer)
+  container.appendChild(descriptionInputContainer)
+  container.appendChild(submitButton)
+
   return {
     toolbarItems: [
       {
@@ -27,10 +82,10 @@ export default function cite(): PluginInfo {
           name: 'cite',
           tooltip: 'Cite',
           className: 'toastui-editor-cite-button cite__popupBtn',
-          //   popup: {
-          //     body: container,
-          //     style: { width: '300px' },
-          //   },
+          popup: {
+            body: container,
+            style: { width: '300px' },
+          },
         },
       },
     ],
