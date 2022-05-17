b6e18835715e9ce7af07d5df0a365167d89a0c5e
Drebakare <bakaredmlr@gmail.com>
2022-05-17 15:00:00 +0100

    fixes Ui design

--- src/utils/getDeadline.ts
+++ src/utils/getDeadline.ts
@@ -1,6 +1,5 @@
 export const getDeadline = () => {
   const deadline = new Date()
-  deadline.setHours(deadline.getHours() + 2)
-
+  deadline.setHours(deadline.getHours() + 3)
   return Math.floor(deadline.getTime() / 1000.0)
 }
