b625c8a80c3c3fd6f25fbbec0f9dc6aa779f04fd
Drebakare <bakaredmlr@gmail.com>
2022-05-17 16:22:34 +0100

    handles transaction error on create wiki

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,10 +1,10 @@
-import { StepsStyleConfig  } from 'chakra-ui-steps'
+import { StepsStyleConfig } from 'chakra-ui-steps'
 import { ChakraTheme } from '@chakra-ui/react'
+import { StyleFunctionProps } from '@chakra-ui/theme-tools'
 import { Avatar } from './Avatar'
 import { Button } from './Button'
 import { Input } from './Input'
 import { Link } from './Link'
-import { StyleFunctionProps } from '@chakra-ui/theme-tools'
 
 const CustomSteps = {
   ...StepsStyleConfig,
@@ -15,11 +15,11 @@ const CustomSteps = {
         ...StepsStyleConfig.baseStyle(props).connector,
         // your custom styles here
         marginLeft: '75px',
-        width: "100px"
+        width: '100px',
       },
-    };
+    }
   },
-};
+}
 
 export const components: ChakraTheme['components'] = {
   Avatar,
