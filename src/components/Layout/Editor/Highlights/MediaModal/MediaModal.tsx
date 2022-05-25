c48601f554e74352a4defe461b9bd52a5544331c
Drebakare <bakaredmlr@gmail.com>
2022-05-25 14:13:35 +0100

    fixes lint error

--- src/components/Layout/Editor/Highlights/MediaModal/MediaModal.tsx
+++ src/components/Layout/Editor/Highlights/MediaModal/MediaModal.tsx
@@ -157,7 +157,9 @@ const MediaModal = ({
                 <Text fontSize="xs">Upload from computer (8mb max)</Text>
               </Button>
               <Stack spacing={4} direction="row" align="center">
-                <Button size="md"><Text fontSize="xs">Save</Text></Button>
+                <Button size="md">
+                  <Text fontSize="xs">Save</Text>
+                </Button>
                 <Button onClick={onClose} variant="outline" size="md">
                   <Text fontSize="xs">Cancel</Text>
                 </Button>
