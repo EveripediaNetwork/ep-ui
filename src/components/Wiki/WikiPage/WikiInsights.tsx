4d90246087867b1b692aff8284ba7dcadb3ab9fb
anubra266 <anubra266@gmail.com>
2022-05-26 00:03:12 +0100

    feat for some widgets

--- src/components/Wiki/WikiPage/WikiInsights.tsx
+++ src/components/Wiki/WikiPage/WikiInsights.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { VStack } from '@chakra-ui/react'
-import { Wiki } from '@/types/Wiki'
+import { CommonMetaIds, Wiki } from '@/types/Wiki'
 import { getWikiImageUrl } from '@/utils/getWikiImageUrl'
 import { WikiDetails } from './InsightComponents/WikiDetails'
 import { RelatedWikis } from './InsightComponents/RelatedWikis'
@@ -15,36 +15,47 @@ interface WikiInsightsProps {
   ipfs?: string
 }
 
-const WikiInsights = ({ wiki, ipfs }: WikiInsightsProps) => (
-  <VStack
-    maxW="500px"
-    borderLeftWidth={{ base: 0, md: '1px' }}
-    w={{ base: '100%', md: '50%' }}
-    mx={{ base: 'auto', md: 0 }}
-    p={4}
-    spacing={4}
-    pt={24}
-  >
-    <WikiDetails
-      wikiTitle={wiki}
-      categories={wiki.categories}
-      lastEdited={wiki.updated || wiki?.created}
-      ipfsHash={ipfs || wiki.ipfs}
-      txHash={wiki.transactionHash}
-      lastEditor={wiki.user?.id}
-      imgSrc={getWikiImageUrl(wiki)}
-    />
-    <ProfileSummary />
-    <ProfileStatistics />
-    <CurrencyConverter token="everipedia" tokenSymbol="IQ" />
-    {wiki.metadata[1]?.value && (
-      <TwitterTimeline url={wiki.metadata[1].value} />
-    )}
-    {wiki.categories.length !== 0 && (
-      <RelatedWikis categories={wiki.categories} />
-    )}
-    <RelatedMediaGrid />
-  </VStack>
-)
+const WikiInsights = ({ wiki, ipfs }: WikiInsightsProps) => {
+  const coingeckoLink = wiki.metadata.find(
+    meta => meta.id === CommonMetaIds.COINGECKO_PROFILE,
+  )?.value
+
+  return (
+    <VStack
+      maxW="500px"
+      borderLeftWidth={{ base: 0, md: '1px' }}
+      w={{ base: '100%', md: '50%' }}
+      mx={{ base: 'auto', md: 0 }}
+      p={4}
+      spacing={4}
+      pt={24}
+    >
+      <WikiDetails
+        wikiTitle={wiki}
+        categories={wiki.categories}
+        lastEdited={wiki.updated || wiki?.created}
+        ipfsHash={ipfs || wiki.ipfs}
+        txHash={wiki.transactionHash}
+        lastEditor={wiki.user?.id}
+        imgSrc={getWikiImageUrl(wiki)}
+      />
+
+      {!!coingeckoLink && (
+        <>
+          <ProfileSummary wiki={wiki} />
+          <ProfileStatistics />
+          <CurrencyConverter token="everipedia" tokenSymbol="IQ" />
+        </>
+      )}
+      {wiki.metadata[1]?.value && (
+        <TwitterTimeline url={wiki.metadata[1].value} />
+      )}
+      {wiki.categories.length !== 0 && (
+        <RelatedWikis categories={wiki.categories} />
+      )}
+      <RelatedMediaGrid />
+    </VStack>
+  )
+}
 
 export default WikiInsights
