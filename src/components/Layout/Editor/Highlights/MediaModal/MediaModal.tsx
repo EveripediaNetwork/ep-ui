169d44554719e89642c52ba828551b29f27c072f
Drebakare <bakaredmlr@gmail.com>
2022-05-25 14:08:24 +0100

    designs media screen

--- src/components/Layout/Editor/Highlights/MediaModal/MediaModal.tsx
+++ src/components/Layout/Editor/Highlights/MediaModal/MediaModal.tsx
@@ -12,7 +12,16 @@ import {
   VStack,
   Box,
   Img,
+  SimpleGrid,
+  Flex,
+  Progress,
+  Stack
 } from '@chakra-ui/react'
+import {
+  RiCloseLine,
+  RiFilmLine,
+  RiImageLine,
+} from 'react-icons/ri'
 
 const MediaModal = ({
   onClose = () => {},
@@ -42,11 +51,105 @@ const MediaModal = ({
         <ModalBody>
           <Box mt="3">
             <Text fontWeight="bold">Local Files</Text>
-            <VStack align="center" mb={8} py={5} gap={5}>
+            <Box my={4}>
+              <SimpleGrid columns={2} spacing={6}>
+                <Flex gap={4} color="linkColor">
+                  <Box mt={2}>
+                    <RiImageLine size="50" />
+                  </Box>
+                  <VStack>
+                      <Flex w="full" gap={16}>
+                          <Text fontSize="sm">brainies.png</Text>
+                          <Box mt={1}>
+                            <RiCloseLine color="red" size="14"/>
+                          </Box>
+                      </Flex>
+                      <Box w="full">
+                         <Progress  value={80}  h="5px" colorScheme="green" size='sm' />
+                      </Box>
+                      <Flex w="full" fontSize="xs" gap={16}>
+                          <Text  flex="1">3mb</Text>
+                          <Text flex="1">Uploading</Text>
+                      </Flex>
+                  </VStack>
+                </Flex>
+                <Flex gap={4} color="linkColor">
+                  <Box mt={2}>
+                    <RiFilmLine size="50" />
+                  </Box>
+                  <VStack>
+                      <Flex w="full" gap={16}>
+                          <Text fontSize="sm">brainies.png</Text>
+                          <Box mt={1}>
+                            <RiCloseLine color="red" size="14"/>
+                          </Box>
+                      </Flex>
+                      <Box w="full">
+                         <Progress  value={80} h="5px"  colorScheme="green" size='sm' />
+                      </Box>
+                      <Flex w="full" fontSize="xs" gap={16}>
+                          <Text  flex="1">3mb</Text>
+                          <Text flex="1">Uploading</Text>
+                      </Flex>
+                  </VStack>
+                </Flex>
+                <Flex gap={4} color="linkColor">
+                  <Box mt={2}>
+                    <RiImageLine size="50" />
+                  </Box>
+                  <VStack>
+                      <Flex w="full" gap={16}>
+                          <Text fontSize="sm">brainies.png</Text>
+                          <Box mt={1}>
+                            <RiCloseLine color="red" size="14"/>
+                          </Box>
+                      </Flex>
+                      <Box w="full">
+                         <Progress  value={80} h="5px" colorScheme="green" size='sm' />
+                      </Box>
+                      <Flex w="full" fontSize="xs" gap={16}>
+                          <Text  flex="1">3mb</Text>
+                          <Text flex="1">Uploading</Text>
+                      </Flex>
+                  </VStack>
+                </Flex>
+                <Flex gap={4} color="linkColor">
+                  <Box mt={2}>
+                    <RiFilmLine size="50" />
+                  </Box>
+                  <VStack>
+                      <Flex w="full" gap={16}>
+                          <Text fontSize="sm">brainies.png</Text>
+                          <Box mt={1}>
+                            <RiCloseLine color="red" size="14"/>
+                          </Box>
+                      </Flex>
+                      <Box w="full">
+                         <Progress  value={80} h="5px" colorScheme="green" size='sm' />
+                      </Box>
+                      <Flex w="full" fontSize="xs" gap={16}>
+                          <Text  flex="1">3mb</Text>
+                          <Text flex="1">Uploading</Text>
+                      </Flex>
+                  </VStack>
+                </Flex>
+
+                
+              </SimpleGrid>
+            </Box>
+            <VStack align="center" mb={8} py={5} gap={10}>
               <Img src="/images/file-image.png" h={150} w={250} />
-              <Button colorScheme="blue" mx="auto">
+              <Button mx="auto">
                 <Text fontSize="xs">Upload from computer (8mb max)</Text>
               </Button>
+              <Stack spacing={4} direction='row' align='center'>
+                <Button size='md'>
+                  Save
+                </Button>
+                <Button onClick={onClose} variant="outline" size='md'>
+                  Cancel
+                </Button>
+              </Stack>
             </VStack>
           </Box>
         </ModalBody>
