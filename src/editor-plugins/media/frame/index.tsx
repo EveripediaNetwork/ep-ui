a7fc8109b1c535b58ec9fd085aee74ef0d0867bd
Srujan Gurram <srujangs8@gmail.com>
2022-05-26 20:46:03 +0530

    made plugin to use the state for data and added state where there are no media files

--- src/editor-plugins/media/frame/index.tsx
+++ src/editor-plugins/media/frame/index.tsx
@@ -1,12 +1,13 @@
 import { WikiImage } from '@/components/WikiImage'
 import config from '@/config'
+import { store } from '@/store/store'
 import { Media } from '@/types/Wiki'
-import { Box, SimpleGrid } from '@chakra-ui/react'
+import { Box, Center, SimpleGrid, Text } from '@chakra-ui/react'
 import { PluginContext } from '@toast-ui/editor'
-import React from 'react'
-import { media } from '../dummyData'
+import React, { useEffect } from 'react'
 
 const MediaFrame = ({ editorContext }: { editorContext: PluginContext }) => {
+  const [media, setMedia] = React.useState<Media[]>()
   function handleImageClick(m: Media): void {
     editorContext.eventEmitter.emit('command', 'insertImage', {
       src: `${config.pinataBaseUrl}${m.ipfs}`,
@@ -14,26 +15,44 @@ const MediaFrame = ({ editorContext }: { editorContext: PluginContext }) => {
     })
   }
 
+  const fetchReferences = () => {
+    const fetchedMedia = store.getState().wiki.media
+
+    setMedia(fetchedMedia)
+  }
+
+  useEffect(() => {
+    fetchReferences()
+  }, [])
+
   return (
     <Box>
-      <SimpleGrid columns={3} gap={4}>
-        {media.map(m => {
-          if (m.type === 'IMAGE')
-            return (
-              <WikiImage
-                w="100%"
-                h="100px"
-                borderRadius="3px"
-                overflow="hidden"
-                key={m.id}
-                onClick={() => handleImageClick(m)}
-                cursor="pointer"
-                imageURL={`${config.pinataBaseUrl}${m.ipfs}`}
-              />
-            )
-          return null
-        })}
-      </SimpleGrid>
+      {media && media.length ? (
+        <SimpleGrid columns={3} gap={4}>
+          {media.map(m => {
+            if (m.type === 'IMAGE')
+              return (
+                <WikiImage
+                  w="100%"
+                  h="100px"
+                  borderRadius="3px"
+                  overflow="hidden"
+                  key={m.id}
+                  onClick={() => handleImageClick(m)}
+                  cursor="pointer"
+                  imageURL={`${config.pinataBaseUrl}${m.ipfs}`}
+                />
+              )
+            return null
+          })}
+        </SimpleGrid>
+      ) : (
+        <Center h="100px" w="100% !important">
+          <Text textAlign="center" color="#758093" maxW="250px">
+            Your Media files will be displayed here as you add them.
+          </Text>
+        </Center>
+      )}
     </Box>
   )
 }
