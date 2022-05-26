c07c2d922df9d7bbaa897a9cae1431de7936312b
Srujan Gurram <srujangs8@gmail.com>
2022-05-26 19:09:26 +0530

    added basic image insertion in media plugin

--- src/editor-plugins/media/frame/index.tsx
+++ src/editor-plugins/media/frame/index.tsx
@@ -1,7 +1,41 @@
+import { WikiImage } from '@/components/WikiImage'
+import config from '@/config'
+import { Media } from '@/types/Wiki'
+import { Box, SimpleGrid } from '@chakra-ui/react'
+import { PluginContext } from '@toast-ui/editor'
 import React from 'react'
+import { media } from '../dummyData'
 
-const MediaFrame = () => {
-  return <div>TODO</div>
+const MediaFrame = ({ editorContext }: { editorContext: PluginContext }) => {
+  function handleImageClick(m: Media): void {
+    editorContext.eventEmitter.emit('command', 'insertImage', {
+      src: `${config.pinataBaseUrl}${m.ipfs}`,
+      alt: m.name,
+    })
+  }
+
+  return (
+    <Box>
+      <SimpleGrid columns={3} gap={4}>
+        {media.map(m => {
+          if (m.type === 'IMAGE')
+            return (
+              <WikiImage
+                w="100%"
+                h="100px"
+                borderRadius="3px"
+                overflow="hidden"
+                key={m.id}
+                onClick={() => handleImageClick(m)}
+                cursor="pointer"
+                imageURL={`${config.pinataBaseUrl}${m.ipfs}`}
+              />
+            )
+          return null
+        })}
+      </SimpleGrid>
+    </Box>
+  )
 }
 
 export default MediaFrame
