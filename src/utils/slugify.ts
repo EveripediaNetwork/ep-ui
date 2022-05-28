17ed13307b4d8b809b5edcc63e9bff1e91576baf
codiejay <jamesakpan016@gmail.com>
2022-05-28 20:13:11 +0100

    Code review: Updating var name to avoid confusion

--- src/utils/slugify.ts
+++ src/utils/slugify.ts
@@ -1,7 +1,7 @@
 import slugify from 'slugify'
 
-export const slugifyText = (string: string) => {
-  return slugify(string, {
+export const slugifyText = (text: string) => {
+  return slugify(text, {
     strict: true,
     lower: true,
     remove: /[*+~.()'"!:@]/g,
