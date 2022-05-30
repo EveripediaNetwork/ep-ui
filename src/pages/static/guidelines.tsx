41edaaff2c36969004c4f09049101a8515fadd2d
Drebakare <bakaredmlr@gmail.com>
2022-05-30 10:23:33 +0100

    reworks margin on policy page

--- src/pages/static/guidelines.tsx
+++ src/pages/static/guidelines.tsx
@@ -5,12 +5,13 @@ import {
   Flex,
   UnorderedList,
   ListItem,
+  Container
 } from '@chakra-ui/react'
 import React from 'react'
 import RelatedTopics from '@/components/Elements/RelatedTopics/RelatedTopics'
 
 const Privacy = () => (
-  <Box w="min(90%, 1200px)" mx="auto" my={{ base: '10', lg: '16' }}>
+  <Container w="min(90%, 1200px)" maxW={{ base: '7xl', xl: '6xl', '2xl': '80%' }}   my={{ base: '10', lg: '16' }}>
     <Heading my={8} as="h1" size="3xl" letterSpacing="wide">
       Guidelines
     </Heading>
@@ -175,7 +176,7 @@ const Privacy = () => (
         />
       </Box>
     </Flex>
-  </Box>
+  </Container>
 )
 
 export default Privacy
