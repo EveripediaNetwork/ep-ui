c8ede545c30df9da323ec09c4702dbea703f34f2
Drebakare <bakaredmlr@gmail.com>
2022-06-02 01:55:17 +0100

    fixes lint error

--- src/utils/getFormattedDate.ts
+++ src/utils/getFormattedDate.ts
@@ -1,7 +1,7 @@
 import { formatDistanceToNowStrict } from 'date-fns'
 
 export const getReadableDate = (dateToFormat: string) => {
-  return formatDistanceToNowStrict(new Date(dateToFormat),  {
+  return formatDistanceToNowStrict(new Date(dateToFormat), {
     addSuffix: false,
   })
 }
