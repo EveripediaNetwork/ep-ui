8855579cc67149036ddd2597851d8405d47a15b7
OleanjiKingCode <holamilean56@gmail.com>
2022-06-08 14:20:32 +0100

    Fixed the duplicate wiki in profile problem

--- src/components/Profile/Collected.tsx
+++ src/components/Profile/Collected.tsx
@@ -55,7 +55,7 @@ const Collected = () => {
     if (address) {
       fetchMoreWikis()
     }
-  }, [address, fetchMoreWikis])
+  }, [address])
 
   const [sentryRef] = useInfiniteScroll({
     loading,
