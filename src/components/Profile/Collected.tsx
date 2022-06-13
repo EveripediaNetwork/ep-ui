e4e2aef4977b16dfb8795ff56ed53a3996fe8f8d
anubra266 <anubra266@gmail.com>
2022-06-14 00:56:32 +0100

    ref infinite scroll

--- src/components/Profile/Collected.tsx
+++ src/components/Profile/Collected.tsx
@@ -2,56 +2,31 @@ import useInfiniteScroll from 'react-infinite-scroll-hook'
 import { FilterLayout } from '@/components/Profile/FilterLayout'
 import { getUserWikis } from '@/services/wikis'
 import { Center, SimpleGrid, Text, Spinner } from '@chakra-ui/react'
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { EmptyState } from '@/components/Profile/EmptyState'
 import { Wiki } from '@/types/Wiki'
-import { FETCH_DELAY_TIME, ITEM_PER_PAGE } from '@/data/Constants'
-import { store } from '@/store/store'
 import { useTranslation } from 'react-i18next'
+import { useInfiniteData } from '@/utils/useInfiniteData'
 import WikiPreviewCard from '../Wiki/WikiPreviewCard/WikiPreviewCard'
 
 const Collected = () => {
   const router = useRouter()
   const address = router.query.profile as string
-  const [hasMore, setHasMore] = useState<boolean>(true)
-  const [wikis, setWikis] = useState<Wiki[] | []>([])
-  const [offset, setOffset] = useState<number>(0)
-  const [loading, setLoading] = useState<boolean>(true)
-  const fetchMoreWikis = React.useCallback(
-    (fetchOffset = 0) => {
-      setTimeout(() => {
-        const fetchNewWikis = async () => {
-          const result = await store.dispatch(
-            getUserWikis.initiate({
-              id: address,
-              limit: ITEM_PER_PAGE,
-              offset: fetchOffset,
-            }),
-          )
-          if (result.data && result.data?.length > 0) {
-            const data = result.data || []
-            const updatedWiki = [...wikis, ...data]
-            setWikis(updatedWiki)
-            setOffset(fetchOffset)
-            if (data.length < ITEM_PER_PAGE) {
-              setLoading(false)
-              setHasMore(false)
-            }
-          } else {
-            setHasMore(false)
-            setLoading(false)
-          }
-        }
-        fetchNewWikis()
-      }, FETCH_DELAY_TIME)
-    },
-    [address, wikis],
-  )
+
+  const {
+    data: wikis,
+    fetcher: fetchMoreWikis,
+    loading,
+    hasMore,
+  } = useInfiniteData<Wiki>({
+    initiator: getUserWikis,
+    arg: { address },
+  })
 
   useEffect(() => {
     if (address) {
-      fetchMoreWikis()
+      fetchMoreWikis(true)
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address])
@@ -59,7 +34,7 @@ const Collected = () => {
   const [sentryRef] = useInfiniteScroll({
     loading,
     hasNextPage: hasMore,
-    onLoadMore: () => fetchMoreWikis(offset + ITEM_PER_PAGE),
+    onLoadMore: () => fetchMoreWikis(),
   })
   const { t } = useTranslation()
   return (
