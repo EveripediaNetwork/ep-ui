8f2553b61e3e3eed71606fb9b550e5eed4d1ac93
Srujan Gurram <srujangs8@gmail.com>
2022-07-01 20:36:11 +0530

    added createProfile mutation to rtk query

--- src/services/settings/index.tsx
+++ src/services/settings/index.tsx
@@ -1,6 +1,9 @@
 import { createApi } from '@reduxjs/toolkit/query/react'
 import { graphqlRequestBaseQuery } from '@rtk-query/graphql-request-base-query'
-import { GET_USER_PROFILE } from '@/services/settings/queries'
+import {
+  GET_USER_PROFILE,
+  POST_USER_PROFILE,
+} from '@/services/settings/queries'
 import config from '@/config'
 import { ProfileDataType } from '@/types/SettingsType'
 import { GraphQLClient } from 'graphql-request'
@@ -10,6 +13,13 @@ export const settingsApiClient = new GraphQLClient(config.graphqlUrl)
 type UserProfileData = {
   getProfile: ProfileDataType
 }
+
+type SetProfileResponse = {
+  createProfile: {
+    UserProfile: ProfileDataType
+  }
+}
+
 export const settingsApi = createApi({
   reducerPath: 'settingsApi',
   baseQuery: graphqlRequestBaseQuery({ client: settingsApiClient }),
@@ -24,12 +34,26 @@ export const settingsApi = createApi({
       }),
       transformResponse: (response: UserProfileData) => response.getProfile,
     }),
+    postUserProfile: builder.mutation<
+      ProfileDataType,
+      { profileInfo: Partial<ProfileDataType> }
+    >({
+      query: ({ profileInfo }) => ({
+        document: POST_USER_PROFILE,
+        variables: {
+          profileInfo: JSON.stringify(profileInfo),
+        },
+      }),
+      transformResponse: (response: SetProfileResponse) =>
+        response.createProfile.UserProfile,
+    }),
   }),
 })
 
 export const {
   useGetUserProfileQuery,
+  usePostUserProfileMutation,
   util: { getRunningOperationPromises },
 } = settingsApi
 
-export const { getUserProfile } = settingsApi.endpoints
+export const { getUserProfile, postUserProfile } = settingsApi.endpoints
--- src/services/settings/queries.ts
+++ src/services/settings/queries.ts
@@ -24,3 +24,27 @@ export const GET_USER_PROFILE = gql`
     }
   }
 `
+export const POST_USER_PROFILE = gql`
+  query getUserProfile($id: String!) {
+    getProfile(id: $id) {
+      id
+      username
+      bio
+      email
+      banner
+      links {
+        twitter
+        website
+        instagram
+      }
+      notifications {
+        EVERIPEDIA_NOTIFICATIONS
+        WIKI_OF_THE_DAY
+        WIKI_OF_THE_MONTH
+      }
+      advancedSettings {
+        SIGN_EDITS_WITH_RELAYER
+      }
+    }
+  }
+`
