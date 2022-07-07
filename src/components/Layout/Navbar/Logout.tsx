4d4a0a92d8f09927cf96e0df56c15fb9badf71ad
Mario <mario.everipedia@gmail.com>
2022-07-07 14:53:51 +0100

    Reload the page once the user is signed out.

--- src/components/Layout/Navbar/Logout.tsx
+++ src/components/Layout/Navbar/Logout.tsx
@@ -4,15 +4,28 @@ import React from 'react'
 import { RiLogoutBoxRFill } from 'react-icons/ri'
 import { useDispatch } from 'react-redux'
 import { useDisconnect, useAccount } from 'wagmi'
+import { useRouter } from 'next/router'
 
 export const LogOutBttn = ({ isInMobileMenu }: { isInMobileMenu: boolean }) => {
   const userAccount = useAccount()
   const dispatch = useDispatch()
   const { disconnect } = useDisconnect()
-  const handleLogOut = () => {
+  const router = useRouter()
+
+  const userIsSignedIn: boolean = userAccount?.data?.address !== undefined
+
+  const signOut = () => {
     disconnect()
     dispatch(setStateToDefault())
+    router.reload()
   }
+
+  const onClick = () => {
+    if (userIsSignedIn) {
+      signOut()
+    }
+  }
+
   return (
     <>
       {!isInMobileMenu && <Divider />}
@@ -22,7 +35,7 @@ export const LogOutBttn = ({ isInMobileMenu }: { isInMobileMenu: boolean }) => {
           px={isInMobileMenu ? 0 : 3}
           bgColor={!isInMobileMenu ? 'subMenuBg' : 'transparent'}
           sx={{ '&:hover, &:focus, &:active': { bgColor: 'transparent' } }}
-          onClick={userAccount?.data && handleLogOut}
+          onClick={onClick}
           cursor={userAccount?.data ? 'pointer' : 'not-allowed'}
           display={userAccount?.data ? 'flex' : 'none'}
         >
