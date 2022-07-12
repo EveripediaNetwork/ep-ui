c11428f74c2fee854dace3c26c5ee7e8bbda6705
codiejay <jamesakpan016@gmail.com>
2022-07-12 23:20:42 +0100

    Updating with code review

--- src/components/Layout/Editor/Highlights/HighlightsModal/HighlightsModal.tsx
+++ src/components/Layout/Editor/Highlights/HighlightsModal/HighlightsModal.tsx
@@ -34,7 +34,6 @@ import InstagramIcon from '@/components/Icons/instagramIcon'
 import FacebookIcon from '@/components/Icons/facebookIcon'
 import GithubIcon from '@/components/Icons/githubIcon'
 import TelegramIcon from '@/components/Icons/telegramIcon'
-import EmailIcon from '@/components/Icons/emailIcon'
 
 import { MdEmail } from 'react-icons/md'
 import { CommonMetaIds, MData } from '@/types/Wiki'
