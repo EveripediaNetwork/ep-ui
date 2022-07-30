c9eab647829441395c9a6b5d5925ab55262eca11
Srujan Gurram <srujangs8@gmail.com>
2022-07-30 20:42:56 +0530

    removed console logs

--- src/utils/dataURLtoArrayBuffer.ts
+++ src/utils/dataURLtoArrayBuffer.ts
@@ -1,8 +1,6 @@
 export const dataURLtoArrayBuffer = async (imageUrl: string) => {
-  console.log('dataURLtoArrayBuffer', imageUrl)
   const response = fetch(imageUrl)
   const blob = await (await response).blob()
   const arrayBuffer = await blob.arrayBuffer()
-  console.log('dataURLtoArrayBuffer', arrayBuffer)
   return arrayBuffer
 }
