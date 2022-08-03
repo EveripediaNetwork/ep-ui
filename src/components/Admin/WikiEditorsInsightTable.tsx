93b458108d59f74af0c262200e8198dbd579d3ec
James Akpan <jamesakpan016@gmail.com>
2022-08-03 22:43:31 +0100

    adding pagination to wikieditor admin

--- src/components/Admin/WikiEditorsInsightTable.tsx
+++ src/components/Admin/WikiEditorsInsightTable.tsx
@@ -1,4 +1,15 @@
 import {
+  Pagination,
+  PaginationContainer,
+  PaginationPrevious,
+  PaginationPageGroup,
+  PaginationSeparator,
+  PaginationPage,
+  PaginationNext,
+  usePagination,
+} from '@ajna/pagination'
+import { ArrowBackIcon, ArrowForwardIcon } from '@chakra-ui/icons'
+import {
   Text,
   Flex,
   Tag,
@@ -7,6 +18,7 @@ import {
   InputGroup,
   InputLeftElement,
   Select,
+  Button,
 } from '@chakra-ui/react'
 import React from 'react'
 import { FiSearch } from 'react-icons/fi'
@@ -78,7 +90,281 @@ export const WikiEditorsInsightTable = () => {
       },
       latestActivity: 'Apr 11, 2022 - 19:22am',
     },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Mike Drawn',
+      editorAvatar:
+        'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZkRriG5pzevdGvLMh6EVUKkaobTUykw7AhGtTswW35DM&w=3840&q=95',
+      editorAddress: 'FxSegd...2358',
+      createdWikis: 93,
+      editiedWikis: 122,
+      lastCreatedWiki: {
+        title: 'From Mars',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZxbkyAGqJt6yUo74QsoKeP6WLWRTn7921Qwxwngxv977&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Mike Drawn',
+      editorAvatar:
+        'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZkRriG5pzevdGvLMh6EVUKkaobTUykw7AhGtTswW35DM&w=3840&q=95',
+      editorAddress: 'FxSegd...2358',
+      createdWikis: 93,
+      editiedWikis: 122,
+      lastCreatedWiki: {
+        title: 'From Mars',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZxbkyAGqJt6yUo74QsoKeP6WLWRTn7921Qwxwngxv977&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Mike Drawn',
+      editorAvatar:
+        'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZkRriG5pzevdGvLMh6EVUKkaobTUykw7AhGtTswW35DM&w=3840&q=95',
+      editorAddress: 'FxSegd...2358',
+      createdWikis: 93,
+      editiedWikis: 122,
+      lastCreatedWiki: {
+        title: 'From Mars',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZxbkyAGqJt6yUo74QsoKeP6WLWRTn7921Qwxwngxv977&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Mike Drawn',
+      editorAvatar:
+        'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZkRriG5pzevdGvLMh6EVUKkaobTUykw7AhGtTswW35DM&w=3840&q=95',
+      editorAddress: 'FxSegd...2358',
+      createdWikis: 93,
+      editiedWikis: 122,
+      lastCreatedWiki: {
+        title: 'From Mars',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmZxbkyAGqJt6yUo74QsoKeP6WLWRTn7921Qwxwngxv977&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
+    {
+      editorName: 'Joe Dohn',
+      editorAvatar: 'https://bit.ly/dan-abramov',
+      editorAddress: '0xFe9d...2358',
+      createdWikis: 3,
+      editiedWikis: 12,
+      lastCreatedWiki: {
+        title: 'Sushiswap',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmNqqvGLEyTanEHowse84GtXoqVmZDBDKdA846u3RRaGck&w=3840&q=95',
+      },
+      latestActivity: 'Jan 6, 2022 - 12:22am',
+    },
+    {
+      editorName: 'Lucy Xander',
+      editorAvatar:
+        'https://www.the.com/__PUBLIC_LIGHT/assets/RpJA1-grace-headshot.jpg',
+      editorAddress: '0xSegd...2358',
+      createdWikis: 12,
+      editiedWikis: 8,
+      lastCreatedWiki: {
+        title: 'Flying Jeans',
+        img: 'https://alpha.everipedia.org/_next/image?url=https%3A%2F%2Fipfs.everipedia.org%2Fipfs%2FQmaow6d3nriW4XoaVqTVWsJMr5bME4JFrNsGXDuijMYocZ&w=3840&q=95',
+      },
+      latestActivity: 'Apr 11, 2022 - 19:22am',
+    },
   ]
+  const {
+    pages,
+    // pagesCount,
+    // offset,
+    // currentPage,
+    // setCurrentPage,
+    // setIsDisabled,
+    // isDisabled,
+    // pageSize,
+    // setPageSize,
+  } = usePagination({
+    total: wikiInsightData.length,
+    limits: {
+      outer: 3,
+      inner: 3,
+    },
+    initialState: {
+      pageSize: 5,
+      isDisabled: false,
+      currentPage: 1,
+    },
+  })
   return (
     <Flex
       flexDir="column"
@@ -117,19 +403,107 @@ export const WikiEditorsInsightTable = () => {
           </InputGroup>
           <Select
             cursor="pointer"
-            w="30%"
+            w="40%"
             placeholder="Filter"
             icon={<MdFilterList />}
           >
-            <option value="option1">Weekly (2020)</option>
-            <option value="option1">Monthly (2020)</option>
-            <option value="option1">Yearly (2020)</option>
+            <option value="option1">Weekly</option>
+            <option value="option1">Monthly</option>
+            <option value="option1">Yearly</option>
           </Select>
         </Flex>
       </Flex>
       <Flex pb={5}>
         <InsightTableWikiEditors wikiInsightData={wikiInsightData} />
       </Flex>
+      <Pagination
+        pagesCount={10}
+        currentPage={1}
+        isDisabled={false}
+        onPageChange={() => {
+          console.log('hey')
+        }}
+      >
+        <PaginationContainer
+          align="center"
+          justify="space-between"
+          p={4}
+          w="full"
+        >
+          <PaginationPrevious
+            bg="white"
+            onClick={() => null}
+            _hover={{ bg: 'none' }}
+            _active={{ bg: 'none' }}
+          >
+            <Button
+              leftIcon={<ArrowBackIcon />}
+              color="black"
+              variant="outline"
+            >
+              Previous
+            </Button>
+          </PaginationPrevious>
+          <PaginationPageGroup
+            isInline
+            align="center"
+            separator={
+              <PaginationSeparator
+                onClick={() =>
+                  console.log(
+                    'Im executing my own function along with Separator component functionality',
+                  )
+                }
+                bg="blue.300"
+                fontSize="sm"
+                w={7}
+                jumpSize={11}
+              />
+            }
+          >
+            {pages.map((page: number) => (
+              <PaginationPage
+                w={7}
+                key={`pagination_page_${page}`}
+                page={page}
+                p={5}
+                onClick={() =>
+                  console.log(
+                    'Im executing my own function along with Page component functionality',
+                  )
+                }
+                fontSize="sm"
+                _hover={{
+                  bg: '#FFE5F1',
+                  color: 'brand.500',
+                }}
+                _current={{
+                  bg: '#FFE5F1',
+                  fontSize: 'sm',
+                  color: 'brand.500',
+                  w: 7,
+                }}
+                bg="transparent"
+                color="#718096"
+              />
+            ))}
+          </PaginationPageGroup>
+          <PaginationNext
+            bg="white"
+            onClick={() => null}
+            _hover={{ bg: 'none' }}
+            _active={{ bg: 'none' }}
+          >
+            <Button
+              rightIcon={<ArrowForwardIcon />}
+              color="black"
+              variant="outline"
+            >
+              Next
+            </Button>
+          </PaginationNext>
+        </PaginationContainer>
+      </Pagination>
     </Flex>
   )
 }
