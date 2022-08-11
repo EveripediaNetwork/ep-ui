a7e83c3955f90fb8e20b06900fcbaff8ef51c763
Adebayo Olamilekan <75235148+OleanjiKingCode@users.noreply.github.com>
2022-08-11 09:35:24 +0100

    Search By Users (#539)
    * usernames now searchable
    
    * usernames now searchable
    
    * usernames now searchable
    
    * usernames now searchable
    
    * Update index.tsx
    
    * usernames and addresses now searchable
    
    * usernames and addresses now searchable
    
    * usernames and addresses now searchable

--- src/services/search/queries.ts
+++ src/services/search/queries.ts
@@ -36,3 +36,14 @@ export const GET_CATEGORIES_BY_TITLE = gql`
     }
   }
 `
+
+export const GET_USERNAME_BY_TITLE = gql`
+  query getProfileLikeUsername($id: String, $username: String) {
+    getProfileLikeUsername(id: $id, username: $username) {
+      id
+      username
+      bio
+      avatar
+    }
+  }
+`
