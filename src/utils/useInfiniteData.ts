7a98fd9aa8ced5004cc433d877502c55f0ebf230
Srujan Gurram <srujangs8@gmail.com>
2022-08-17 19:21:03 +0530

    removed comments

--- src/utils/useInfiniteData.ts
+++ src/utils/useInfiniteData.ts
@@ -42,33 +42,6 @@ export const useInfiniteData = <T>(opts: Opts) => {
     setTimeout(fetchNewWikis, FETCH_DELAY_TIME)
   }
 
-  // const fetcher = (noOffset?: boolean) => {
-  //   setTimeout(() => {
-  //     const fetchNewWikis = async () => {
-  //       const result = await store.dispatch(
-  //         opts.initiator.initiate({
-  //           ...opts.arg,
-  //           limit: ITEM_PER_PAGE,
-  //           offset: noOffset ? 0 : updatedOffset,
-  //         }),
-  //       )
-  //       if (result.data && result.data?.length > 0) {
-  //         const { data: resData } = result
-  //         setData(prevData => [...prevData, ...resData])
-  //         setOffset(updatedOffset)
-  //         if (resData.length < ITEM_PER_PAGE) {
-  //           setHasMore(false)
-  //           setLoading(false)
-  //         }
-  //       } else {
-  //         setHasMore(false)
-  //         setLoading(false)
-  //       }
-  //     }
-  //     fetchNewWikis()
-  //   }, FETCH_DELAY_TIME)
-  // }
-
   return {
     fetcher,
     hasMore,
