0095f2530a37e8498e5d2c3555704b3ddd14dc26
OleanjiKingCode <holamilean56@gmail.com>
2022-09-05 13:39:09 +0100

    fixed the height of the wiki rows and also the pomote functionality to remove old wiki and replace with new

--- src/services/admin/queries.ts
+++ src/services/admin/queries.ts
@@ -1,26 +1,37 @@
 import { gql } from 'graphql-request'
 
-export const GET_WIKIS_BY_TITLE = gql`
-  query wikisByTitle($title: String!) {
-    wikisByTitle(title: $title) {
+export const WIKIS_EDITED = gql`
+  query WikisEdited($startDate: Int, $endDate: Int, $interval: String) {
+    wikisEdited(startDate: $startDate, endDate: $endDate, interval: $interval) {
+      amount
+      startOn
+      endOn
+    }
+  }
+`
+
+export const WIKIS_CREATED = gql`
+  query WikisCreated($startDate: Int, $endDate: Int, $interval: String) {
+    wikisCreated(
+      startDate: $startDate
+      endDate: $endDate
+      interval: $interval
+    ) {
+      amount
+      startOn
+      endOn
+    }
+  }
+`
+
+export const CREATED_WIKIS_TABLE = gql`
+  query Wikis($offset: Int!) {
+    wikis(limit: 10, offset: $offset) {
       id
       title
-      content
-      summary
-      created
-      updated
-      tags {
-        id
-      }
       images {
         id
-      }
-      categories {
-        id
-        title
-      }
-      user {
-        id
+        type
       }
       author {
         id
@@ -28,29 +39,210 @@ export const GET_WIKIS_BY_TITLE = gql`
           username
         }
       }
+      created
+      tags {
+        id
+      }
+      promoted
+      hidden
     }
   }
 `
-
-export const GET_CATEGORIES_BY_TITLE = gql`
-  query categoryByTitle($title: String!) {
-    categoryByTitle(title: $title) {
+export const PROMOTED_WIKIS_TABLE = gql`
+  query PromotedWikis($offset: Int!) {
+    promotedWikis(limit: 10, offset: $offset) {
       id
       title
-      cardImage
-      heroImage
-      icon
+      images {
+        id
+        type
+      }
+      author {
+        id
+        profile {
+          username
+        }
+      }
+      created
+      tags {
+        id
+      }
+      promoted
+      hidden
+    }
+  }
+`
+export const HIDDEN_WIKIS_TABLE = gql`
+  query HiddenWikis($offset: Int!) {
+    wikisHidden(limit: 10, offset: $offset) {
+      id
+      title
+      images {
+        type
+      }
+      author {
+        id
+        profile {
+          username
+        }
+      }
+      created
+      tags {
+        id
+      }
+      promoted
+      hidden
     }
   }
 `
 
-export const GET_USERNAME_BY_TITLE = gql`
-  query getProfileLikeUsername($id: String, $username: String) {
-    getProfileLikeUsername(id: $id, username: $username) {
+export const EDITORS_TABLE = gql`
+  query Editors($limit: Int!, $offset: Int!) {
+    users(limit: $limit, offset: $offset) {
       id
-      username
-      bio
-      avatar
+      active
+      profile {
+        username
+        avatar
+      }
+      wikisEdited {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+      wikisCreated {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+export const HIDDEN_EDITORS_TABLE = gql`
+  query HiddenEditors($limit: Int!, $offset: Int!) {
+    usersHidden(limit: $limit, offset: $offset) {
+      id
+      active
+      profile {
+        username
+        avatar
+      }
+      wikisEdited {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+      wikisCreated {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+export const SEARCHED_EDITORS = gql`
+  query Editors($id: String!) {
+    usersById(id: $id) {
+      id
+      active
+      profile {
+        username
+        avatar
+      }
+      wikisEdited {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+      wikisCreated {
+        id
+        wikiId
+        datetime
+        ipfs
+        content {
+          title
+          images {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+export const EDITORS_COUNT = gql`
+  query EditorCount($startDate: Int, $endDate: Int) {
+    editorCount(startDate: $startDate, endDate: $endDate) {
+      amount
+    }
+  }
+`
+export const HIDE_WIKI = gql`
+  mutation HideWiki($id: String!) {
+    hideWiki(id: $id) {
+      id
+      ipfs
+    }
+  }
+`
+
+export const UNHIDE_WIKI = gql`
+  mutation UnHideWiki($id: String!) {
+    unhideWiki(id: $id) {
+      id
+      ipfs
+    }
+  }
+`
+export const POST_PROMOTED_WIKI = gql`
+  mutation PromotedWiki($id: String!, $level: Int) {
+    promoteWiki(id: $id, level: $level) {
+      id
+    }
+  }
+`
+
+export const TOGGLE_USER = gql`
+  mutation ToggleUser($id: String!, $active: Boolean) {
+    toggleUserStateById(id: $id, active: $active) {
+      id
     }
   }
 `
