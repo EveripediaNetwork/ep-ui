73fa6441526c945967d3ee968c6ba52878eaccef
César Rodríguez <kesarr@gmail.com>
2022-09-08 10:54:35 +0200

    Update DiscoverMore.tsx

--- src/components/Landing/DiscoverMore.tsx
+++ src/components/Landing/DiscoverMore.tsx
@@ -12,7 +12,7 @@ const DiscoverMore = ({ tagsData }: DiscoverMoreProps) => {
     <Box bgColor="gray.50" _dark={{ bgColor: 'whiteAlpha.50' }} p={8} pb={20}>
       <Box maxW="1020px" mx="auto" py={8} px={4}>
         <Heading fontWeight="600" opacity={0.8} fontSize={20}>
-          Discover More on everipedia
+          Discover More on IQ.wiki
         </Heading>
         <Wrap mt={8} spacing={4}>
           {tagsData?.map(tag => (
