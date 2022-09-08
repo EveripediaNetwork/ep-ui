7799a87c4101613f144a228fa83367e0fc73c4f1
Srujan Gurram <srujangs8@gmail.com>
2022-09-08 19:42:24 +0530

    Switched ens caching to do with rtk query instead

---
+++ src/services/ens/index.ts
@@ -0,0 +1,27 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export interface ENSData {
+  address: string
+  name: string
+  displayName: string
+  avatar: string
+}
+
+export const ensApi = createApi({
+  reducerPath: 'ensApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: '/api',
+  }),
+  endpoints: builder => ({
+    getEns: builder.query<ENSData, string>({
+      query: (address: string) => ({ url: `/ens/${address}` }),
+    }),
+  }),
+})
+
+export const {
+  useGetEnsQuery,
+  util: { getRunningOperationPromises },
+} = ensApi
+
+export const { getEns } = ensApi.endpoints
