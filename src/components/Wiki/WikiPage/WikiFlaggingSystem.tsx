d61196a556dafc4d843f93185e89502f13245c27
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-19 19:56:51 +0100

    Fixes wiki flagging system UI

--- src/components/Wiki/WikiPage/WikiFlaggingSystem.tsx
+++ src/components/Wiki/WikiPage/WikiFlaggingSystem.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Box,
   Flex,
@@ -14,6 +14,7 @@ import {
   ModalBody,
   ModalCloseButton,
   Button,
+  useToast,
 } from '@chakra-ui/react'
 import { RiFlagFill } from 'react-icons/ri'
 
@@ -32,39 +33,56 @@ const FlaggingSystemModal = ({
   isOpen,
   onClose,
 }: WikiFlaggingModalProps) => {
-  console.log(id)
+  const toast = useToast()
+  const [flagContent, setFlagContent] = useState('')
+
+  const postFlagHandler = async e => {
+    e.preventDefault()
+
+    console.log({
+      id,
+      flagContent,
+    })
+    onClose()
+    toast({
+      title: 'Your report has been successfully submitted!',
+      status: 'success',
+      duration: 3000,
+    })
+  }
+
   return (
     <Modal
       isOpen={isOpen}
       onClose={onClose}
       isCentered
-      size={{ base: 'sm', md: '2xl' }}
+      size={{ base: 'md', md: '2xl' }}
       motionPreset="slideInBottom"
     >
       <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>
-          <Text>Report</Text>
-          <Text
-            fontWeight={500}
-            mt="3"
-            pb="3"
-            fontSize={{ base: '12px', md: '14px' }}
-            borderBottomWidth="1px"
-            borderBottomColor="gray.300"
-            color="gray.800"
-            _dark={{
-              borderBottomColor: 'whiteAlpha.300',
-              color: 'whiteAlpha.900',
-            }}
-          >
-            Please, in a few words, write about what you would like to be
-            reported in the text box below.
-          </Text>
-        </ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>
-          <form>
+      <ModalContent mx={{ base: '15px', md: '0' }}>
+        <form onSubmit={postFlagHandler}>
+          <ModalHeader>
+            <Text>Report</Text>
+            <Text
+              fontWeight={500}
+              mt="3"
+              pb="3"
+              fontSize={{ base: '12px', md: '14px' }}
+              borderBottomWidth="1px"
+              borderBottomColor="gray.300"
+              color="gray.800"
+              _dark={{
+                borderBottomColor: 'whiteAlpha.300',
+                color: 'whiteAlpha.900',
+              }}
+            >
+              Please, in a few words, write about what you would like to be
+              reported in the text box below.
+            </Text>
+          </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
             <Textarea
               fontWeight={400}
               fontSize="14px"
@@ -72,24 +90,29 @@ const FlaggingSystemModal = ({
               color="gray.800"
               borderColor="gray.300"
               placeholder="Write your text here...."
+              name="report-content"
+              onChange={e => {
+                setFlagContent(e.target.value)
+              }}
               _dark={{ color: 'whiteAlpha.800', borderColor: 'whiteAlpha.200' }}
             />
-          </form>
-        </ModalBody>
-        <ModalFooter justifyContent="center">
-          <Button
-            background="brand.500"
-            fontSize="12px"
-            fontWeight="400"
-            _dark={{ background: 'brand.800' }}
-            w={{
-              base: 'full',
-              md: 'initial',
-            }}
-          >
-            Confirm
-          </Button>
-        </ModalFooter>
+          </ModalBody>
+          <ModalFooter justifyContent="center">
+            <Button
+              type="submit"
+              background="brand.500"
+              fontSize="12px"
+              fontWeight="400"
+              _dark={{ background: 'brand.800' }}
+              w={{
+                base: 'full',
+                md: 'initial',
+              }}
+            >
+              Confirm
+            </Button>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
   )
