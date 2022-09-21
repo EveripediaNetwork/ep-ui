febe9890f4602cc0fbe3507280c3326245a5a6d3
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-21 15:51:26 +0100

    Fixes wiki flagging system UI

--- src/components/Wiki/WikiPage/WikiMainContent.tsx
+++ src/components/Wiki/WikiPage/WikiMainContent.tsx
@@ -10,7 +10,6 @@ import { customTableRenderer } from '@/utils/customTableRender'
 import { getWikiMetadataById } from '@/utils/getWikiFields'
 import styles from '../../../styles/markdown.module.css'
 import { WikiFlaggingSystem } from './WikiFlaggingSystem'
-import { getUserAddressFromCache } from '@/utils/getUserAddressFromCache'
 
 interface WikiMainContentProps {
   wiki: Wiki
