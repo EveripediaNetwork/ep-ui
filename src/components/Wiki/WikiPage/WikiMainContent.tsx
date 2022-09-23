4d45e1bee104b199265926ac6166e41c7674f45e
Adebesin Cell <adebesintolulope80@gmail.com>
2022-09-23 12:37:01 +0100

    Removed rehype-raw package

--- src/components/Wiki/WikiPage/WikiMainContent.tsx
+++ src/components/Wiki/WikiPage/WikiMainContent.tsx
@@ -3,7 +3,6 @@ import { Box, Heading, useColorMode } from '@chakra-ui/react'
 import React, { useMemo } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
-import rehypeRaw from 'rehype-raw'
 import { store } from '@/store/store'
 import { addToTOC } from '@/components/Wiki/WikiPage/CustomRenderers/customHeadingRender'
 import { getWikiMetadataById } from '@/utils/getWikiFields'
@@ -34,7 +33,6 @@ const MarkdownRender = React.memo(({ wiki }: { wiki: Wiki }) => {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeRaw]}
       components={{
         h1: addToTOC,
         h2: addToTOC,
