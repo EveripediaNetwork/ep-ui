14f17b380fcbd6c1833f3336b266bb7008ea4acb
Adebayo Olamilekan <75235148+OleanjiKingCode@users.noreply.github.com>
2022-09-26 14:02:40 +0100

    Update getWikiImageUrl.ts

--- src/utils/getWikiImageUrl.ts
+++ src/utils/getWikiImageUrl.ts
@@ -1,6 +1,9 @@
 import config from '@/config'
 import { Wiki, WikiPreview } from '@/types/Wiki'
+import { CreatedWikisCount } from '@/types/admin'
 
-export const getWikiImageUrl = (wiki?: Partial<Wiki> | WikiPreview) => {
+export const getWikiImageUrl = (
+  wiki?: Partial<Wiki> | WikiPreview | CreatedWikisCount,
+) => {
   return `${config.pinataBaseUrl}${wiki?.images ? wiki?.images[0]?.id : ''}`
 }
