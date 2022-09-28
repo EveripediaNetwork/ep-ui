6de6e3a50928d10aa6e70dd39b229967021db56c
Srujan Gurram <srujangs8@gmail.com>
2022-09-28 22:16:48 +0530

    splits the 5286 chunk into smaller pieces

--- src/components/Wagmi/DeferredWagmiProvider.tsx
+++ src/components/Wagmi/DeferredWagmiProvider.tsx
@@ -1,62 +1,97 @@
-import { createClient, WagmiConfig, chain, configureChains } from 'wagmi'
-import React, { ReactNode } from 'react'
-import { MagicConnector } from '@everipedia/wagmi-magic-connector'
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
-import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
-import { alchemyProvider } from 'wagmi/providers/alchemy'
-import { infuraProvider } from 'wagmi/providers/infura'
-import { publicProvider } from 'wagmi/providers/public'
-import { AlchemyProvider, Network } from '@ethersproject/providers'
+import React, { ReactNode, useContext, useEffect, useState } from 'react'
 import config from '@/config'
+import { WagmiStatusContext } from './WagmiStatusContext'
 
 const DeferredWagmiProvider = ({ children }: { children: ReactNode }) => {
-  const chainArray =
-    config.alchemyChain === 'matic' ? [chain.polygon] : [chain.polygonMumbai]
+  const [client, setClient] = useState<any>({})
+  const [WagmiConfiguration, setWagmiConfiguration] = useState<any>({})
+  const [initFinished, setInitFinished] = useState(false)
+  const { isWagmiWrapped } = useContext(WagmiStatusContext)
 
-  const { chains, provider } = configureChains(chainArray, [
-    alchemyProvider({ alchemyId: config.alchemyApiKey, weight: 1 }),
-    infuraProvider({ infuraId: config.infuraId, weight: 2 }),
-    publicProvider({ weight: 3 }),
-  ])
+  useEffect(() => {
+    const init = async () => {
+      const { createClient, chain, configureChains, WagmiConfig } =
+        await import('wagmi')
+      const { AlchemyProvider } = await import('@ethersproject/providers')
+      const { WalletConnectConnector } = await import(
+        'wagmi/connectors/walletConnect'
+      )
+      const { MetaMaskConnector } = await import('wagmi/connectors/metaMask')
+      const { MagicConnector } = await import(
+        '@everipedia/wagmi-magic-connector'
+      )
+      const { alchemyProvider } = await import('wagmi/providers/alchemy')
+      const { infuraProvider } = await import('wagmi/providers/infura')
+      const { publicProvider } = await import('wagmi/providers/public')
 
-  const network: Network = {
-    name: config.alchemyChain,
-    chainId: Number(config.chainId),
-  }
+      const chainArray =
+        config.alchemyChain === 'matic'
+          ? [chain.polygon]
+          : [chain.polygonMumbai]
 
-  const connectors = [
-    new MetaMaskConnector({ chains, options: { shimDisconnect: true } }),
-    new WalletConnectConnector({
-      chains,
-      options: {
-        qrcode: true,
-      },
-    }),
-    new MagicConnector({
-      chains,
-      options: {
-        apiKey: config.magicLinkApiKey,
-        oauthOptions: {
-          providers: ['google', 'discord', 'facebook', 'twitter'],
-        },
-        customLogo: '/images/braindao-logo.svg',
-        accentColor: '#ea3b87',
-        additionalMagicOptions: {
-          network: {
-            rpcUrl: AlchemyProvider.getUrl(network, config.alchemyApiKey).url,
-            chainId: Number(config.chainId),
+      const { chains, provider } = configureChains(chainArray, [
+        alchemyProvider({ alchemyId: config.alchemyApiKey, weight: 1 }),
+        infuraProvider({ infuraId: config.infuraId, weight: 2 }),
+        publicProvider({ weight: 3 }),
+      ])
+
+      const connectors = [
+        new MetaMaskConnector({
+          chains,
+          options: { shimDisconnect: true },
+        }),
+        new WalletConnectConnector({
+          chains,
+          options: {
+            qrcode: true,
           },
-        },
-      },
-    }),
-  ]
-  const client = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
-  })
+        }),
+        new MagicConnector({
+          chains,
+          options: {
+            apiKey: config.magicLinkApiKey,
+            oauthOptions: {
+              providers: ['google', 'discord', 'facebook', 'twitter'],
+            },
+            customLogo: '/images/braindao-logo.svg',
+            accentColor: '#ea3b87',
+            additionalMagicOptions: {
+              network: {
+                rpcUrl: AlchemyProvider.getUrl(
+                  {
+                    name: config.alchemyChain,
+                    chainId: Number(config.chainId),
+                  },
+                  config.alchemyApiKey,
+                ).url,
+                chainId: Number(config.chainId),
+              },
+            },
+          },
+        }),
+      ]
 
-  return <WagmiConfig client={client}>{children}</WagmiConfig>
+      const newClient = createClient({
+        autoConnect: true,
+        connectors,
+        provider,
+      })
+
+      setClient({ newClient })
+      setWagmiConfiguration({ WagmiConfig })
+      setInitFinished(true)
+    }
+    if (isWagmiWrapped && !initFinished) init()
+  }, [initFinished, isWagmiWrapped])
+
+  return (
+    WagmiConfiguration.WagmiConfig &&
+    client.newClient && (
+      <WagmiConfiguration.WagmiConfig client={client.newClient}>
+        {children}
+      </WagmiConfiguration.WagmiConfig>
+    )
+  )
 }
 
 export default DeferredWagmiProvider
