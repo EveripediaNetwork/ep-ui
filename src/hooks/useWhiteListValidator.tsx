fdc6c8c2c0c06254b17c510dde7e41417532858c
Drebakare <bakaredmlr@gmail.com>
2022-10-06 09:36:22 +0100

    fixes lint error

--- src/hooks/useWhiteListValidator.tsx
+++ src/hooks/useWhiteListValidator.tsx
@@ -15,7 +15,7 @@ export const useWhiteListValidator = (address: string | undefined | null) => {
     args: [address],
   })
   return {
-    userCanEdit: config.isProduction ? isEditorWhiteListed :  true,
+    userCanEdit: config.isProduction ? isEditorWhiteListed : true,
   }
 }
 
