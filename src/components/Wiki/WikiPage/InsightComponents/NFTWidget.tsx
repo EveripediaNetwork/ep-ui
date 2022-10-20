383dec574c6b70b1b18bda0e1f8eb59e12e2fc85
James Akpan <jamesakpan016@gmail.com>
2022-10-20 20:17:01 +0100

    removing logs and alchemy package, conditional rendering

--- src/components/Wiki/WikiPage/InsightComponents/NFTWidget.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/NFTWidget.tsx
@@ -12,13 +12,13 @@ import { nftListing } from '@/services/nftlisting/index'
 import { store } from '@/store/store'
 
 const NFTWidget = ({
-  category,
+  categories,
   metaData,
 }: {
-  category: { id: string; title: string }[]
+  categories: { id: string; title: string }[]
   metaData: { id: string; value: string }[]
 }) => {
-  const isNFTWiki = category.find((item: { id: string; title: string }) => {
+  const isNFTWiki = categories.find((item: { id: string; title: string }) => {
     return item.id === 'nfts'
   })
   const [currentNFTImage, setCurrentNTFImage] = useState<string>('')
@@ -38,7 +38,6 @@ const NFTWidget = ({
         }),
       )
       const nftImgURL = data?.media[0].gateway
-      console.log(data)
       setCurrentNTFImage(nftImgURL || '')
       setCurrentNFTHashDisplay(currentNFTHash)
     }
@@ -51,65 +50,67 @@ const NFTWidget = ({
     }
   }, [])
 
-  return (
-    <Flex
-      display={isNFTWiki && contractData ? 'flex' : 'none'}
-      flexDirection="column"
-      p="14px 10px"
-      bg="#f5f5f5"
-      _dark={{ bg: 'transparent', border: '1px solid #3f444e' }}
-      w="100%"
-      gap="4"
-      borderRadius={8}
-    >
-      <Text textAlign="left" fontSize="12px" color="#9d9d9d">
-        Search NFT Collection
-      </Text>
-      <Image src={currentNFTImage} />
-      <Flex justifyContent="center">
-        <Heading>#{currentNFTHashDisplay}</Heading>
-      </Flex>
+  if (isNFTWiki) {
+    return (
       <Flex
-        borderRadius="8px"
-        flexDir="row"
-        alignItems="center"
-        bg="white"
-        _dark={{ bg: '#2d3748' }}
-        p="2"
-        gap="2"
+        flexDirection="column"
+        p="14px 10px"
+        bg="#f5f5f5"
+        _dark={{ bg: 'transparent', border: '1px solid #3f444e' }}
+        w="100%"
+        gap="4"
+        borderRadius={8}
       >
-        <FormControl>
-          <Input
-            boxSizing="border-box"
-            _focus={{ border: '1px solid #FF5CAA' }}
-            p="1"
-            border="none"
-            type="number"
-            placeholder="Input NFT ID"
-            onChange={e => {
-              const value = Number(e.target.value)
-              if (!Number.isNaN(value)) {
-                setCurrentNFTHash(value)
-              }
-            }}
-          />
-        </FormControl>
-        <Button
-          cursor="pointer"
-          as="a"
-          target="_blank"
-          size="md"
-          variant="solid"
-          disabled={!currentNFTHash}
-          onClick={() => {
-            fetchNFT()
-          }}
+        <Text textAlign="left" fontSize="12px" color="#9d9d9d">
+          Search NFT Collection
+        </Text>
+        <Image src={currentNFTImage} />
+        <Flex justifyContent="center">
+          <Heading>#{currentNFTHashDisplay}</Heading>
+        </Flex>
+        <Flex
+          borderRadius="8px"
+          flexDir="row"
+          alignItems="center"
+          bg="white"
+          _dark={{ bg: '#2d3748' }}
+          p="2"
+          gap="2"
         >
-          Search
-        </Button>
+          <FormControl>
+            <Input
+              boxSizing="border-box"
+              _focus={{ border: '1px solid #FF5CAA' }}
+              p="1"
+              border="none"
+              type="number"
+              placeholder="Input NFT ID"
+              onChange={e => {
+                const value = Number(e.target.value)
+                if (!Number.isNaN(value)) {
+                  setCurrentNFTHash(value)
+                }
+              }}
+            />
+          </FormControl>
+          <Button
+            cursor="pointer"
+            as="a"
+            target="_blank"
+            size="md"
+            variant="solid"
+            disabled={!currentNFTHash}
+            onClick={() => {
+              fetchNFT()
+            }}
+          >
+            Search
+          </Button>
+        </Flex>
       </Flex>
-    </Flex>
-  )
+    )
+  }
+  return null
 }
 
 export default NFTWidget
--- src/components/Wiki/WikiPage/WikiInsights.tsx
+++ src/components/Wiki/WikiPage/WikiInsights.tsx
@@ -106,7 +106,7 @@ const WikiInsights = ({
               )}
             </>
           )}
-          <NFTWidget category={wiki.categories} metaData={wiki.metadata} />
+          <NFTWidget categories={wiki.categories} metaData={wiki.metadata} />
           <WikiCommitMessage
             commitMessage={commitMessage}
             user={wiki.user}
