81148244b5e35ebb0994c60640ef1fc865e90f6b
James Akpan <jamesakpan016@gmail.com>
2022-10-24 20:37:21 +0100

    make nft accordion, set fallback for nft img

---
+++ src/components/Wiki/WikiPage/InsightComponents/NFTImageFallBack.tsx
@@ -0,0 +1,20 @@
+import { Flex, Text, Image } from '@chakra-ui/react'
+import React from 'react'
+
+const PLACEHOLDER_IMAGE = `/broken-image.png`
+
+export const NFTImgFallback = () => {
+  return (
+    <Flex
+      flexDir="column"
+      alignItems="center"
+      justifyContent="center"
+      bg="#2D3748"
+      h="305px"
+      gap="2"
+    >
+      <Image w="30px" src={PLACEHOLDER_IMAGE} />
+      <Text fontSize="12px">There seems to be a problem with the image</Text>
+    </Flex>
+  )
+}
