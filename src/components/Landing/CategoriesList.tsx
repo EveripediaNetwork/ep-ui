c156ffb8bb3ff471a860194a15e96bf28834c2bb
OleanjiKingCode <holamilean56@gmail.com>
2022-10-27 15:59:32 +0100

    random category items

--- src/components/Landing/CategoriesList.tsx
+++ src/components/Landing/CategoriesList.tsx
@@ -18,6 +18,9 @@ interface CategoriesListProps {
 }
 const CategoriesList = ({ categories }: CategoriesListProps) => {
   const { t } = useTranslation()
+  console.log(new URL(categories[0].cardImage).pathname)
+  let RandcategoryItem: string =
+    categories[Math.floor(Math.random() * categories.length - 1)].id
 
   return (
     <VStack mt={10} mb={20} spacing={2}>
@@ -45,39 +48,43 @@ const CategoriesList = ({ categories }: CategoriesListProps) => {
         spacingY={12}
         px={6}
       >
-        {categories.map(category => (
-          <div key={category.id}>
-            <LinkBox
-              _hover={{ boxShadow: 'rgb(4 17 29 / 25%) 0px 0px 8px 0px' }}
-              cursor="pointer"
-              bgColor="cardBg"
-              borderRadius="lg"
-              overflow="hidden"
-              shadow="base"
-            >
-              <LinkOverlay href={`/categories/${category.id}`}>
-                <Image
-                  bgColor="DimColor"
-                  src={new URL(category.cardImage).pathname}
-                  h="200px"
-                  w="100%"
-                  alt={category.title}
-                />
+        {categories
+          .filter(item => {
+            return item.id !== RandcategoryItem
+          })
+          .map(category => (
+            <div key={category.id}>
+              <LinkBox
+                _hover={{ boxShadow: 'rgb(4 17 29 / 25%) 0px 0px 8px 0px' }}
+                cursor="pointer"
+                bgColor="cardBg"
+                borderRadius="lg"
+                overflow="hidden"
+                shadow="base"
+              >
+                <LinkOverlay href={`/categories/${category.id}`}>
+                  <Image
+                    bgColor="DimColor"
+                    src={new URL(category.cardImage).pathname}
+                    h="200px"
+                    w="100%"
+                    alt={category.title}
+                  />
 
-                <Text
-                  py="4"
-                  w="100%"
-                  textAlign="center"
-                  fontWeight="bold"
-                  fontSize="lg"
-                  size="md"
-                >
-                  {category.title}
-                </Text>
-              </LinkOverlay>
-            </LinkBox>
-          </div>
-        ))}
+                  <Text
+                    py="4"
+                    w="100%"
+                    textAlign="center"
+                    fontWeight="bold"
+                    fontSize="lg"
+                    size="md"
+                  >
+                    {category.title}
+                  </Text>
+                </LinkOverlay>
+              </LinkBox>
+            </div>
+          ))}
       </SimpleGrid>
       {categories.length < 1 && (
         <Center w="full" h="16">
