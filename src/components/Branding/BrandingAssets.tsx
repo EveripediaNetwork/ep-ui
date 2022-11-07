c0b3e6b2b9af0a38564975fc4590826c088b39e6
James Akpan <jamesakpan016@gmail.com>
2022-11-07 17:12:27 +0100

    Branding Page (#936)
    * adding brand images
    
    * updating with mobile and tab responsiveness
    
    * implementing display download optns
    
    * change state of asset to initial when siblin is selected
    
    * code review resuable color/fix img alt
    
    * code review resuable color/fix img alt

---
+++ src/components/Branding/BrandingAssets.tsx
@@ -0,0 +1,72 @@
+import { Button, Flex, Image } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { BiCloudDownload } from 'react-icons/bi'
+import BrandingAssetDownloadBttn from './BrandingAssetDownloadBttn'
+
+export const BrandingAssets = ({
+  bg,
+  updateSelectedAsset,
+  currentlyViewed,
+}: {
+  bg: { bg: string; download: string }
+  updateSelectedAsset: () => void
+  currentlyViewed: string
+}) => {
+  const [showDownloadOptions, setShowDownloadOptions] = useState<boolean>(false)
+  useEffect(() => {
+    if (currentlyViewed === bg.bg) {
+      setShowDownloadOptions(true)
+    } else {
+      setShowDownloadOptions(false)
+    }
+  }, [currentlyViewed, bg.bg])
+  return (
+    <Flex w={{ base: '45%', md: '30%' }} flexDirection="column" gap={2}>
+      <Flex
+        flexDir="column"
+        gap={4}
+        display={showDownloadOptions ? 'flex' : 'none'}
+      >
+        <BrandingAssetDownloadBttn
+          text="download as .SVG"
+          href={`${bg.download}.svg`}
+          closeDownloadOptions={() => {
+            setShowDownloadOptions(false)
+          }}
+        />
+        <BrandingAssetDownloadBttn
+          text="download as .PNG"
+          href={`${bg.download}.png`}
+          closeDownloadOptions={() => {
+            setShowDownloadOptions(false)
+          }}
+        />
+      </Flex>
+
+      <Flex
+        gap="2"
+        display={!showDownloadOptions ? 'flex' : 'none'}
+        flexDir="column"
+      >
+        <Image src={bg.bg} alt={`IQ ${bg.bg} Logo`} />
+        <Button
+          onClick={() => {
+            updateSelectedAsset()
+            setShowDownloadOptions(true)
+          }}
+          justifyContent="end"
+          variant="outline"
+          border="none"
+          p="0"
+          rightIcon={<BiCloudDownload fontSize="22px" />}
+          h="fit-content"
+          fontWeight="light"
+          fontSize="md"
+          _hover={{ backgroundColor: 'transparent' }}
+        >
+          Download
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
