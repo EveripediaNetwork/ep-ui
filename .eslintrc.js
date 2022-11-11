874f229c0b52190660c6a04dfa50b932256a95ee
Adebesin Cell <adebesintolulope80@gmail.com>
2022-11-11 06:42:55 +0100

    Fixes react/no-property found error

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,12 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
+  rules: {
+    'react/no-unknown-property': [
+      2,
+      {
+        ignore: ['jsx', 'global'],
+      },
+    ],
+  },
 }
