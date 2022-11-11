6c4f1de3a8f0fe2795b068567a652d66ea4d9320
Srujan Gurram <srujangs8@gmail.com>
2022-11-11 15:09:02 +0530

    reverted husky and eslint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,4 @@ module.exports = {
     tsconfigRootDir: __dirname,
     project: ['./tsconfig.json'],
   },
-  rules: {
-    'react/no-unknown-property': [
-      2,
-      {
-        ignore: ['jsx', 'global'],
-      },
-    ],
-  },
 }
