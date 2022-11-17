8339b83f8abae2a65d31d4a4ae7cdfe9918b8e39
James Akpan <jamesakpan016@gmail.com>
2022-11-17 18:31:41 +0100

    mocked rank data

--- src/components/Rank/RankCard.tsx
+++ src/components/Rank/RankCard.tsx
@@ -1,3 +1,4 @@
+import { MarketCap } from '@/data/MarketCapData'
 import { Box, Flex, Icon, Text, useColorModeValue } from '@chakra-ui/react'
 import React from 'react'
 import { AiFillCaretDown, AiFillCaretUp } from 'react-icons/ai'
@@ -7,68 +8,6 @@ interface RankCardProps {
   title: string
   icon: IconType
 }
-const dummyData = [
-  {
-    name: 'Bored Ape Yatch Club',
-    alias: 'Bape',
-    capital: '87,932,562',
-    marketCap: '82,932,562',
-  },
-  {
-    name: 'Axie',
-    alias: 'Axs',
-    capital: '22,032,322',
-    marketCap: '62,032,322',
-  },
-  {
-    name: 'Pudgy',
-    alias: 'PPG',
-    capital: '322,321',
-    marketCap: '124,321',
-  },
-  {
-    name: 'Hashmasks',
-    alias: 'HM',
-    capital: '322,422,435',
-    marketCap: '432,434,676',
-  },
-  {
-    name: 'Ragnar',
-    alias: 'RG',
-    capital: '322,422,435',
-    marketCap: '432,434,676',
-  },
-  {
-    name: 'Hashmasks',
-    alias: 'HM',
-    capital: '322,422,435',
-    marketCap: '432,434,676',
-  },
-  {
-    name: 'Desperate Ape',
-    alias: 'DAW',
-    capital: '321,539,234',
-    marketCap: '221,839,434',
-  },
-  {
-    name: 'Punks Comics',
-    alias: 'PC',
-    capital: '1,432,532',
-    marketCap: '2,432,532',
-  },
-  {
-    name: 'Yaka Maza',
-    alias: 'YM',
-    capital: '434,234',
-    marketCap: '234,234',
-  },
-  {
-    name: 'CryptoGoonz',
-    alias: 'CG',
-    capital: '223,432,422',
-    marketCap: '123,432,422',
-  },
-]
 const RankCard = ({ title, icon }: RankCardProps) => {
   const downIndicationIconColor = useColorModeValue('#E53E3E', '#FC8181')
   const upIndicationIconColor = useColorModeValue('#25855A', '#68D391')
@@ -87,7 +26,7 @@ const RankCard = ({ title, icon }: RankCardProps) => {
         <Text>{title}</Text>
       </Flex>
       <Flex gap={12} flexDir="column">
-        {dummyData.map((item, index) => {
+        {MarketCap.map((item, index) => {
           return (
             <Flex gap={4} alignItems="center">
               <Text fontSize="lg">{index + 1}</Text>
