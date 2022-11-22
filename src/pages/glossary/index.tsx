4c7c40fe00363eeaf36c5a7abc2b2fd2334cffef
Drebakare <bakaredmlr@gmail.com>
2022-11-22 15:59:26 +0100

    refactors glossary filter section

--- src/pages/glossary/index.tsx
+++ src/pages/glossary/index.tsx
@@ -23,16 +23,92 @@ import {
   COMMONLY_SEARCHED_WIKIS,
   glossaryAlphabetsData,
 } from '@/data/GlossaryAlphabetsData'
-import { Search2Icon } from '@chakra-ui/icons'
+import { Search2Icon, ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 import GlossaryItem from '@/components/Glossary/GlossaryItems'
 import {
   useGetGlossaryTagWikisQuery,
   // useGetTagsQuery,
 } from '@/services/glossary'
 import { useInView } from 'react-intersection-observer'
-import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
 
 // const CONVERTED_CURRENT_DATE = Math.floor(Date.now() / 1000)
+
+const GlossaryFilterSection = ({
+  searchText,
+  searchPage,
+  shouldBeFixed,
+  setSearchText,
+  activeIndex,
+  setActiveIndex
+}: {
+  searchText: string
+  searchPage: (value: string) => void
+  shouldBeFixed: boolean
+  setSearchText: (value: string) => void
+  activeIndex: number|undefined
+  setActiveIndex: (value: number) => void
+}) => {
+  return (
+    <>
+      <Box w="full" mb={shouldBeFixed ? 5 : 0}>
+        <InputGroup size="md" w="full">
+          <InputLeftElement
+            ml={{ base: '15px', xl: 'unset' }}
+            pointerEvents="none"
+            h="full"
+          >
+            <Search2Icon
+              color="gray.300"
+              fontSize={{ base: 'sm', lg: 'auto' }}
+              ml={{ base: '-8', md: '0' }}
+            />
+          </InputLeftElement>
+          <Input
+            type="tel"
+            placeholder="Search for words"
+            value={searchText}
+            onChange={e => searchPage(e.target.value)}
+            _placeholder={{ color: 'placeholderColor' }}
+          />
+        </InputGroup>
+      </Box>
+      <Flex
+        my="4"
+        w="full"
+        wrap="wrap"
+        alignItems="center"
+        justifyContent={{ lg: 'start', '2xl': 'center' }}
+        gap={{ base: '3', '2xl': '10' }}
+      >
+        {COMMONLY_SEARCHED_WIKIS.slice(0, 5)?.map((word, i) => {
+          return (
+            <Tag
+              size="lg"
+              key={word}
+              bg={i === activeIndex ? 'tagActiveBgColor' : 'transparent'}
+              color={i === activeIndex ? 'tagActiveColor' : 'tagColor'}
+              cursor="pointer"
+              borderRadius="full"
+              borderWidth="thin"
+              fontWeight="normal"
+              fontSize={{ base: 'sm', lg: 'md' }}
+              _hover={{
+                bgColor: 'tagHoverColor',
+              }}
+              onClick={() => {
+                setActiveIndex(i)
+                setSearchText(word)
+                searchPage(word)
+              }}
+            >
+              <TagLabel>{word}</TagLabel>
+            </Tag>
+          )
+        })}
+      </Flex>
+    </>
+  )
+}
 
 const Glossary: NextPage = () => {
   const { ref, entry } = useInView({
@@ -62,8 +138,9 @@ const Glossary: NextPage = () => {
     entry?.intersectionRatio !== undefined && !entry?.isIntersecting
 
   const heightOfElement = (newEntry?.boundingClientRect.height || 96) + 68
-  const [activeIndex, setActiveIndex] = useState<number>()
   const [isVisible, setIsVisible] = useState(false)
+  const [activeIndex, setActiveIndex] = useState<number>()
+
 
   const searchPage = (input: string) => {
     const letter =
@@ -161,132 +238,26 @@ const Glossary: NextPage = () => {
 
           {!shouldBeFixed ? (
             <>
-              <Box w="full" mb={shouldBeFixed ? 5 : 0}>
-                <InputGroup size="md" w="full">
-                  <InputLeftElement
-                    ml={{ base: '15px', xl: 'unset' }}
-                    pointerEvents="none"
-                    h="full"
-                  >
-                    <Search2Icon
-                      color="gray.300"
-                      fontSize={{ base: 'sm', lg: 'auto' }}
-                      ml={{ base: '-8', md: '0' }}
-                    />
-                  </InputLeftElement>
-                  <Input
-                    type="tel"
-                    placeholder="Search for words"
-                    value={searchText}
-                    onChange={e => searchPage(e.target.value)}
-                    _placeholder={{ color: 'placeholderColor' }}
-                  />
-                </InputGroup>
-              </Box>
-              <Flex
-                my="4"
-                w="full"
-                wrap="wrap"
-                alignItems="center"
-                justifyContent={{ lg: 'start', '2xl': 'center' }}
-                gap={{ base: '3', '2xl': '10' }}
-              >
-                {COMMONLY_SEARCHED_WIKIS.slice(0, 5)?.map((word, i) => {
-                  return (
-                    <Tag
-                      size="lg"
-                      key={word}
-                      bg={
-                        i === activeIndex ? 'tagActiveBgColor' : 'transparent'
-                      }
-                      color={i === activeIndex ? 'tagActiveColor' : 'tagColor'}
-                      cursor="pointer"
-                      borderRadius="full"
-                      borderWidth="thin"
-                      fontWeight="normal"
-                      fontSize={{ base: 'sm', lg: 'md' }}
-                      _hover={{
-                        bgColor: 'tagHoverColor',
-                      }}
-                      onClick={() => {
-                        setActiveIndex(i)
-                        setSearchText(word)
-                        searchPage(word)
-                      }}
-                    >
-                      <TagLabel>{word}</TagLabel>
-                    </Tag>
-                  )
-                })}
-              </Flex>
+              <GlossaryFilterSection
+                setSearchText={text => setSearchText(text)}
+                shouldBeFixed={shouldBeFixed}
+                searchText={searchText}
+                searchPage={(text: string) => searchPage(text)}
+                activeIndex={activeIndex}
+                setActiveIndex={(index: number) => setActiveIndex(index)}
+              />
             </>
           ) : (
             <>
               {isVisible && (
-                <>
-                  <Box w="full" mb={shouldBeFixed ? 5 : 0}>
-                    <InputGroup size="md" w="full">
-                      <InputLeftElement
-                        ml={{ base: '15px', xl: 'unset' }}
-                        pointerEvents="none"
-                        h="full"
-                      >
-                        <Search2Icon
-                          color="gray.300"
-                          fontSize={{ base: 'sm', lg: 'auto' }}
-                          ml={{ base: '-8', md: '0' }}
-                        />
-                      </InputLeftElement>
-                      <Input
-                        type="tel"
-                        placeholder="Search for words"
-                        value={searchText}
-                        onChange={e => searchPage(e.target.value)}
-                        _placeholder={{ color: 'placeholderColor' }}
-                      />
-                    </InputGroup>
-                  </Box>
-                  <Flex
-                    my="4"
-                    w="full"
-                    wrap="wrap"
-                    alignItems="center"
-                    justifyContent={{ lg: 'start', '2xl': 'center' }}
-                    gap={{ base: '3', '2xl': '10' }}
-                  >
-                    {COMMONLY_SEARCHED_WIKIS.slice(0, 5)?.map((word, i) => {
-                      return (
-                        <Tag
-                          size="lg"
-                          key={word}
-                          bg={
-                            i === activeIndex
-                              ? 'tagActiveBgColor'
-                              : 'transparent'
-                          }
-                          color={
-                            i === activeIndex ? 'tagActiveColor' : 'tagColor'
-                          }
-                          cursor="pointer"
-                          borderRadius="full"
-                          borderWidth="thin"
-                          fontWeight="normal"
-                          fontSize={{ base: 'sm', lg: 'md' }}
-                          _hover={{
-                            bgColor: 'tagHoverColor',
-                          }}
-                          onClick={() => {
-                            setActiveIndex(i)
-                            setSearchText(word)
-                            searchPage(word)
-                          }}
-                        >
-                          <TagLabel>{word}</TagLabel>
-                        </Tag>
-                      )
-                    })}
-                  </Flex>
-                </>
+                <GlossaryFilterSection
+                  setSearchText={text => setSearchText(text)}
+                  shouldBeFixed={shouldBeFixed}
+                  searchText={searchText}
+                  searchPage={(text: string) => searchPage(text)}
+                  activeIndex={activeIndex}
+                  setActiveIndex={(index: number) => setActiveIndex(index)}
+                />
               )}
               <IconButton
                 width="full"
