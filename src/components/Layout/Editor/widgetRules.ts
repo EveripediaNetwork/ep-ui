6b35db5173f816b69e72480a9e25446107f0b888
Adebesin Cell <adebesintolulope80@gmail.com>
2022-12-29 18:57:01 +0100

    Refactored widget rules

--- src/components/Layout/Editor/widgetRules.ts
+++ src/components/Layout/Editor/widgetRules.ts
@@ -1,48 +1,10 @@
+import { CreateWidget } from '@/data/EmbedsData'
+
 const ytWidgetRule = /\[YOUTUBE@VID\]\((\S+)\)/
 const duneWidgetRule = /\[DUNE@EMBED\]\((\S+)\)/
 
-const ytWidget = {
-  rule: ytWidgetRule,
-  toDOM(text: string) {
-    const rule = ytWidgetRule
-    const matched = text.match(rule)
-    if (!matched) return null
+const ytWidget = CreateWidget(ytWidgetRule, 'https://www.youtube.com/embed/$1')
 
-    const ytIframeContainer = document.createElement('div')
-    ytIframeContainer.classList.add('embed-widget-iframe')
-
-    const ytIframe = document.createElement('iframe')
-    ytIframe.onload = () => {
-      ytIframeContainer.classList.add('embed-widget-iframe__loaded')
-    }
-    ytIframe.src = `https://www.youtube.com/embed/${matched[1]}`
-
-    ytIframeContainer.appendChild(ytIframe)
-
-    return ytIframeContainer
-  },
-}
-
-const duneWidget = {
-  rule: duneWidgetRule,
-  toDOM(text: string) {
-    const rule = duneWidgetRule
-    const matched = text.match(rule)
-    if (!matched) return null
-
-    const duneIframeContainer = document.createElement('div')
-    duneIframeContainer.classList.add('embed-widget-iframe')
-
-    const duneIframe = document.createElement('iframe')
-    duneIframe.src = `https://dune.com/embeds/${matched[1]}`
-    duneIframe.onload = () => {
-      duneIframeContainer.classList.add('embed-widget-iframe__loaded')
-    }
-
-    duneIframeContainer.appendChild(duneIframe)
-
-    return duneIframeContainer
-  },
-}
+const duneWidget = CreateWidget(duneWidgetRule, 'https://dune.com/embeds/$1')
 
 export const widgetRules = [ytWidget, duneWidget]
--- src/data/EmbedsData.ts
+++ src/data/EmbedsData.ts
@@ -6,3 +6,26 @@ export const embeds = [
     type: 'DUNE',
   },
 ]
+
+export const CreateWidget = (rule: RegExp, template: string) => {
+  return {
+    rule,
+    toDOM(text: string) {
+      const matched = text.match(rule)
+      if (!matched) return null
+
+      const container = document.createElement('div')
+      container.classList.add('embed-widget-iframe')
+
+      const iframe = document.createElement('iframe')
+      iframe.src = template.replace('$1', matched[1])
+      iframe.onload = () => {
+        container.classList.add('embed-widget-iframe__loaded')
+      }
+
+      container.appendChild(iframe)
+
+      return container
+    },
+  }
+}
