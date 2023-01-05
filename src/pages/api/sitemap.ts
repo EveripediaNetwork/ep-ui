9794165e023e02304bac3bd34dbe480c5be471c4
Drebakare <bakaredmlr@gmail.com>
2023-01-05 12:35:27 +0100

    fixes lint error

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -5,12 +5,12 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<string>,
 ) {
-  const apiUrl = config.graphqlUrl.replace('/graphql','')
+  const apiUrl = config.graphqlUrl.replace('/graphql', '')
   const url = `${apiUrl}/sitemap.xml`
   const result = await fetch(url, {
     headers: {
       Accept: 'text/xml',
-    }
+    },
   })
   const response = await result.text()
   res.setHeader('Cache-Control', 's-maxage=60')
