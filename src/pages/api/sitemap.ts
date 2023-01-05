65f8364ba2a89f951a61e6bfba7c912b6c3a3363
Drebakare <bakaredmlr@gmail.com>
2023-01-05 12:04:26 +0100

    adds api domain

--- src/pages/api/sitemap.ts
+++ src/pages/api/sitemap.ts
@@ -1,9 +1,14 @@
-import type { NextApiResponse } from 'next'
+import config from '@/config'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(res: NextApiResponse<string>) {
-  const url = process.env.NEXT_PUBLIC_EP_API as string
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>,
+) {
+  const url = `${config.apiUrl}/sitemap.xml`
   const result = await fetch(url)
   const response = await result.text()
   res.setHeader('Cache-Control', 's-maxage=60')
   return res.status(200).send(response)
 }
+
