732196c21e10fbeedd11aaf1706f2b2ecd8c4453
Srujan Gurram <srujangs8@gmail.com>
2023-01-18 16:56:32 +0530

    added intersection observers to lazyload widgets and embeds on wiki page

--- src/components/Wiki/WikiPage/CustomRenderers/LinkRenderers/WidgetLinkRender.tsx
+++ src/components/Wiki/WikiPage/CustomRenderers/LinkRenderers/WidgetLinkRender.tsx
@@ -1,9 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import LiteYouTubeEmbed from 'react-lite-youtube-embed'
 import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css'
 
 const WidgetLinkRender = ({ text, href }: { text: string; href: string }) => {
   const [iframeLoading, setIframeLoading] = useState(true)
+  const [isInView, setIsInView] = useState(false)
+  const widgetRef = React.useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) setIsInView(true)
+      },
+      {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1,
+      },
+    )
+    observer.observe(widgetRef.current as Element)
+
+    return () => observer.disconnect()
+  }, [])
 
   const iframeLoadHandler = () => {
     setIframeLoading(false)
@@ -12,26 +30,34 @@ const WidgetLinkRender = ({ text, href }: { text: string; href: string }) => {
   const renderWidget = (src: string, title: string, type: string) => {
     if (type === 'youtube') {
       return (
-        <div className="embed-widget-iframe embed-widget-iframe__loaded">
-          <LiteYouTubeEmbed id={src} title="" />
+        <div
+          ref={widgetRef}
+          className="embed-widget-iframe embed-widget-iframe__loaded"
+        >
+          {isInView ? <LiteYouTubeEmbed id={src} title="" /> : <article />}
         </div>
       )
     }
     return (
       <div
+        ref={widgetRef}
         className={
           iframeLoading
             ? 'embed-widget-iframe'
             : 'embed-widget-iframe embed-widget-iframe__loaded'
         }
       >
-        <iframe
-          className={type === 'dune' ? 'embed-widget-iframe-dune-bg' : ''}
-          onLoad={iframeLoadHandler}
-          loading="lazy"
-          title={title}
-          src={src}
-        />
+        {isInView ? (
+          <iframe
+            className={type === 'dune' ? 'embed-widget-iframe-dune-bg' : ''}
+            onLoad={iframeLoadHandler}
+            loading="lazy"
+            title={title}
+            src={src}
+          />
+        ) : (
+          <article />
+        )}
       </div>
     )
   }
