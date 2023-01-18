026ba3f86444f8d120098dc410fd74ae1d17db6e
Srujan Gurram <srujangs8@gmail.com>
2023-01-19 01:16:26 +0530

    made related media image bit more responsive with proper resolution

--- src/components/Wiki/WikiPage/InsightComponents/RelatedMedia.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/RelatedMedia.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, VStack } from '@chakra-ui/react'
+import { AspectRatio, SimpleGrid, VStack } from '@chakra-ui/react'
 import React from 'react'
 import MediaPreview from '@/components/Elements/MediaPreview/MediaPreview'
 import { Media } from '@everipedia/iq-utils'
@@ -6,41 +6,45 @@ import { constructMediaUrl } from '@/utils/mediaUtils'
 import { Image } from '@/components/Elements/Image/Image'
 import WikiAccordion from '../../WikiAccordion'
 
+const RELATED_MEDIA_IMAGE_BOX_SIZE = 90
+
 const RelatedMediaGrid = ({ media }: { media?: Media[] }) => {
   if (!media || media.length === 0) return null
   return (
     <VStack w="100%" spacing={4} borderRadius={2}>
       <WikiAccordion title="Media">
         <SimpleGrid
-          columns={{ base: 2, sm: 4, md: 5, lg: 8, xl: 3 }}
-          spacing={5}
+          columns={{ base: 3, xl: 3, sm: 4, md: 6, '2xl': 4 }}
+          spacing={3}
         >
           {media.map((m, i) => (
-            <MediaPreview
-              type={m.source}
-              src={constructMediaUrl(m)}
-              key={i}
-              bgColor="dimColor"
-              borderRadius={4}
-              w="90px"
-              h="90px"
-              overflow="hidden"
-              data-caption={m?.caption}
-            >
-              <Image
-                src={
-                  m.source !== 'YOUTUBE'
-                    ? constructMediaUrl(m)
-                    : `https://i3.ytimg.com/vi/${m.name}/maxresdefault.jpg`
-                }
-                alt="related media"
-                imgH={90}
-                imgW={90}
-                hideOnError
-                objectFit="cover"
-                bgColor="fadedText2"
-              />
-            </MediaPreview>
+            <AspectRatio ratio={1}>
+              <MediaPreview
+                type={m.source}
+                src={constructMediaUrl(m)}
+                key={i}
+                bgColor="dimColor"
+                borderRadius={4}
+                boxSize="100%"
+                overflow="hidden"
+                data-caption={m?.caption}
+              >
+                <Image
+                  boxSize="100%"
+                  src={
+                    m.source !== 'YOUTUBE'
+                      ? constructMediaUrl(m)
+                      : `https://i3.ytimg.com/vi/${m.name}/maxresdefault.jpg`
+                  }
+                  alt="related media"
+                  imgH={RELATED_MEDIA_IMAGE_BOX_SIZE}
+                  imgW={RELATED_MEDIA_IMAGE_BOX_SIZE}
+                  hideOnError
+                  objectFit="cover"
+                  bgColor="fadedText2"
+                />
+              </MediaPreview>
+            </AspectRatio>
           ))}
         </SimpleGrid>
       </WikiAccordion>
