b3709087b5ae7cf61162a7c7d094113b17b25843
Srujan Gurram <srujangs8@gmail.com>
2023-01-19 15:11:51 +0530

    fixed paddings and margins around wiki details table after review

--- src/components/Wiki/WikiPage/InsightComponents/WikiDetails.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/WikiDetails.tsx
@@ -14,7 +14,6 @@ import {
   Wrap,
   Box,
   Stack,
-  Center,
 } from '@chakra-ui/react'
 import shortenAccount from '@/utils/shortenAccount'
 import { SiIpfs } from 'react-icons/si'
@@ -54,7 +53,9 @@ export const WikiDetails = ({
       <Stack
         direction={{ base: 'column', lg: 'row', xl: 'column' }}
         alignItems="center"
+        justifyContent="space-between"
         spacing={4}
+        w="full"
       >
         <Heading
           bgColor="wikiTitleBg"
@@ -72,7 +73,8 @@ export const WikiDetails = ({
         <AspectRatio
           mx="auto"
           w="100%"
-          maxW="450"
+          ml="0 !important"
+          maxW="400"
           ratio={WIKI_IMAGE_ASPECT_RATIO}
         >
           <WikiImage
@@ -82,135 +84,142 @@ export const WikiDetails = ({
             alt={title}
           />
         </AspectRatio>
-        <VStack w="100%" spacing={4} maxW="450px" mx="auto">
-          <Center>
-            <TableContainer w="full">
-              <Table size="sm" variant="simple">
-                <Tbody>
-                  {categories.length !== 0 && (
-                    <Tr>
-                      <Td pt={1} pb={3}>
-                        Categories
-                      </Td>
-                      <Td pt={1} pb={3}>
-                        <HStack flexWrap="wrap" justify="start">
-                          {categories?.map((category, i) => (
-                            <Link
-                              key={i}
-                              isExternal
-                              href={`/categories/${category.id}`}
-                              color="brandLinkColor"
-                            >
-                              {category.title}
-                            </Link>
-                          ))}
-                        </HStack>
-                      </Td>
-                    </Tr>
-                  )}
-                  {tags.length !== 0 && (
-                    <Tr>
-                      <Td py={1}>Tags</Td>
-                      <Td py={1}>
-                        <Wrap marginLeft={-2} spacing={1}>
-                          {tags?.map((tag, i) => (
-                            <Link key={i} href={`/tags/${tag.id}`} py={1}>
-                              <Tag whiteSpace="nowrap">{tag.id}</Tag>
-                            </Link>
-                          ))}
-                        </Wrap>
-                      </Td>
-                    </Tr>
-                  )}
+        <VStack
+          maxW={{ base: 'unset', sm: '400px', lg: 'unset' }}
+          w="100%"
+          spacing={4}
+          mx="auto"
+        >
+          <TableContainer w="full">
+            <Table size="sm" variant="simple">
+              <Tbody>
+                {categories.length !== 0 && (
                   <Tr>
-                    <Td>
-                      <HStack spacing={3} py="2">
-                        <Text>IPFS</Text>
+                    <Td
+                      w={{ base: 'unset', sm: '50%', xl: 'unset' }}
+                      pt={1}
+                      pb={3}
+                    >
+                      Categories
+                    </Td>
+                    <Td ml="auto" flex={1} pt={1} pb={3}>
+                      <HStack flexWrap="wrap" justify="start">
+                        {categories?.map((category, i) => (
+                          <Link
+                            key={i}
+                            isExternal
+                            href={`/categories/${category.id}`}
+                            color="brandLinkColor"
+                          >
+                            {category.title}
+                          </Link>
+                        ))}
                       </HStack>
                     </Td>
-                    <Td display="flex" align="center">
-                      <HStack gap={1} py="2">
-                        <SiIpfs />
+                  </Tr>
+                )}
+                {tags.length !== 0 && (
+                  <Tr>
+                    <Td py={1}>Tags</Td>
+                    <Td py={1}>
+                      <Wrap marginLeft={-2} spacing={1}>
+                        {tags?.map((tag, i) => (
+                          <Link key={i} href={`/tags/${tag.id}`} py={1}>
+                            <Tag whiteSpace="nowrap">{tag.id}</Tag>
+                          </Link>
+                        ))}
+                      </Wrap>
+                    </Td>
+                  </Tr>
+                )}
+                <Tr>
+                  <Td>
+                    <HStack spacing={3} py="2">
+                      <Text>IPFS</Text>
+                    </HStack>
+                  </Td>
+                  <Td display="flex" align="center">
+                    <HStack gap={1} py="2">
+                      <SiIpfs />
+                      <Link
+                        target="_blank"
+                        href={`https://ipfs.everipedia.org/ipfs/${ipfsHash}`}
+                        color="brandLinkColor"
+                      >
+                        <Text>{shortenAccount(ipfsHash || '')}</Text>
+                      </Link>
+                    </HStack>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td>
+                    <HStack spacing={3}>
+                      <Text>TX Hash</Text>
+                    </HStack>
+                  </Td>
+                  <Td display="flex" align="center" gap={3}>
+                    <GoLink />
+                    <Link
+                      target="_blank"
+                      href={`${config.blockExplorerUrl}/tx/${txHash}`}
+                      color="brandLinkColor"
+                    >
+                      <Text>{shortenAccount(txHash || '')}</Text>
+                    </Link>
+                  </Td>
+                </Tr>
+                <Tr>
+                  <Td whiteSpace="nowrap">
+                    <Text py="2">Created</Text>
+                  </Td>
+                  <Td>
+                    <Text>
+                      {createdTime
+                        ? new Date(createdTime).toLocaleDateString('en-US', {
+                            year: 'numeric',
+                            month: 'long',
+                            day: 'numeric',
+                          })
+                        : '-'}
+                    </Text>
+                  </Td>
+                </Tr>
+                {createdBy && (
+                  <Tr>
+                    <Td whiteSpace="nowrap">
+                      <Text py="2">Created By</Text>
+                    </Td>
+                    <Td>
+                      <HStack py="2">
+                        <DisplayAvatar
+                          alt={createdBy.profile?.username}
+                          address={createdBy.id}
+                          avatarIPFS={createdBy.profile?.avatar}
+                          size={24}
+                        />
                         <Link
-                          target="_blank"
-                          href={`https://ipfs.everipedia.org/ipfs/${ipfsHash}`}
+                          href={`/account/${createdBy.id}`}
                           color="brandLinkColor"
                         >
-                          <Text>{shortenAccount(ipfsHash || '')}</Text>
+                          {getUsername(createdBy, username)}
                         </Link>
                       </HStack>
                     </Td>
                   </Tr>
-                  <Tr>
-                    <Td>
-                      <HStack spacing={3}>
-                        <Text>TX Hash</Text>
-                      </HStack>
-                    </Td>
-                    <Td display="flex" align="center" gap={3}>
-                      <GoLink />
-                      <Link
-                        target="_blank"
-                        href={`${config.blockExplorerUrl}/tx/${txHash}`}
-                        color="brandLinkColor"
-                      >
-                        <Text>{shortenAccount(txHash || '')}</Text>
-                      </Link>
-                    </Td>
-                  </Tr>
+                )}
+                {views && views > 250 && (
                   <Tr>
                     <Td whiteSpace="nowrap">
-                      <Text py="2">Created</Text>
+                      <Text py="2">Views</Text>
                     </Td>
                     <Td>
-                      <Text>
-                        {createdTime
-                          ? new Date(createdTime).toLocaleDateString('en-US', {
-                              year: 'numeric',
-                              month: 'long',
-                              day: 'numeric',
-                            })
-                          : '-'}
-                      </Text>
+                      <Text py="2">{views}</Text>
                     </Td>
                   </Tr>
-                  {createdBy && (
-                    <Tr>
-                      <Td whiteSpace="nowrap">
-                        <Text py="2">Created By</Text>
-                      </Td>
-                      <Td>
-                        <HStack py="2">
-                          <DisplayAvatar
-                            alt={createdBy.profile?.username}
-                            address={createdBy.id}
-                            avatarIPFS={createdBy.profile?.avatar}
-                            size={24}
-                          />
-                          <Link
-                            href={`/account/${createdBy.id}`}
-                            color="brandLinkColor"
-                          >
-                            {getUsername(createdBy, username)}
-                          </Link>
-                        </HStack>
-                      </Td>
-                    </Tr>
-                  )}
-                  {views && views > 250 && (
-                    <Tr>
-                      <Td whiteSpace="nowrap">
-                        <Text py="2">Views</Text>
-                      </Td>
-                      <Td>
-                        <Text py="2">{views}</Text>
-                      </Td>
-                    </Tr>
-                  )}
-                </Tbody>
-              </Table>
-            </TableContainer>
-          </Center>
+                )}
+              </Tbody>
+            </Table>
+          </TableContainer>
         </VStack>
       </Stack>
     </Box>
