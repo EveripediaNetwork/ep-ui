d8e214a0132e5dc9a41ef879cdb8b02ea4d2de8f
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2023-01-19 18:51:56 +0530

    Fixes image sizes on wikipage (#1061)

--- src/components/Elements/Image/Image.tsx
+++ src/components/Elements/Image/Image.tsx
@@ -4,9 +4,7 @@ import NextImage, { ImageProps } from 'next/image'
 
 export type NextChakraImageProps = Omit<Omit<BoxProps, 'as'>, 'objectFit'> &
   Omit<Omit<ImageProps, 'width'>, 'height'> & {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     imgW?: number
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     imgH?: number
     hideOnError?: boolean
     objectFit?: 'cover' | 'contain' | 'fill' | 'none' | 'scale-down'
@@ -21,6 +19,7 @@ export const Image = ({
   placeholder,
   blurDataURL,
   hideOnError,
+  sizes,
   objectFit = 'cover',
   ...rest
 }: NextChakraImageProps) => (
@@ -35,7 +34,7 @@ export const Image = ({
       quality={95}
       style={{
         objectFit,
-        objectPosition: 'center center !important',
+        objectPosition: 'center',
         width: '100%',
         height: '100%',
       }}
@@ -48,6 +47,7 @@ export const Image = ({
         }
       }}
       priority={priority}
+      sizes={sizes}
       placeholder={placeholder}
       blurDataURL={blurDataURL}
       loading={priority ? 'eager' : 'lazy'}
--- src/components/Wiki/WikiPage/InsightComponents/RelatedMedia.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/RelatedMedia.tsx
@@ -6,36 +6,39 @@ import { constructMediaUrl } from '@/utils/mediaUtils'
 import { Image } from '@/components/Elements/Image/Image'
 import WikiAccordion from '../../WikiAccordion'
 
+const RELATED_MEDIA_IMAGE_BOX_SIZE = 90
+
 const RelatedMediaGrid = ({ media }: { media?: Media[] }) => {
   if (!media || media.length === 0) return null
   return (
     <VStack w="100%" spacing={4} borderRadius={2}>
       <WikiAccordion title="Media">
         <SimpleGrid
-          columns={{ base: 3, sm: 5, md: 6, lg: 8, xl: 3 }}
-          spacing={5}
+          columns={{ base: 3, xl: 3, sm: 4, md: 6, '2xl': 4 }}
+          spacing={3}
         >
           {media.map((m, i) => (
-            <AspectRatio ratio={1} key={i}>
+            <AspectRatio ratio={1}>
               <MediaPreview
                 type={m.source}
                 src={constructMediaUrl(m)}
+                key={i}
                 bgColor="dimColor"
                 borderRadius={4}
-                w="100%"
-                h="100%"
+                boxSize="100%"
                 overflow="hidden"
                 data-caption={m?.caption}
               >
                 <Image
+                  boxSize="100%"
                   src={
                     m.source !== 'YOUTUBE'
                       ? constructMediaUrl(m)
                       : `https://i3.ytimg.com/vi/${m.name}/maxresdefault.jpg`
                   }
-                  imgH={130}
-                  imgW={130}
                   alt="related media"
+                  imgH={RELATED_MEDIA_IMAGE_BOX_SIZE}
+                  imgW={RELATED_MEDIA_IMAGE_BOX_SIZE}
                   hideOnError
                   objectFit="cover"
                   bgColor="fadedText2"
