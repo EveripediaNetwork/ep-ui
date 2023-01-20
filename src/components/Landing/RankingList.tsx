30fcbc90d27cbfb64f81316193509302cda0b72a
Adebesin Cell <adebesintolulope80@gmail.com>
2023-01-20 17:54:28 +0100

    Refactored common error logic

--- src/components/Landing/RankingList.tsx
+++ src/components/Landing/RankingList.tsx
@@ -73,6 +73,48 @@ const getFounderName = (text: string) => {
   return `${names[0]} ${names[1][0]}.`
 }
 
+const RankingItemNotAvailable = ({ index }: { index: number }) => {
+  return (
+    <Tr>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        <Text color="rankingListText">{index + 1}</Text>
+      </Td>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        <Flex gap="2.5" alignItems="center">
+          <Box flexShrink="0" w="40px" h="40px">
+            <Icon as={BiImage} w="full" h="full" color="gray.500" />
+          </Box>
+          <Box>Coming Soon</Box>
+        </Flex>
+      </Td>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        <Text color="rankingListText">Coming Soon</Text>
+      </Td>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        <Flex gap="1">
+          <Text color="rankingListText">NA</Text>
+        </Flex>
+      </Td>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        <Flex gap="1">
+          <Text color="rankingListText">Coming Soon</Text>
+        </Flex>
+      </Td>
+      <Td borderColor="rankingListBorder" fontWeight={500} fontSize="14px">
+        NA
+      </Td>
+      <Td
+        borderColor="rankingListBorder"
+        fontWeight={500}
+        color="rankingListText"
+        fontSize="14px"
+      >
+        Coming soon
+      </Td>
+    </Tr>
+  )
+}
+
 const RankingList = ({ rankings }: RankingListProps) => {
   const { t } = useTranslation()
 
@@ -137,74 +179,7 @@ const RankingList = ({ rankings }: RankingListProps) => {
                   <Tbody>
                     {rankings.TokensListing.map((token, index) => {
                       if (!token) {
-                        return (
-                          <Tr>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              <Text color="rankingListText">{index + 1}</Text>
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              <Flex gap="2.5" alignItems="center">
-                                <Box flexShrink="0" w="40px" h="40px">
-                                  <Icon
-                                    as={BiImage}
-                                    w="full"
-                                    h="full"
-                                    color="gray.500"
-                                  />
-                                </Box>
-                                <Box>Coming Soon</Box>
-                              </Flex>
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              <Text color="rankingListText">Coming Soon</Text>
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              <Flex gap="1">
-                                <Text color="rankingListText">NA</Text>
-                              </Flex>
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              <Flex gap="1">
-                                <Text color="rankingListText">Coming Soon</Text>
-                              </Flex>
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              fontSize="14px"
-                            >
-                              NA
-                            </Td>
-                            <Td
-                              borderColor="rankingListBorder"
-                              fontWeight={500}
-                              color="rankingListText"
-                              fontSize="14px"
-                            >
-                              Coming soon
-                            </Td>
-                          </Tr>
-                        )
+                        return <RankingItemNotAvailable index={index} />
                       }
                       return (
                         <Tr>
@@ -471,74 +446,7 @@ const RankingList = ({ rankings }: RankingListProps) => {
                           </Tr>
                         )
                       }
-                      return (
-                        <Tr>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            <Text color="rankingListText">{index + 1}</Text>
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            <Flex gap="2.5" alignItems="center">
-                              <Box flexShrink="0" w="40px" h="40px">
-                                <Icon
-                                  as={BiImage}
-                                  w="full"
-                                  h="full"
-                                  color="gray.500"
-                                />
-                              </Box>
-                              <Box>Coming Soon</Box>
-                            </Flex>
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            <Text color="rankingListText">Coming Soon</Text>
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            <Flex gap="1">
-                              <Text color="rankingListText">NA</Text>
-                            </Flex>
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            <Flex gap="1">
-                              <Text color="rankingListText">Coming Soon</Text>
-                            </Flex>
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            fontSize="14px"
-                          >
-                            NA
-                          </Td>
-                          <Td
-                            borderColor="rankingListBorder"
-                            fontWeight={500}
-                            color="rankingListText"
-                            fontSize="14px"
-                          >
-                            Coming soon
-                          </Td>
-                        </Tr>
-                      )
+                      return <RankingItemNotAvailable index={index} />
                     })}
                   </Tbody>
                 </Table>
