e4d62ecdf757b1ae4cd5f84febd213034a350fd5
Adebesin Cell <adebesintolulope80@gmail.com>
2023-01-26 19:28:45 +0100

    fixes conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Box, Flex, Heading, chakra, Text } from '@chakra-ui/react'
-import { getPromotedWikis, wikiApi } from '@/services/wikis'
+import { getPromotedWikis, getWikis, wikiApi } from '@/services/wikis'
 import { store } from '@/store/store'
 import { Wiki } from '@everipedia/iq-utils'
 import Hero from '@/components/Landing/Hero'
@@ -100,7 +100,7 @@ export const Index = ({
       >
         <RankingList rankings={rankings} />
         <TrendingWikis
-          drops={promotedWikis && promotedWikis.slice(1)}
+          drops={promotedWikis && promotedWikis.slice(0, 4)}
           recent={recentWikis && recentWikis.slice(0, 4)}
           featuredWikis={promotedWikis && promotedWikis}
         />
@@ -115,6 +115,10 @@ export const Index = ({
 export async function getStaticProps() {
   const { data: promotedWikis, error: promotedWikisError } =
     await store.dispatch(getPromotedWikis.initiate())
+  const { data: recent, error: recentError } = await store.dispatch(
+    getWikis.initiate(),
+  )
+
   const { data: categories, error: categoriesError } = await store.dispatch(
     getCategories.initiate(),
   )
@@ -152,12 +156,13 @@ export async function getStaticProps() {
     store.dispatch(rankingAPI.util.getRunningQueriesThunk()),
   ])
 
-  if (promotedWikisError || categoriesError || tagsDataError) {
+  if (promotedWikisError || categoriesError || tagsDataError || recentError) {
     throw new Error(
       `Error fetching data. the error is: ${
         (JSON.stringify(tagsDataError?.message),
         JSON.stringify(categoriesError?.message),
-        JSON.stringify(promotedWikisError?.message))
+        JSON.stringify(promotedWikisError?.message),
+        JSON.stringify(recentError?.message))
       }`,
     )
   }
@@ -177,6 +182,7 @@ export async function getStaticProps() {
   return {
     props: {
       promotedWikis: sortedPromotedWikis || [],
+      recentWikis: recent || [],
       categories: categories || [],
       popularTags: tagsData || [],
       leaderboards: sortedleaderboards || [],
