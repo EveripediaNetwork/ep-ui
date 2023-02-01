b985191ff3a745af5ff06f588b271fbd2f29fe43
Adebesin Cell <adebesintolulope80@gmail.com>
2023-02-01 21:44:18 +0100

    Updated span css

--- src/components/Elements/LinkElements/LinkWrapper.tsx
+++ src/components/Elements/LinkElements/LinkWrapper.tsx
@@ -1,27 +1,27 @@
 import React from 'react'
-import { LinkProps as ChakraLinkProps } from '@chakra-ui/react'
-import NextLink, { LinkProps } from 'next/link'
+import { LinkProps as ChakraLinkProps, Link } from '@chakra-ui/react'
+import { LinkProps } from 'next/link'
 
 type ChakraLinkAndNextProps = ChakraLinkProps & LinkProps
 
 export const LinkWrapper = ({
   href,
-  prefetch,
+  // prefetch,
   children,
 }: ChakraLinkAndNextProps) => {
   return (
-    <NextLink
+    <Link
       href={href}
-      prefetch={
-        String(href).includes('/create-wiki') ||
-        String(href).includes('/static/about')
-          ? false
-          : prefetch
-      }
-      passHref
-      legacyBehavior
+      // prefetch={
+      //   String(href).includes('/create-wiki') ||
+      //   String(href).includes('/static/about')
+      //     ? false
+      //     : prefetch
+      // }
+      // passHref
+      // legacyBehavior
     >
       {children}
-    </NextLink>
+    </Link>
   )
 }
