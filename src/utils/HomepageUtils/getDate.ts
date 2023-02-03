3d8e6fc69e090b145ba64790b552abf8027cf7b5
César Rodríguez <kesarr@gmail.com>
2023-02-03 21:03:57 +0100

    Apply suggestions from code review

--- src/utils/HomepageUtils/getDate.ts
+++ src/utils/HomepageUtils/getDate.ts
@@ -1,7 +1,7 @@
 export const getDateRange = (dayRange = 1) => {
-  const startOfDay = new Date().setUTCHours(0, 0, 0, 0)
+  const startOfDay = new Date()
   const endOfDay =
-    new Date().setUTCHours(23, 59, 59, 999) + dayRange * 24 * 60 * 60 * 1000
+    new Date() - dayRange * 24 * 60 * 60 * 1000
 
   const startDay = new Date(startOfDay).toLocaleDateString('en-US', {
     year: 'numeric',
