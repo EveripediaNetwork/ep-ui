9e2fee28a30401cd0a4f68f89917cfa49ea9068a
César Rodríguez <kesarr@gmail.com>
2023-02-03 21:09:29 +0100

    Update getDate.ts

--- src/utils/HomepageUtils/getDate.ts
+++ src/utils/HomepageUtils/getDate.ts
@@ -1,15 +1,14 @@
 export const getDateRange = (dayRange = 1) => {
-  const startOfDay = new Date() - dayRange * 24 * 60 * 60 * 1000
-  const endOfDay =
-    new Date()
+  const yday = new Date(new Date().getTime() - (dayRange * 24 * 60 * 60 * 1000))
+  const today = new Date()
 
-  const startDay = new Date(startOfDay).toLocaleDateString('en-US', {
+  const startDay = new Date(yday).toLocaleDateString('en-US', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
   })
 
-  const endDay = new Date(endOfDay).toLocaleDateString('en-US', {
+  const endDay = new Date(today).toLocaleDateString('en-US', {
     year: 'numeric',
     month: '2-digit',
     day: '2-digit',
