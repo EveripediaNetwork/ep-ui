0cf83fba37e1abcfedebc14d5332e4f3ea5855c9
Srujan Gurram <srujangs8@gmail.com>
2023-02-06 13:08:56 +0530

    refactored isValidWiki to reduce congitive complexity

--- src/utils/CreateWikiUtils/isValidWiki.ts
+++ src/utils/CreateWikiUtils/isValidWiki.ts
@@ -4,78 +4,52 @@ import { MEDIA_POST_DEFAULT_ID, WIKI_SUMMARY_LIMIT } from '@/data/Constants'
 import { getWordCount } from '../DataTransform/getWordCount'
 import { MINIMUM_WORDS, isVerifiedContentLinks } from './createWiki'
 
+const isMediaUploading = (wiki: Wiki) =>
+  !wiki.media?.every(m => !m.id.endsWith(MEDIA_POST_DEFAULT_ID))
+
+const isSummaryExceedsLimit = (wiki: Wiki) =>
+  !!(wiki.summary && wiki.summary.length > WIKI_SUMMARY_LIMIT)
+
+const checkErrors = (
+  toast: CreateToastFnReturn,
+  conditionErrorPair: [() => boolean, string][],
+) => {
+  let errorPresence = false
+  conditionErrorPair.forEach(([condition, errorText]) => {
+    if (condition()) {
+      toast({
+        title: errorText,
+        status: 'error',
+        duration: 3000,
+      })
+      errorPresence = true
+    }
+  })
+  return errorPresence
+}
+
+const NO_WIKI_TITLE_ERROR = 'Add a Title at the top for this Wiki to continue'
+const TITLE_EXCEEDS_LIMIT_ERROR = `Title should be less than 60 characters`
+const NO_WIKI_CONTENT_ERROR = 'Add a Content section to continue'
+const SUMMARY_EXCEEDS_LIMIT_ERROR = `Summary exceeds maximum limit of ${WIKI_SUMMARY_LIMIT}`
+const NO_WIKI_IMAGES_ERROR = 'Add a main image on the right column to continue'
+const NO_WIKI_CATEGORIES_ERROR = 'Add one category to continue'
+const MEDIA_UPLOADING_ERROR = 'Some of media are still uploading, please wait'
+const EXTERNAL_LINKS_ERROR = 'Please remove all external links from the content'
+const CONTENT_WORDS_ERROR = (words: number) =>
+  `Add a minimum of ${MINIMUM_WORDS} words in the content section to continue. you have written ${words}`
+
 export const isValidWiki = (toast: CreateToastFnReturn, wiki: Wiki) => {
-  if (!wiki.title) {
-    toast({
-      title: `Add a Title at the top for this Wiki to continue `,
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (wiki.title.length > 60) {
-    toast({
-      title: `Title should be less than 60 characters`,
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
   const words = getWordCount(wiki.content || '')
-
-  if (words < MINIMUM_WORDS) {
-    toast({
-      title: `Add a minimum of ${MINIMUM_WORDS} words in the content section to continue, you have written ${words}`,
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (!isVerifiedContentLinks(wiki.content)) {
-    toast({
-      title: 'Please remove all external links from the content',
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (!wiki.images?.length) {
-    toast({
-      title: 'Add a main image on the right column to continue',
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (wiki.categories.length === 0) {
-    toast({
-      title: 'Add one category to continue',
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (!wiki.media?.every(m => !m.id.endsWith(MEDIA_POST_DEFAULT_ID))) {
-    toast({
-      title: 'Some of media are still uploading, please wait',
-      status: 'error',
-      duration: 3000,
-    })
-    return false
-  }
-
-  if (wiki.summary && wiki.summary.length > WIKI_SUMMARY_LIMIT) {
-    toast({
-      title: `Summary exceeds maximum limit of ${WIKI_SUMMARY_LIMIT}`,
-      status: 'error',
-      duration: 3000,
-    })
-  }
-  return true
+  return checkErrors(toast, [
+    [() => !wiki.title, NO_WIKI_TITLE_ERROR],
+    [() => wiki.title.length > 60, TITLE_EXCEEDS_LIMIT_ERROR],
+    [() => !wiki.content, NO_WIKI_CONTENT_ERROR],
+    [() => words < MINIMUM_WORDS, CONTENT_WORDS_ERROR(words)],
+    [() => !isVerifiedContentLinks(wiki.content), EXTERNAL_LINKS_ERROR],
+    [() => !wiki.images?.length, NO_WIKI_IMAGES_ERROR],
+    [() => wiki.categories.length === 0, NO_WIKI_CATEGORIES_ERROR],
+    [() => isSummaryExceedsLimit(wiki), SUMMARY_EXCEEDS_LIMIT_ERROR],
+    [() => isMediaUploading(wiki), MEDIA_UPLOADING_ERROR],
+  ])
 }
