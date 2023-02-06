9cfb1ae30cc438326f91d538c4cb330fbb5a7570
Srujan Gurram <srujangs8@gmail.com>
2023-02-06 15:29:26 +0530

    craetes seperate component for wiki title

---
+++ src/components/Editor/CreateWikiTopBar/index.tsx
@@ -0,0 +1,27 @@
+import { HStack } from '@chakra-ui/react'
+import React from 'react'
+import { WikiTitleInput } from './WikiTitleInput'
+import WikiScoreIndicator from '../WikiScoreIndicator'
+import { WikiPublishButton } from './WikiPublishButton'
+
+export const CreateWikiTopBar = () => {
+  return (
+    <HStack
+      boxShadow="sm"
+      borderRadius={4}
+      borderWidth="1px"
+      p={3}
+      justifyContent="space-between"
+      mx="auto"
+      mb={3}
+      mt={2}
+      w="96%"
+    >
+      <WikiTitleInput />
+      <HStack gap={5}>
+        <WikiScoreIndicator />
+        <WikiPublishButton />
+      </HStack>
+    </HStack>
+  )
+}
