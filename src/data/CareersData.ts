790dfa411da336181804ef954391ffac4d6fca42
Adebesin Cell <adebesintolulope80@gmail.com>
2023-02-07 20:36:45 +0100

    fixing line jump

--- src/data/CareersData.ts
+++ src/data/CareersData.ts
@@ -8,9 +8,7 @@ export type CareersDatatype = {
 export const AllCareers: CareersDatatype[] = [
   {
     title: 'AI developer',
-    description: `Are you an AI Developer with an insatiable appetite for a challenge? Then this job opportunity is for you! We are looking for an ambitious and creative AI Developer to join our team and help solve some of the most complex problems in the tech industry.
-
-    You will have the opportunity to work with the most cutting edge technologies and develop innovative solutions that have the potential to make a real impact on the world.If you’re looking to take your AI development career to the next level, then this is the job for you! So, c’mon – let’s get coding and make some magic!`,
+    description: `Are you an AI Developer with an insatiable appetite for a challenge? Then this job opportunity is for you! We are looking for an ambitious and creative AI Developer to join our team and help solve some of the most complex problems in the tech industry. \n\nYou will have the opportunity to work with the most cutting edge technologies and develop innovative solutions that have the potential to make a real impact on the world.If you’re looking to take your AI development career to the next level, then this is the job for you! So, c’mon – let’s get coding and make some magic!`,
     link: 'https://forms.gle/EYYt4JudkVtnxZgE7',
     location: 'Remote',
   },
