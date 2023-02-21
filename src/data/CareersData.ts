c1c43e5b514570a2d5015a4cdf02ed75438f9a96
Adebesin Cell <adebesintolulope80@gmail.com>
2023-02-21 21:49:27 +0100

    added job roles

--- src/data/CareersData.ts
+++ src/data/CareersData.ts
@@ -12,4 +12,16 @@ export const AllCareers: CareersDatatype[] = [
     link: 'https://forms.gle/EYYt4JudkVtnxZgE7',
     location: 'Remote',
   },
+  {
+    title: 'Frontend developer',
+    description: `Are you tired of working at a boring job that's sucking the life out of you? Do you want to work in a place where you can let your creativity run wild? If you answered "yes" to these questions, then we have the perfect job for you! \n\nWe are BrainDAO, and we're looking for a frontend developer to join our team. But we don't want just any ordinary developer, we want someone who's passionate, quirky, and isn't afraid to wear a tutu to work (we promise not to judge). \n\nAt BrainDAO, we work with the latest and greatest technologies, like chakraUI, nextJS, and redux. But don't worry if you're not familiar with these tools yet, we'll teach you everything you need to know (except how to ride a unicycle, you'll have to learn that on your own).\n\nWe want someone who's not afraid to speak their mind, and who's always up for a good laugh (or a bad pun). Bonus points if you can juggle while coding (just don't drop any balls on the CEO's head). \n\nSo if you're ready to join a team that's as fun as it is talented, then send us your application today! We can't wait to see what kind of weird and wonderful ideas you'll bring to the table.`,
+    location: 'Remote',
+    link: 'https://forms.gle/EYYt4JudkVtnxZgE7',
+  },
+  {
+    title: 'Backend developer',
+    description: `Are you a backend developer who loves to code in their pajamas? Do you dream in SQL queries and have a secret love affair with databases? Then we have the job for you! \n\nAt BrainDAO, we're looking for a backend developer to join our team (and to help us figure out why the coffee machine keeps spitting out code instead of caffeine). We use the latest and greatest technologies like nestJS, graphql, and PostgreSQL. But don't worry if you don't know these technologies yet, we'll teach you everything you need to know (except how to fix the coffee machine, that's a lost cause). \n\nWe're looking for someone who's passionate about backend development and isn't afraid to get their hands dirty (metaphorically speaking, we have a strict "no mud wrestling" policy in the office). You should be able to write clean, efficient code, and be a master at debugging (because let's face it, bugs are just as inevitable as bad hair days).\n\nSo, if you're ready to join a team of quirky, fun-loving developers who are as talented as they are hilarious, then send us your application today! We can't wait to see what kind of backend magic you'll bring to the table.`,
+    location: 'Remote',
+    link: 'https://forms.gle/EYYt4JudkVtnxZgE7',
+  },
 ]
