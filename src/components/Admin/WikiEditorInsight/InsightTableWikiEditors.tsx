84ec4aa0f21b6c1d137cc2ee04f7b6304a8b503f
Adebesin Cell <adebesintolulope80@gmail.com>
2023-02-24 21:17:18 +0100

    added filter

--- src/components/Admin/WikiEditorInsight/InsightTableWikiEditors.tsx
+++ src/components/Admin/WikiEditorInsight/InsightTableWikiEditors.tsx
@@ -56,133 +56,140 @@ export const InsightTableWikiEditors = (
           </Tr>
         </Thead>
         <Tbody>
-          {wikiEditorInsightData?.map((item, i) => {
-            return (
-              <Tr key={i}>
-                <Td>
-                  <Link href={`/account/${item.id}`} py={1}>
-                    <Flex align="center" gap={2}>
-                      <Avatar
-                        boxSize="40px"
-                        name={userNameData(item)}
-                        src={item?.profile?.avatar ? item?.profile?.avatar : ''}
-                      />
-                      <Flex flexDirection="column">
-                        <Text opacity={item.active ? 1 : 0.3}>
-                          {userNameData(item)}
-                        </Text>
-                        <Text
-                          color="#718096"
-                          fontSize="sm"
-                          opacity={item.active ? 1 : 0.3}
-                        >
-                          {shortenAccount(item.id)}
+          {wikiEditorInsightData
+            ?.filter(
+              editor =>
+                editor.wikisCreated.length > 0 || editor.wikisEdited.length > 0,
+            )
+            .map((item, i) => {
+              return (
+                <Tr key={i}>
+                  <Td>
+                    <Link href={`/account/${item.id}`} py={1}>
+                      <Flex align="center" gap={2}>
+                        <Avatar
+                          boxSize="40px"
+                          name={userNameData(item)}
+                          src={
+                            item?.profile?.avatar ? item?.profile?.avatar : ''
+                          }
+                        />
+                        <Flex flexDirection="column">
+                          <Text opacity={item.active ? 1 : 0.3}>
+                            {userNameData(item)}
+                          </Text>
+                          <Text
+                            color="#718096"
+                            fontSize="sm"
+                            opacity={item.active ? 1 : 0.3}
+                          >
+                            {shortenAccount(item.id)}
+                          </Text>
+                        </Flex>
+                      </Flex>
+                    </Link>
+                  </Td>
+                  <Td opacity={item.active ? 1 : 0.3}>
+                    <Text color="#718096">{item.wikisCreated.length}</Text>
+                  </Td>
+                  <Td opacity={item.active ? 1 : 0.3}>
+                    <Text color="#718096">{item.wikisEdited.length}</Text>
+                  </Td>
+                  <Td opacity={item.active ? 1 : 0.3}>
+                    <Text color="#718096">
+                      {item.wikisCreated.length + item.wikisEdited.length}
+                    </Text>
+                  </Td>
+                  <Td>
+                    <Link href={`/wiki/${item.wikisEdited[0]?.wikiId}`} py={1}>
+                      <Flex flexDir="row" align="center" gap={2}>
+                        <AspectRatio ratio={WIKI_IMAGE_ASPECT_RATIO} w="40px">
+                          <WikiImage
+                            cursor="pointer"
+                            flexShrink={0}
+                            alt="wiki"
+                            imageURL={`${config.pinataBaseUrl}${
+                              item.wikisEdited[0]?.content
+                                ? item.wikisEdited[0]?.content[0].images[0].id
+                                : ''
+                            }`}
+                          />
+                        </AspectRatio>
+                        <Text opacity={item.active ? 1 : 0.1}>
+                          {item.wikisEdited[0]?.content[0]
+                            ? shortenText(
+                                item.wikisEdited[0]?.content[0].title,
+                                18,
+                              )
+                            : ''}
                         </Text>
                       </Flex>
-                    </Flex>
-                  </Link>
-                </Td>
-                <Td opacity={item.active ? 1 : 0.3}>
-                  <Text color="#718096">{item.wikisCreated.length}</Text>
-                </Td>
-                <Td opacity={item.active ? 1 : 0.3}>
-                  <Text color="#718096">{item.wikisEdited.length}</Text>
-                </Td>
-                <Td opacity={item.active ? 1 : 0.3}>
-                  <Text color="#718096">
-                    {item.wikisCreated.length + item.wikisEdited.length}
-                  </Text>
-                </Td>
-                <Td>
-                  <Link href={`/wiki/${item.wikisEdited[0]?.wikiId}`} py={1}>
-                    <Flex flexDir="row" align="center" gap={2}>
-                      <AspectRatio ratio={WIKI_IMAGE_ASPECT_RATIO} w="40px">
-                        <WikiImage
-                          cursor="pointer"
-                          flexShrink={0}
-                          alt="wiki"
-                          imageURL={`${config.pinataBaseUrl}${
-                            item.wikisEdited[0]?.content
-                              ? item.wikisEdited[0]?.content[0].images[0].id
-                              : ''
-                          }`}
-                        />
-                      </AspectRatio>
-                      <Text opacity={item.active ? 1 : 0.1}>
-                        {item.wikisEdited[0]?.content[0]
-                          ? shortenText(
-                              item.wikisEdited[0]?.content[0].title,
-                              18,
-                            )
-                          : ''}
-                      </Text>
-                    </Flex>
-                  </Link>
-                </Td>
-                <Td color="#718096">
-                  {item?.wikisCreated[0]?.datetime.split('T')[0]}
-                </Td>
-                <Td>
-                  <Tag
-                    bg={item.active ? '#F0FFF4' : '#FBD38D'}
-                    display="flex"
-                    w="fit-content"
-                    gap="2"
-                    py="1"
-                    borderRadius="100px"
-                    color={item.active ? '#276749' : '#9C4221'}
-                  >
-                    <Box
-                      w="8px"
-                      h="8px"
-                      bg={item.active ? '#38A169' : '#DD6B20'}
+                    </Link>
+                  </Td>
+                  <Td color="#718096">
+                    {item?.wikisCreated[0]?.datetime.split('T')[0]}
+                  </Td>
+                  <Td>
+                    <Tag
+                      bg={item.active ? '#F0FFF4' : '#FBD38D'}
+                      display="flex"
+                      w="fit-content"
+                      gap="2"
+                      py="1"
                       borderRadius="100px"
-                    />
-                    {item.active ? 'Active' : 'Banned'}
-                  </Tag>
-                </Td>
-                <Td _dark={{ opacity: item.active ? 1 : 0.5 }}>
-                  {item.active ? (
-                    <Text
-                      cursor="pointer"
-                      fontWeight="medium"
-                      onClick={() => {
-                        onOpen()
-                        if (toggleUserFunc) {
-                          toggleUserFunc(item.active, item.id)
-                        }
-                      }}
+                      color={item.active ? '#276749' : '#9C4221'}
                     >
-                      Ban
-                    </Text>
-                  ) : (
-                    <HStack
-                      spacing={2}
-                      onClick={() => {
-                        if (toggleUserFunc) {
-                          toggleUserFunc(item.active, item.id)
-                        }
-                      }}
-                    >
-                      <Text
-                        color="#E2E8F0"
-                        _dark={{ color: '#495a68' }}
-                        cursor="pointer"
-                      >
-                        UnBan
-                      </Text>
-                      <Icon
-                        fontSize="20px"
-                        cursor="pointer"
-                        color="#F11a82"
-                        as={RiQuestionLine}
+                      <Box
+                        w="8px"
+                        h="8px"
+                        bg={item.active ? '#38A169' : '#DD6B20'}
+                        borderRadius="100px"
                       />
-                    </HStack>
-                  )}
-                </Td>
-              </Tr>
-            )
-          })}
+                      {item.active ? 'Active' : 'Banned'}
+                    </Tag>
+                  </Td>
+                  <Td _dark={{ opacity: item.active ? 1 : 0.5 }}>
+                    {item.active ? (
+                      <Text
+                        cursor="pointer"
+                        fontWeight="medium"
+                        onClick={() => {
+                          onOpen()
+                          if (toggleUserFunc) {
+                            toggleUserFunc(item.active, item.id)
+                          }
+                        }}
+                      >
+                        Ban
+                      </Text>
+                    ) : (
+                      <HStack
+                        spacing={2}
+                        onClick={() => {
+                          if (toggleUserFunc) {
+                            toggleUserFunc(item.active, item.id)
+                          }
+                        }}
+                      >
+                        <Text
+                          color="#E2E8F0"
+                          _dark={{ color: '#495a68' }}
+                          cursor="pointer"
+                        >
+                          UnBan
+                        </Text>
+                        <Icon
+                          fontSize="20px"
+                          cursor="pointer"
+                          color="#F11a82"
+                          as={RiQuestionLine}
+                        />
+                      </HStack>
+                    )}
+                  </Td>
+                </Tr>
+              )
+            })}
         </Tbody>
       </Table>
     </TableContainer>
