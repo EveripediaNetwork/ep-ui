578d87a47bdd1c3caa41949fb1809ab7c3ca74b5
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-02 18:53:49 +0100

    fixes span element into p

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -85,16 +85,20 @@ const Footer = () => {
               <Text py={3} pr={2}>
                 {' '}
                 © {thisYear} {`${t('copyRight')}`}
-              </Text>
-              <Flex>
                 <Link target="_blank" href="https://braindao.org/">
-                  <Text _hover={{ textDecoration: 'underline' }}>BrainDAO</Text>
+                  <Text as="span" _hover={{ textDecoration: 'underline' }}>
+                    BrainDAO
+                  </Text>
                 </Link>
-                <Text px="1">& </Text>
+                <Text as="span" px="1">
+                  &{' '}
+                </Text>
                 <Link target="_blank" href="https://iq.braindao.org/">
-                  <Text _hover={{ textDecoration: 'underline' }}>IQ </Text>
+                  <Text as="span" _hover={{ textDecoration: 'underline' }}>
+                    IQ{' '}
+                  </Text>
                 </Link>
-              </Flex>
+              </Text>
             </Flex>
           </Stack>
           <Stack mt={[4, 0]} align={{ base: 'center', lg: 'flex-end' }}>
