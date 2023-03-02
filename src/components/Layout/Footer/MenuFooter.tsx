5aeeadad8f067979d48ea4932824af0aa00ed627
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-02 14:00:13 +0100

    added link

--- src/components/Layout/Footer/MenuFooter.tsx
+++ src/components/Layout/Footer/MenuFooter.tsx
@@ -34,7 +34,12 @@ const MenuFooter = () => {
             px={{ base: 0, lg: 0 }}
             pb={{ base: '6', md: '0' }}
           >
-            {`${t('visionFooterText')}`}
+            IQ.wiki&apos;s vision is to bring blockchain knowledge to the world
+            and knowledge onto the blockchain. A part of{' '}
+            <Link href="https://www.brainfund.com/" target="_blank">
+              Brainfund
+            </Link>{' '}
+            Group.
           </Text>
         </Stack>
       </GridItem>
