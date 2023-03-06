ff071ba62404b2d1eb6bc45c41913feb8b7e8d89
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-06 18:16:14 +0100

    added UI feedback to first event removal

--- src/components/CreateWiki/EditorModals/EditWikiDetailsModal/EventsInput/EventsList.tsx
+++ src/components/CreateWiki/EditorModals/EditWikiDetailsModal/EventsInput/EventsList.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Center, Flex, Icon, Text } from '@chakra-ui/react'
 import { useAppDispatch, useAppSelector } from '@/store/hook'
 import { FiCalendar } from 'react-icons/fi'
@@ -12,8 +12,16 @@ export const EventsList = ({
 }) => {
   const wiki = useAppSelector(state => state.wiki)
   const dispatch = useAppDispatch()
+  const [feedbackMessage, setFeedbackMessage] = useState<string>('')
 
   const removeEventHandler = (date: string) => {
+    if (wiki.events && wiki.events.length > 1 && date === wiki.events[0].date) {
+      setFeedbackMessage('The first event cannot be removed.')
+      return
+    }
+
+    setFeedbackMessage('')
+
     dispatch({
       type: 'wiki/removeEvent',
       payload: {
@@ -25,80 +33,87 @@ export const EventsList = ({
   if (!wiki.events?.length) return <></>
 
   return (
-    <Flex
-      border="1px solid"
-      borderRadius="6px"
-      py="3"
-      px="2.5"
-      gap="2.5"
-      flexWrap="wrap"
-      borderColor="gray.200"
-      _dark={{
-        borderColor: 'whiteAlpha.200',
-      }}
-    >
-      {wiki.events?.map(wikiEvent => (
-        <Flex key={wikiEvent.date} gap="2">
-          <Flex alignItems="center" gap="1" flexShrink={0}>
-            <Icon as={FiCalendar} w="16px" h="16px" />
-          </Flex>
-          <Button
-            onClick={() => {
-              handleFormChange(wikiEvent)
-            }}
-            w="max-content"
-            display="flex"
-            gap={2}
-            bg="gray.100"
-            color="black"
-            _active={{
-              bg: 'gray.200',
-            }}
-            _hover={{
-              bg: 'gray.100',
-            }}
-            _dark={{
-              color: 'white',
-              bg: 'whiteAlpha.100',
-            }}
-            rounded="md"
-            size="xs"
-            px={2}
-          >
-            <Text
-              fontWeight="normal"
-              fontSize="xs"
-              noOfLines={1}
-              display="inline-block"
-              overflowX="hidden"
-              textOverflow="ellipsis"
-              whiteSpace="nowrap"
-              maxWidth="150px"
-            >
-              {wikiEvent.title}
-            </Text>
-            <Center
-              flexShrink={0}
-              boxSize={4}
-              fontSize="xs"
-              fontWeight="bold"
-              lineHeight="none"
-              color="red.100"
-              bg="red.400"
+    <>
+      <Flex
+        border="1px solid"
+        borderRadius="6px"
+        py="3"
+        px="2.5"
+        gap="2.5"
+        flexWrap="wrap"
+        borderColor="gray.200"
+        _dark={{
+          borderColor: 'whiteAlpha.200',
+        }}
+      >
+        {wiki.events?.map(wikiEvent => (
+          <Flex key={wikiEvent.date} gap="2">
+            <Flex alignItems="center" gap="1" flexShrink={0}>
+              <Icon as={FiCalendar} w="16px" h="16px" />
+            </Flex>
+            <Button
+              onClick={() => {
+                handleFormChange(wikiEvent)
+              }}
+              w="max-content"
+              display="flex"
+              gap={2}
+              bg="gray.100"
+              color="black"
+              _active={{
+                bg: 'gray.200',
+              }}
               _hover={{
-                bg: 'red.500',
+                bg: 'gray.100',
               }}
-              rounded="full"
-              onClick={e => {
-                e.stopPropagation()
-                removeEventHandler(wikiEvent.date)
+              _dark={{
+                color: 'white',
+                bg: 'whiteAlpha.100',
               }}
+              rounded="md"
+              size="xs"
+              px={2}
             >
-              <Icon as={RiCloseLine} />
-            </Center>
-          </Button>
-        </Flex>
-      ))}
-    </Flex>
+              <Text
+                fontWeight="normal"
+                fontSize="xs"
+                noOfLines={1}
+                display="inline-block"
+                overflowX="hidden"
+                textOverflow="ellipsis"
+                whiteSpace="nowrap"
+                maxWidth="150px"
+              >
+                {wikiEvent.title}
+              </Text>
+              <Center
+                flexShrink={0}
+                boxSize={4}
+                fontSize="xs"
+                fontWeight="bold"
+                lineHeight="none"
+                color="red.100"
+                bg="red.400"
+                _hover={{
+                  bg: 'red.500',
+                }}
+                rounded="full"
+                onClick={e => {
+                  e.stopPropagation()
+                  removeEventHandler(wikiEvent.date)
+                }}
+              >
+                <Icon as={RiCloseLine} />
+              </Center>
+            </Button>
+          </Flex>
+        ))}
+      </Flex>
+      {feedbackMessage && (
+        <Text color="red.500" fontSize="sm" mt="2">
+          {feedbackMessage}
+        </Text>
+      )}
+    </>
   )
 }
