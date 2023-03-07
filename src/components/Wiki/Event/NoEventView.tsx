fdd9ca1d7bc8f218f835a56fce60b5785d03b207
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-07 13:51:42 +0100

    fixes no events view

--- src/components/Wiki/Event/NoEventView.tsx
+++ src/components/Wiki/Event/NoEventView.tsx
@@ -1,5 +1,7 @@
 import { LinkButton } from '@/components/Elements'
-import { Box, Flex, Text } from '@chakra-ui/react'
+import { Image } from '@/components/Elements/Image/Image'
+import { WIKI_IMAGE_ASPECT_RATIO } from '@/data/Constants'
+import { AspectRatio, Box, Flex, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const NoEventView = () => {
@@ -9,13 +11,29 @@ const NoEventView = () => {
       align="center"
       justify="center"
       direction="column"
-      py={{ base: '12', lg: '20' }}
+      pb={{ base: '10', lg: '16' }}
     >
       <Box textAlign="center" pb={8}>
+        <Flex justifyContent="center">
+          <AspectRatio
+            ratio={WIKI_IMAGE_ASPECT_RATIO}
+            w={{ base: '399px', md: '500px' }}
+            h={{ base: '399px', md: '500px' }}
+          >
+            <Image
+              imgBoxSize={300}
+              alt="No Events "
+              src="/images/GIFs/event-timeline.png"
+              w="full"
+              objectFit="contain"
+            />
+          </AspectRatio>
+        </Flex>
         <Text
           fontWeight={500}
-          fontSize={{ base: '20px', md: '24px', xl: '30px' }}
+          fontSize={{ base: '16px', md: '18px' }}
           color="linkColor"
+          mt="2"
         >
           There are no records of events created for this wiki yet.
         </Text>
