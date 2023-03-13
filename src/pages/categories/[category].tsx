fecc4df3374eb356af769b57be3b28246c824ecd
Adebesin Cell <adebesintolulope80@gmail.com>
2023-03-13 18:05:39 +0100

    added a condition to show trending/newWikis

--- src/pages/categories/[category].tsx
+++ src/pages/categories/[category].tsx
@@ -97,11 +97,14 @@ const CategoryPage = ({
           description={categoryData?.description}
           title={categoryData?.title}
         />
-        <TrendingCategoriesWiki
-          categoryType={categoryData?.title}
-          trending={trending}
-          newWikis={newWikis}
-        />
+        {trending.length > 0 ||
+          (newWikis.length > 0 && (
+            <TrendingCategoriesWiki
+              categoryType={categoryData?.title}
+              trending={trending}
+              newWikis={newWikis}
+            />
+          ))}
         <Divider
           opacity="1"
           borderColor="gray.300"
