e25667336fc84dd8ce1d8184d6d7572cf47e8efb
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-04-18 12:16:24 +0100

    update: formatting number with commas

--- src/components/Layout/WalletDrawer/Connectors.tsx
+++ src/components/Layout/WalletDrawer/Connectors.tsx
@@ -92,7 +92,7 @@ const Connectors = ({ openWalletDrawer }: ConnectorsProps) => {
 
   useEffect(() => {
     if (walletDetails) {
-      fetchRateAndCalculateTotalBalance(walletDetails).then(result => {
+      fetchRateAndCalculateTotalBalance(walletDetails).then((result) => {
         dispatch(updateTotalBalance(calculateTotalBalance(result)))
         dispatch(updateBalanceBreakdown(result))
 
@@ -199,18 +199,21 @@ const Connectors = ({ openWalletDrawer }: ConnectorsProps) => {
                     <Divider />
                   </React.Fragment>
                 ))}
-                {hiiq && walletDetails && walletDetails.length > 0 && hiIQData && (
-                  <>
-                    <WalletDetails
-                      symbol={hiIQData?.symbol}
-                      tokensArray={[hiIQData?.tokensArray]}
-                      balance={shortenBigBalance(
-                        Number(BigInt(String(hiiq?.hiiqBalance))),
-                      )}
-                    />
-                    <Divider />
-                  </>
-                )}
+                {hiiq &&
+                  walletDetails &&
+                  walletDetails.length > 0 &&
+                  hiIQData && (
+                    <>
+                      <WalletDetails
+                        symbol={hiIQData?.symbol}
+                        tokensArray={[hiIQData?.tokensArray]}
+                        balance={shortenBigBalance(
+                          Number(BigInt(String(hiiq?.hiiqBalance))),
+                        )}
+                      />
+                      <Divider />
+                    </>
+                  )}
               </Box>
             )}
           </>
