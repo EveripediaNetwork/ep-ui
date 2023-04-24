fa1f61f2180e5b96dfc867c0b62bb29dda16edd9
OleanjiKingCode <holamilean56@gmail.com>
2023-04-24 17:53:43 +0100

    removing comments

--- src/components/Elements/Carousel/Carousel.tsx
+++ src/components/Elements/Carousel/Carousel.tsx
@@ -6,12 +6,6 @@ import { IconType } from 'react-icons/lib'
 import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 
-// const Slider = SliderClass as unknown as (props: {
-//   children: React.ReactNode
-//   nextArrow: JSX.Element
-//   prevArrow: JSX.Element
-// }) => JSX.Element
-
 const Slider = SliderClass as unknown as React.ForwardRefExoticComponent<
   {
     children: React.ReactNode
@@ -58,25 +52,19 @@ interface CarouselProps {
 
 const Carousel = ({ settings, children, topArrow }: CarouselProps) => {
   const [activeSlide, setActiveSlide] = useState(0)
-  // rome-ignore lint/correctness/noUnusedVariables: <explanation>
-  const [activeSlide2, setActiveSlide2] = useState(0)
 
+  const sliderRef = useRef<SliderClass>(null)
   const fullSettings: Settings = {
     ...settings,
-    // rome-ignore lint/correctness/noUnusedVariables: <explanation>
-    beforeChange: (current, next) => setActiveSlide(next),
-    afterChange: (current) => setActiveSlide2(current),
+    beforeChange: (_current, next) => setActiveSlide(next),
   }
-  const sliderRef = useRef<SliderClass>(null)
+
   const circles = [0, 1, 2, 3, 4]
+
   const handleClick = (index: number) => {
-    sliderRef?.current?.slickGoTo(
-      activeSlide < 4
-        ? index
-        : 2 > index
-        ? activeSlide - (2 - index)
-        : activeSlide + (index - 2),
-    )
+    const nextSlide =
+      activeSlide < 4 ? index : index === 2 ? 2 : activeSlide - (2 - index)
+    sliderRef?.current?.slickGoTo(nextSlide)
   }
 
   return (
@@ -92,7 +80,7 @@ const Carousel = ({ settings, children, topArrow }: CarouselProps) => {
         {children}
       </Slider>
       <HStack gap="2" w="full" justifyContent="center" pt="3">
-        {circles.map((index) => {
+        {circles.map(index => {
           const isActive = activeSlide < 4 ? activeSlide === index : index === 2
           const bg = isActive ? 'brand.500' : 'brand.50'
           const darkBg = isActive ? 'brand.800' : 'brand.200'
