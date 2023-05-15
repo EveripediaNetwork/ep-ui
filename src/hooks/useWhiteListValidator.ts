40143c81448fb67893ba5104fabb7a38e72c23a8
Damilare Bakare <bakaredmlr@gmail.com>
2023-05-15 15:42:33 +0100

    fixes useWhiteListValidator bug

--- src/hooks/useWhiteListValidator.ts
+++ src/hooks/useWhiteListValidator.ts
@@ -2,17 +2,14 @@ import { EditorABI } from '@/abi/EditorAbi'
 import config from '@/config'
 import { useContractRead } from 'wagmi'
 
-const readContract = {
-  addressOrName: config.editorAddress,
-  contractInterface: EditorABI,
-}
-
 export const useWhiteListValidator = (address: string | undefined | null) => {
   const { data: isEditorWhiteListed } = useContractRead({
-    ...readContract,
+    address: config.editorAddress as `0x${string}`,
+    abi: EditorABI,
     functionName: 'isEditorWhitelisted',
     args: [address],
   })
+
 
   return {
     userCanEdit: config.isProduction === 'true' ? isEditorWhiteListed : true,
