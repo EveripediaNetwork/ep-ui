f2a2ad677da100b1bee5815bb479e2256dba48f9
OleanjiKingCode <holamilean56@gmail.com>
2023-05-17 07:37:51 +0100

    re rending of table of contents

--- src/components/Wiki/WikiPage/WikiTableOfContents.tsx
+++ src/components/Wiki/WikiPage/WikiTableOfContents.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {
   VStack,
   Text,
@@ -20,7 +20,7 @@ interface WikiTableOfContentsProps {
 }
 
 const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
-  const toc = useAppSelector((state) => state.toc)
+  const toc = useAppSelector(state => state.toc)
   const { asPath, query, push } = useRouter()
 
   const { slug } = query
@@ -36,13 +36,14 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
   const { colorMode } = useColorMode()
   const { isOpen, onToggle } = useDisclosure()
   const isDefaultOpen = useBreakpointValue({ base: false, xs: true, xl: false })
-  const [activeId, setActiveId] = React.useState<string | null>()
+  const [activeId, setActiveId] = useState<string | null>()
 
   // the below ref is used to store all the heading that are in view
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const headingElementsRef: any = React.useRef({})
+  const headingElementsRef: any = useRef({})
 
-  React.useEffect(() => {
+  useEffect(() => {
+    console.log('tktktkrk')
     // get all the heading elements
     const headingElements = Array.from(
       document.querySelectorAll('h1, h2, h3, h4, h5, h6'),
@@ -52,7 +53,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
     // this function will be called when the heading element is in view
     // hence when the heading element is in view,
     // we will set the activeId to the id of the heading element
-    const callback: IntersectionObserverCallback = (headings) => {
+    const callback: IntersectionObserverCallback = headings => {
       headingElementsRef.current = headings.reduce((map, headingElement) => {
         map[headingElement.target.id] = headingElement
         return map
@@ -60,14 +61,14 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
 
       // get the id of the heading element that is in view
       const visibleHeadings: IntersectionObserverEntry[] = []
-      Object.keys(headingElementsRef.current).forEach((key) => {
+      Object.keys(headingElementsRef.current).forEach(key => {
         const headingElement: IntersectionObserverEntry =
           headingElementsRef.current[key]
         if (headingElement.isIntersecting) visibleHeadings.push(headingElement)
       })
 
       const getIndexFromId = (id: string) =>
-        headingElements.findIndex((heading) => heading.id === id)
+        headingElements.findIndex(heading => heading.id === id)
 
       // setting the activeId to the id of the heading element that is in view
       if (visibleHeadings.length === 1) {
@@ -75,7 +76,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
       } else if (visibleHeadings.length > 1) {
         // if there are multiple heading elements in view then set heading near to top as active
         let closestHeading: IntersectionObserverEntry = visibleHeadings[0]
-        visibleHeadings.forEach((headingElement) => {
+        visibleHeadings.forEach(headingElement => {
           if (
             closestHeading === undefined ||
             getIndexFromId(closestHeading.target.id) >
@@ -92,93 +93,94 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
     const observer = new IntersectionObserver(callback, {
       rootMargin: '-100px 0px 0px 0px',
     })
-    headingElements.forEach((element) => observer.observe(element))
+    headingElements.forEach(element => observer.observe(element))
 
     return () => observer.disconnect()
   }, [setActiveId, toc])
 
-  React.useEffect(() => {
+  useEffect(() => {
     if (!activeId) setActiveId(toc[0]?.id)
   }, [activeId, toc])
 
-  if (isOpen === isDefaultOpen) {
-    return (
-      <VStack
-        display={{ base: 'none', xl: 'block' }}
-        borderLeftWidth="1px"
-        w="20vw"
-        px={6}
-        py="30px"
-        ml="0 !important"
-      >
-        <VStack
-          spacing={4}
-          align="start"
-          position="sticky"
-          top="calc(70px + 30px + 2px)"
-        >
-          <Flex w="100%" justify="end">
-            <IconButton
-              aria-label="Toggle Table of Contents"
-              icon={<RiMenu3Fill />}
-              onClick={onToggle}
-            />
-          </Flex>
-          <StaticContent>
-            <VStack
-              as="nav"
-              spacing={4}
-              h="calc(100vh - (70px + 90px))"
-              overflowY="scroll"
-              pr={4}
-              w="100%"
-              align="start"
-              css={{
-                '&::-webkit-scrollbar': {
-                  width: '4px',
-                },
-                '&::-webkit-scrollbar-track': {
-                  width: '6px',
-                },
-                '&::-webkit-scrollbar-thumb': {
-                  background: colorMode === 'light' ? '#0000002a' : '#3f444e',
-                  borderRadius: '24px',
-                },
-              }}
-            >
-              {toc.map(({ level, id, title }) => (
-                <Box key={id} pl={`calc(${(level - 1) * 20}px)`}>
-                  <Text
-                    color={activeId === id ? 'brandLinkColor' : 'unset'}
-                    boxShadow={
-                      activeId === id ? '-2px 0px 0px 0px #ff5caa' : '0'
-                    }
-                    outlineColor="brandLinkColor"
-                    pl={2}
-                  >
-                    <Link href={`#${id}`}>{title}</Link>
-                  </Text>
-                </Box>
-              ))}
-            </VStack>
-          </StaticContent>
-        </VStack>
-      </VStack>
-    )
-  }
   return (
-    <Box
-      display={{ base: 'none', md: 'block' }}
-      pos="absolute"
-      right="24px"
-      top={`calc(70px + ${isAlertAtTop ? '80px' : '32px'})`}
-    >
-      <IconButton
-        aria-label="Toggle Table of Contents"
-        icon={<RiMenu3Fill />}
-        onClick={onToggle}
-      />
-    </Box>
+    <>
+      {isOpen === isDefaultOpen ? (
+        <VStack
+          display={{ base: 'none', xl: 'block' }}
+          borderLeftWidth="1px"
+          w="20vw"
+          px={6}
+          py="30px"
+          ml="0 !important"
+        >
+          <VStack
+            spacing={4}
+            align="start"
+            position="sticky"
+            top="calc(70px + 30px + 2px)"
+          >
+            <Flex w="100%" justify="end">
+              <IconButton
+                aria-label="Toggle Table of Contents"
+                icon={<RiMenu3Fill />}
+                onClick={onToggle}
+              />
+            </Flex>
+            <StaticContent>
+              <VStack
+                as="nav"
+                spacing={4}
+                h="calc(100vh - (70px + 90px))"
+                overflowY="scroll"
+                pr={4}
+                w="100%"
+                align="start"
+                css={{
+                  '&::-webkit-scrollbar': {
+                    width: '4px',
+                  },
+                  '&::-webkit-scrollbar-track': {
+                    width: '6px',
+                  },
+                  '&::-webkit-scrollbar-thumb': {
+                    background: colorMode === 'light' ? '#0000002a' : '#3f444e',
+                    borderRadius: '24px',
+                  },
+                }}
+              >
+                {toc.map(({ level, id, title }) => (
+                  <Box key={id} pl={`calc(${(level - 1) * 20}px)`}>
+                    <Text
+                      color={activeId === id ? 'brandLinkColor' : 'unset'}
+                      boxShadow={
+                        activeId === id ? '-2px 0px 0px 0px #ff5caa' : '0'
+                      }
+                      outlineColor="brandLinkColor"
+                      pl={2}
+                    >
+                      <Link href={`#${id}`}>{title}</Link>
+                    </Text>
+                  </Box>
+                ))}
+              </VStack>
+            </StaticContent>
+          </VStack>
+        </VStack>
+      ) : (
+        <Box
+          display={{ base: 'none', md: 'block' }}
+          pos="absolute"
+          right="24px"
+          top={`calc(70px + ${isAlertAtTop ? '80px' : '32px'})`}
+        >
+          <IconButton
+            aria-label="Toggle Table of Contents"
+            icon={<RiMenu3Fill />}
+            onClick={onToggle}
+          />
+        </Box>
+      )}
+    </>
   )
 }
 
