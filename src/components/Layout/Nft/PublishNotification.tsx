728ecb07ddf283d92726ee79059ba09ba404fcc9
Damilare Bakare <bakaredmlr@gmail.com>
2023-05-26 20:39:56 +0100

    adds notification for unwhitelisted address

--- src/components/Layout/Nft/PublishNotification.tsx
+++ src/components/Layout/Nft/PublishNotification.tsx
@@ -9,7 +9,6 @@ import {
   Icon,
   Button,
   Spacer,
-  Image,
 } from '@chakra-ui/react'
 import { FocusableElement } from '@chakra-ui/utils'
 import { RiCloseLine } from 'react-icons/ri'
@@ -45,17 +44,13 @@ const MintNotification = ({
               onClick={() => setModalState(false)}
             />
           </Flex>
-          <Image src="/images/nft-pass/success.gif" alt="success" />
-          <Text mt="5" fontSize="xl" textAlign="center">
-            BrainPass subscription renewed!
-          </Text>
           <Text fontSize="sm" mt="5" textAlign="center">
-            Your BrainPass subscription has been successfully Renewed. You can
-            continue to create and edit wikis and contribute to the platform's
-            wealth of knowledge.
+            To publish a wiki, it is mandatory to possess a BrainPass. Don't
+            worry if you don't have one yet! Simply mint your own BrainPass to
+            gain access.
           </Text>
           <Flex my="5" justifyContent="center">
-            <Button>Connect Wallet</Button>
+            <Button>Mint</Button>
           </Flex>
         </Box>
       </AlertDialogContent>
