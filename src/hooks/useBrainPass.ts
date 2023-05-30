4147c16b171b1708e232b6f7f26b8d109e5c5277
Damilare Bakare <bakaredmlr@gmail.com>
2023-05-30 17:53:22 +0100

    adds useBrainPass hook

---
+++ src/hooks/useBrainPass.ts
@@ -0,0 +1,24 @@
+import { BrainPassABI } from '@/abi/BrainPass.abi'
+import config from '@/config'
+import { useAccount, useContractRead } from 'wagmi'
+
+const brainpassConfig = {
+    address: config.brainpassAddress as `0x${string}`,
+    abi: BrainPassABI,
+}
+export const useBrainPass = () => {
+  const {address} = useAccount()
+  const { data: userPass } = useContractRead({
+    ...brainpassConfig,
+    functionName: 'addressToNFTPass',
+    args: [address],
+  })
+
+  console.log('userPass', userPass)
+
+  return {
+    userPass
+  }
+}
+
+export default useBrainPass
