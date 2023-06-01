3f599f06d7dd529ac3a9b949280aa846e1fd18d4
Damilare Bakare <bakaredmlr@gmail.com>
2023-06-01 15:32:38 +0100

    implements passType function

--- src/hooks/useBrainPass.ts
+++ src/hooks/useBrainPass.ts
@@ -3,8 +3,6 @@ import config from '@/config'
 import { formatUnits } from 'viem'
 import { useAccount, useContractRead } from 'wagmi'
 
-const DEFAULT_VALUE: bigint = BigInt(0)
-
 const brainpassConfig = {
   address: config.brainpassAddress as `0x${string}`,
   abi: BrainPassABI,
@@ -18,18 +16,30 @@ export const useBrainPass = () => {
     args: [address],
   })
 
-  const { endTimestamp } = (userPass as { endTimestamp: bigint }) || {
-    endTimestamp: DEFAULT_VALUE,
-  }
+  const { data: passTypes } = useContractRead({
+    ...brainpassConfig,
+    functionName: 'getAllPassType',
+  })
 
   const isUserPassActive = () => {
+    if (!userPass) return false
+    const { endTimestamp } = userPass as { endTimestamp: bigint }
     const endTime = Number(formatUnits(endTimestamp, 18))
     const todayToTimestamp = new Date().getTime() / 1000
     if (!endTime || endTime < todayToTimestamp) return false
     return true
   }
 
+  const getCurrentPassName = () => {
+    if (!passTypes) return null
+    const result = passTypes as { name: string }[]
+    const name = result[1].name
+    return name
+  }
+
   const getPassEndDate = () => {
+    if (!userPass) return null
+    const { endTimestamp } = userPass as { endTimestamp: bigint }
     const endTime = Number(formatUnits(endTimestamp, 18))
     if (!endTime) return null
     return endTime * 1000
@@ -38,6 +48,7 @@ export const useBrainPass = () => {
   return {
     passEndDate: getPassEndDate(),
     isUserPassActive: isUserPassActive(),
+    getCurrentPassName: getCurrentPassName(),
   }
 }
 
