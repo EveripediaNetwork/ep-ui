b91d5cdbeaaee6e4bf5b9408eb9964007ae4c1f7
OleanjiKingCode <holamilean56@gmail.com>
2023-06-01 17:14:30 +0100

    got correct expiration dates, and status of pass, reined user details

--- src/hooks/useBrainPass.ts
+++ src/hooks/useBrainPass.ts
@@ -21,9 +21,15 @@ export const useBrainPass = () => {
     functionName: 'getAllPassType',
   })
 
+  const userPassDetails = userPass as {
+    tokenId: bigint
+    startTimestamp: bigint
+    endTimestamp: bigint
+    passId: bigint
+  }
   const isUserPassActive = () => {
     if (!userPass) return false
-    const { endTimestamp } = userPass as { endTimestamp: bigint }
+    const { endTimestamp } = userPassDetails
     const endTime = Number(formatUnits(endTimestamp, 18))
     const todayToTimestamp = new Date().getTime() / 1000
     if (!endTime || endTime < todayToTimestamp) return false
@@ -39,13 +45,26 @@ export const useBrainPass = () => {
 
   const getPassEndDate = () => {
     if (!userPass) return null
-    const { endTimestamp } = userPass as { endTimestamp: bigint }
+    const { endTimestamp } = userPassDetails
     const endTime = Number(formatUnits(endTimestamp, 18))
     if (!endTime) return null
     return endTime * 1000
   }
 
+  const refinePassDetails = () => {
+    if (!userPass) return null
+
+    const details = {
+      tokenId: Number(userPassDetails?.tokenId),
+      passId: Number(userPassDetails?.passId),
+      startTimeStamp: Number(userPassDetails?.startTimestamp),
+      endTimeStamp: Number(userPassDetails?.endTimestamp),
+    }
+    return details
+  }
+
   return {
+    UserPass: refinePassDetails(),
     passEndDate: getPassEndDate(),
     isUserPassActive: isUserPassActive(),
     getCurrentPassName: getCurrentPassName(),
