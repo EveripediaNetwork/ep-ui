0124fa95d2453aa3b43c0de9dfd7e664a3528ddb
OleanjiKingCode <holamilean56@gmail.com>
2023-06-08 13:53:03 +0100

    improved data usuage from rank api

--- src/pages/rank/index.tsx
+++ src/pages/rank/index.tsx
@@ -114,7 +114,7 @@ const Rank = ({
                 currentPage={tokensOffset}
                 totalCount={totalTokens}
                 pageSize={LISTING_LIMITS}
-                onPageChange={page => setTokensOffset(page)}
+                onPageChange={(page) => setTokensOffset(page)}
               >
                 <RankTableHead />
                 <Tbody>
@@ -156,7 +156,7 @@ const Rank = ({
                 currentPage={nftOffset}
                 totalCount={totalNfts}
                 pageSize={LISTING_LIMITS}
-                onPageChange={page => setNftOffset(page)}
+                onPageChange={(page) => setNftOffset(page)}
               >
                 <RankTableHead />
                 <Tbody>
