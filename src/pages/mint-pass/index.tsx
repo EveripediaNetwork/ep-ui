90aaf971961e302c650629fb1a99b285b5b483e3
Damilare Bakare <bakaredmlr@gmail.com>
2023-06-17 08:35:30 +0100

    fixes lint error

--- src/pages/mint-pass/index.tsx
+++ src/pages/mint-pass/index.tsx
@@ -386,7 +386,7 @@ const Mint = () => {
                     min={28}
                     defaultValue={subscriptionPeriod}
                     max={maxPeriod}
-                    onChange={value => updateSubscriptionPeriod(value)}
+                    onChange={(value) => updateSubscriptionPeriod(value)}
                     value={subscriptionPeriod}
                   >
                     <SliderTrack>
@@ -414,7 +414,7 @@ const Mint = () => {
                       color="grayText4"
                       bg="lightCard"
                       textAlign="center"
-                      onChange={e =>
+                      onChange={(e) =>
                         updateSubscriptionPeriod(Number(e.target.value))
                       }
                     />
