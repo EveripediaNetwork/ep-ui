79f7eca01a313c9d27556cbb0381fa0163570569
Damilare Bakare <bakaredmlr@gmail.com>
2023-06-26 18:55:44 +0100

    reworks newsletter subscription

--- src/components/Layout/Footer/NewsletterSubscription.tsx
+++ src/components/Layout/Footer/NewsletterSubscription.tsx
@@ -1,33 +1,28 @@
 import React from 'react'
-import { Box, Button, HStack, Input, Stack, Text } from '@chakra-ui/react'
+import { Button, Stack, Text } from '@chakra-ui/react'
 
 const NewsletterSubscription = () => {
   return (
     <Stack align={{ base: 'center', lg: 'flex-start' }} spacing={6}>
       <Text mb={0} fontSize="xl" fontWeight="bold">
-        Suscribe to our newsletter
+        Subscribe to our newsletter
       </Text>
       <Text align={{ base: 'center', lg: 'start' }} maxW="600px">
         Never miss any of the most popular and trending articles on IQ.Social
         when you sign up to our email newsletter.
       </Text>
-      <Box p={2} rounded="md" bg="#ffffff" w={{ base: '100%', md: '80%' }}>
-        <HStack>
-          <Input
-            variant={'solid'}
-            borderWidth={1}
-            color={'gray.800'}
-            _placeholder={{
-              color: 'gray.700',
-            }}
-            border="none"
-            required
-            placeholder="Enter your email"
-            bg="#ffffff"
-          />
-          <Button variant="solid">Submit</Button>
-        </HStack>
-      </Box>
+      <Button
+        as="a"
+        href="https://www.getdrip.com/forms/505929689/submissions/new"
+        target="_blank"
+        size="lg"
+        variant="solid"
+        bg="#fff"
+        color="gray.800"
+        _hover={{ bg: '#fff', color: 'gray.800' }}
+      >
+        Subscribe
+      </Button>
     </Stack>
   )
 }
