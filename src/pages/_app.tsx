8ac181fa3c37cee0968346b628d480ce856ee1f2
Tolulope Malomo <malomo.tolz@gmail.com>
2023-08-04 14:11:29 +0100

    fix theme glitch with next-themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,7 @@ import React, { StrictMode, useEffect } from 'react'
 import '../styles/global.css'
 import '../styles/editor-dark.css'
 import '@/editor-plugins/pluginStyles.css'
-import {
-  ChakraProvider,
-  createStandaloneToast,
-  cookieStorageManager,
-} from '@chakra-ui/react'
+import { createStandaloneToast } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { Provider as ReduxProvider } from 'react-redux'
 import Layout from '@/components/Layout/Layout/Layout'
@@ -17,8 +13,8 @@ import { pageView } from '@/utils/googleAnalytics'
 import '../utils/i18n'
 import { WagmiConfig, createConfig } from 'wagmi'
 import { Montserrat } from '@next/font/google'
-import chakraTheme from '../theme'
 import { connectors, publicClient, webSocketPublicClient } from '@/config/wagmi'
+import { ThemeProvider } from '@/theme'
 
 const { ToastContainer } = createStandaloneToast()
 
@@ -56,17 +52,13 @@ const App = ({ Component, pageProps, router }: EpAppProps) => {
       <NextNProgress color="#FF5CAA" />
       <SEOHeader router={router} />
       <ReduxProvider store={store}>
-        <ChakraProvider
-          colorModeManager={cookieStorageManager}
-          resetCSS
-          theme={chakraTheme}
-        >
+        <ThemeProvider>
           <WagmiConfig config={client}>
             <Layout noFooter={Component.noFooter}>
               <Component {...pageProps} />
             </Layout>
           </WagmiConfig>
-        </ChakraProvider>
+        </ThemeProvider>
       </ReduxProvider>
       <ToastContainer />
     </StrictMode>
---
+++ src/theme/index.tsx
@@ -0,0 +1,63 @@
+// import { styles } from '@/theme/styles'
+// import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+// import { Dict } from '@chakra-ui/utils'
+// import { components } from './components'
+// import { foundations } from './foundations'
+// import { semanticTokens } from './semantic-tokens'
+
+// const config: ThemeConfig = {
+//   useSystemColorMode: true,
+//   initialColorMode: 'system',
+//   cssVarPrefix: 'chakra',
+// }
+
+// const theme: Dict = extendTheme({
+//   components,
+//   config,
+//   ...foundations,
+//   semanticTokens,
+//   styles,
+// })
+
+// export default theme
+
+import { styles } from '@/theme/styles'
+import { ChakraProvider, extendTheme, ThemeConfig } from '@chakra-ui/react'
+import { Dict } from '@chakra-ui/utils'
+import { components } from './components'
+import { foundations } from './foundations'
+import { semanticTokens } from './semantic-tokens'
+import {
+  ThemeProvider as NextThemeProvider,
+  useTheme as useNextTheme,
+} from 'next-themes'
+import { PropsWithChildren } from 'react'
+
+type UseThemeProps = {
+  resolvedTheme?: 'light' | 'dark'
+  setTheme: (theme: string) => void
+}
+
+export const ThemeProvider = ({ children }: PropsWithChildren<unknown>) => {
+  const { resolvedTheme } = useNextTheme() as UseThemeProps
+
+  const config: ThemeConfig = {
+    useSystemColorMode: true,
+    initialColorMode: resolvedTheme,
+    cssVarPrefix: 'chakra',
+  }
+
+  const theme: Dict = extendTheme({
+    ...foundations,
+    components,
+    config,
+    semanticTokens,
+    styles,
+  })
+
+  return (
+    <NextThemeProvider attribute="class" enableSystem defaultTheme="system">
+      <ChakraProvider theme={theme}>{children}</ChakraProvider>
+    </NextThemeProvider>
+  )
+}
