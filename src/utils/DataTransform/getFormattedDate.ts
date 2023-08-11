410e483226b01aaf9e813adf693f7523122f86cf
Damilare Bakare <bakaredmlr@gmail.com>
2023-08-11 17:26:04 +0100

    fixes notification issue and more button inconsistence

--- src/utils/DataTransform/getFormattedDate.ts
+++ src/utils/DataTransform/getFormattedDate.ts
@@ -1,7 +1,11 @@
 import { formatDistanceToNowStrict } from 'date-fns'
 
 export const getReadableDate = (dateToFormat: string) => {
-  return formatDistanceToNowStrict(new Date(dateToFormat), {
+  const parsedDate = new Date(dateToFormat)
+  if (isNaN(parsedDate.getTime())) {
+    return ''
+  }
+  return formatDistanceToNowStrict(parsedDate, {
     addSuffix: false,
   })
 }
