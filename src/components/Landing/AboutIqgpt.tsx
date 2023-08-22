42d0b67cae649b5844cff3ffc3019543753f940c
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-08-22 20:59:15 +0100

    done with md layout design

--- src/components/Landing/AboutIqgpt.tsx
+++ src/components/Landing/AboutIqgpt.tsx
@@ -17,7 +17,7 @@ const CustomListItem = ({ text }: { text: string }) => {
   return (
     <Flex gap={2} align={'flex-start'}>
       <CheckCircleIcon color={'white'} />
-      <Text mt={-1} color={'white'}>
+      <Text mt={-1} fontSize={{ base: '12px', md: '16px' }} color={'white'}>
         {text}
       </Text>
     </Flex>
@@ -28,12 +28,12 @@ const AboutIqgpt = () => {
   const { t } = useTranslation()
   return (
     <VStack
-      align={'self-start'}
+      align={{ base: 'center', lg: 'self-start' }}
       spacing={6}
       px={{ base: 3, md: 8 }}
       maxW={'1050px'}
-      mt={{ md: '200px', lg: '300px' }}
-      mb={{ base: '100px' }}
+      mt={{ md: '100px', lg: '300px' }}
+      mb={{ base: '200px', lg: 0 }}
       mx={'auto'}
     >
       <HStack
@@ -54,14 +54,14 @@ const AboutIqgpt = () => {
         </HStack>
       </HStack>
       <SimpleGrid
-        columns={{ base: 1, md: 2 }}
-        gap={{ base: '200px', md: 4 }}
+        columns={{ base: 1, lg: 2 }}
+        gap={{ base: '150px', md: '250px', lg: 4 }}
         bgColor={'cardBg'}
-        rounded={'20px'}
+        rounded={{ base: '6px', md: '20px' }}
         border={'1px'}
         borderColor={'gray.100'}
-        px={{ md: '12px', xl: '24px' }}
-        pr={{ md: 0, lg: '12px' }}
+        px={{ base: '20px', md: '36px', xl: '24px' }}
+        pr={{ lg: '12px' }}
         pt={{ base: '18px', md: '32px', lg: '62px' }}
         pb={{ md: '0', lg: '44px', xl: '56px' }}
         _dark={{
@@ -69,26 +69,29 @@ const AboutIqgpt = () => {
         }}
       >
         <VStack
-          maxW={'500px'}
-          px={{ base: '8px', md: '0' }}
+          maxW={{ base: '500px', md: '100%', lg: '500px' }}
           pb={{ md: '18px', lg: '0px' }}
-          align={'self-start'}
+          align={{ base: 'center', lg: 'self-start' }}
         >
           <HStack>
             <Logo
-              w={{ base: '60px', lg: '85px' }}
-              h={{ base: '60px', lg: '85px' }}
+              w={{ base: '50px', md: '60px', lg: '85px' }}
+              h={{ base: '50px', md: '60px', lg: '85px' }}
             />
             <Text
               fontWeight="bold"
-              fontSize={{ base: '28px', md: '36px', lg: '48px' }}
+              fontSize={{ base: '24px', md: '36px', lg: '48px' }}
               color="wikiFlagTextColor"
               _dark={{ color: 'white' }}
             >
               IQ GPT
             </Text>
           </HStack>
-          <Box color={'eventTextColor'} pb={'20px'}>
+          <Box
+            textAlign={{ base: 'center', lg: 'inherit' }}
+            color={'eventTextColor'}
+            pb={'20px'}
+          >
             <Text fontSize={{ base: '14px', md: '16px' }}>
               {`${t('aboutIQGPTOneBody')}`}
             </Text>
@@ -96,12 +99,12 @@ const AboutIqgpt = () => {
               {`${t('aboutIQGPTTwoBody')}`}
             </Text>
           </Box>
-          <HStack gap={{ base: 2, xl: 6 }}>
+          <HStack gap={4}>
             <LinkButton
               href={'https://iqgpt.com/'}
               size="lg"
               variant="solid"
-              px={{ base: 3, md: 5 }}
+              px={{ base: 5 }}
               fontSize={'12px'}
             >
               Explore IQ GPT
@@ -110,7 +113,7 @@ const AboutIqgpt = () => {
               href={'https://iqgpt.com/about'}
               size="lg"
               variant="outline"
-              px={{ base: 3, md: 5 }}
+              px={{ base: 5 }}
               fontSize={'12px'}
               color={'aboutIqTokenText'}
             >
@@ -121,19 +124,20 @@ const AboutIqgpt = () => {
         <Box
           pos={'relative'}
           bgColor={'brandLinkColor'}
-          rounded={'20px'}
-          pt={{ base: '180px', md: '150px', lg: '200px' }}
-          pb={{ base: '30px', md: '0px', lg: '70px' }}
+          rounded={{ base: '6px', md: '20px' }}
+          pt={{ base: '180px', md: '180px', lg: '200px' }}
+          pb={{ base: '10px', md: '20px', lg: '70px' }}
           px={'9px'}
           mt={{ md: '-100px', lg: '-150px' }}
+          mb={{ base: '-200px', lg: 0 }}
         >
           <Box
             pos={'absolute'}
             rounded={'100%'}
             bgColor={'aboutIqgptInfoBg'}
-            w={{ base: '300px', md: '250px', lg: '350px' }}
-            h={{ base: '300px', md: '250px', lg: '350px' }}
-            top={{ base: '-30%', md: '-25%', lg: '-30%' }}
+            w={{ base: 250, md: '300px', lg: '350px' }}
+            h={{ base: 250, md: '300px', lg: '350px' }}
+            top={{ base: '-25%', md: '-30%', lg: '-30%' }}
             left={'50%'}
             transform={'auto-gpu'}
             translateX={'-50%'}
@@ -141,13 +145,13 @@ const AboutIqgpt = () => {
             <Image
               src="/images/GIFs/intelligentRobotMin.gif"
               alt="IQ GPT AI robot"
-              width={{ base: '300px', md: 250, lg: 350 }}
-              height={{ base: '300px', md: 250, lg: 350 }}
+              width={{ base: 250, md: 300, lg: 350 }}
+              height={{ base: 250, md: 300, lg: 350 }}
             />
           </Box>
           <VStack
-            gap={{ md: 2, lg: 5 }}
-            px={{ md: '10px', lg: '20px', xl: '28px' }}
+            gap={{ base: 2, md: 5 }}
+            px={{ md: '30px', lg: '20px', xl: '28px' }}
             pb={'24px'}
           >
             <CustomListItem text={`${t('benefitIQGPTOne')}`} />
