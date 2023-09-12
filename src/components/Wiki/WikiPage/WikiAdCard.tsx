56c4bde8de1ea00693ce8a2eee9ce6eccc744b94
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-09-12 18:31:43 +0100

    done with the responsiveness iqgpt card on wiki page

---
+++ src/components/Wiki/WikiPage/WikiAdCard.tsx
@@ -0,0 +1,39 @@
+import { LinkWrapper } from '@/components/Elements/LinkElements/LinkWrapper'
+import { ArrowForwardIcon } from '@chakra-ui/icons'
+import { Box, HStack, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const WikiAdCard = () => {
+  return (
+    <Box
+      rounded={'lg'}
+      w="full"
+      border={'1px'}
+      borderColor={'iqgptAdCardBorder'}
+      p={'12px'}
+      bgColor={'aboutIqgptInfoBg'}
+    >
+      <Text
+        fontSize={{ base: '12px' }}
+        fontWeight={'500'}
+        color={'brandLinkColor'}
+      >
+        IQ GPT is the world’s first AI crypto search engine that provides
+        insights into intricate terms, live market trends, and breaking news.
+      </Text>
+
+      <HStack spacing={1} mt={'6px'} color={'brandLinkColor'}>
+        <Text fontSize={{ base: '14px' }} fontWeight={'600'}>
+          Check it out
+        </Text>
+        <LinkWrapper href={'https://iqgpt.com/'}>
+          <Box as="a" target="_blank">
+            <ArrowForwardIcon />
+          </Box>
+        </LinkWrapper>
+      </HStack>
+    </Box>
+  )
+}
+
+export default WikiAdCard
