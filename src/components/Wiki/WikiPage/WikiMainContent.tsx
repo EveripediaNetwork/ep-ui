7c0d5b449ff03d3213ed0c8366d771cbdb12ba9c
Tolulope Malomo <malomo.tolz@gmail.com>
2023-09-20 11:03:52 +0100

    load more wiki (wip)

--- src/components/Wiki/WikiPage/WikiMainContent.tsx
+++ src/components/Wiki/WikiPage/WikiMainContent.tsx
@@ -1,6 +1,6 @@
 import { CommonMetaIds, Wiki } from '@everipedia/iq-utils'
-import { Box, Heading, useColorMode } from '@chakra-ui/react'
-import React, { useMemo } from 'react'
+import { Box, Heading, useColorMode, Button } from '@chakra-ui/react'
+import React, { useMemo, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 import { store } from '@/store/store'
@@ -15,7 +15,42 @@ import { WikiFlaggingSystem } from './WikiFlaggingSystem'
 interface WikiMainContentProps {
   wiki: Wiki
 }
+
+function splitMarkdownSections(markdownString: string) {
+  const headingRegex = /^#{1,6}\s+.*/gm
+
+  const sections = markdownString.split(headingRegex)
+
+  const filteredSections = sections.filter(
+    (section: string) => section.trim() !== '',
+  )
+
+  return filteredSections
+}
+
+const getSectionStop = (allSections: string[], lastStop: number) => {
+  /**
+   * If the content has 4 or less sections,
+   * we render the entire thing, else we
+   * render in chunks to improve performance
+   */
+  const noOfSections = allSections.length
+  console.log('length: ', noOfSections)
+  if (allSections.length <= 4) return noOfSections + 1
+
+  if (noOfSections - (lastStop - 1) > 4) return lastStop + 5
+  else return noOfSections + 1
+}
+
 const MarkdownRender = React.memo(({ wiki }: { wiki: Wiki }) => {
+  const contentSections = splitMarkdownSections(wiki.content)
+  const [sectionStop, setSectionStop] = useState(
+    getSectionStop(contentSections, 0),
+  )
+  console.log('First stop: ', sectionStop)
+  const renderedContent = contentSections.splice(0, sectionStop)
+  const hasMoreContent = sectionStop < contentSections.length + 1
+
   store.dispatch({
     type: 'citeMarks/reset',
   })
@@ -30,27 +65,40 @@ const MarkdownRender = React.memo(({ wiki }: { wiki: Wiki }) => {
 
   if (!wiki.content) return null
 
+  const handleLoadContentBtnClick = () => {
+    console.log('Stop: ', getSectionStop)
+    setSectionStop(getSectionStop(contentSections, sectionStop))
+  }
+
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
-      components={{
-        h1: addToTOC,
-        h2: addToTOC,
-        h3: addToTOC,
-        h4: addToTOC,
-        h5: addToTOC,
-        h6: addToTOC,
-        a: (props) =>
-          customLinkRenderer({
-            ...props,
-            referencesString,
-          }),
-        img: customImageRender,
-        table: customTableRenderer,
-      }}
-    >
-      {wiki.content}
-    </ReactMarkdown>
+    <>
+      <ReactMarkdown
+        className="md-container"
+        remarkPlugins={[remarkGfm]}
+        components={{
+          h1: addToTOC,
+          h2: addToTOC,
+          h3: addToTOC,
+          h4: addToTOC,
+          h5: addToTOC,
+          h6: addToTOC,
+          a: props =>
+            customLinkRenderer({
+              ...props,
+              referencesString,
+            }),
+          img: customImageRender,
+          table: customTableRenderer,
+        }}
+      >
+        {renderedContent.join('')}
+      </ReactMarkdown>
+      {hasMoreContent && (
+        <Button onClick={() => handleLoadContentBtnClick()}>
+          Load more content
+        </Button>
+      )}
+    </>
   )
 })
 
@@ -60,7 +108,7 @@ const WikiMainContent = ({ wiki }: WikiMainContentProps) => {
   let content = wiki?.content.replace(/<br( )*\/?>/g, '\n') || ''
 
   const matchRegex = /\$\$widget\d(.*?\))\$\$/
-  content.match(new RegExp(matchRegex, 'g'))?.forEach((match) => {
+  content.match(new RegExp(matchRegex, 'g'))?.forEach(match => {
     const widgetContent = match.match(matchRegex)?.[1]
     if (widgetContent) {
       content = content.replaceAll(match, widgetContent)
