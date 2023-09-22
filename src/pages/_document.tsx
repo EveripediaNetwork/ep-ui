eb93c1ff5506b95a5405a2767a177ec45e12e2de
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-09-22 18:11:14 +0530

    set rel to dns prefetch

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,9 +15,9 @@ export default function Document() {
           content="black-translucent"
         />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="preconnect" href="https://i.ytimg.com" crossOrigin="true" />
+        <link rel="dns-prefetch" href="https://i.ytimg.com" crossOrigin="true" />
         <link
-          rel="preconnect"
+          rel="dns-prefetch"
           href="https://assets.auth.magic.link"
           crossOrigin="true"
         />
