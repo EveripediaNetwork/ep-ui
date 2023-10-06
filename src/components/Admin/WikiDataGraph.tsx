e7c1a3727f237c34e2c82a4a8212f9576c12e3e9
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-10-06 16:30:08 +0100

    biome formatting

--- src/components/Admin/WikiDataGraph.tsx
+++ src/components/Admin/WikiDataGraph.tsx
@@ -149,7 +149,7 @@ export const WikiDataGraph = () => {
           <Select
             w={{ lg: '27%', md: '39%', base: '50%' }}
             icon={<MdArrowDropDown />}
-            onChange={e => {
+            onChange={(e) => {
               handleGraphFilterChange(e.target.value)
             }}
           >
@@ -238,7 +238,7 @@ export const WikiDataGraph = () => {
           <Select
             w={{ lg: '80%', md: '60%', base: '54%' }}
             icon={<MdArrowDropDown />}
-            onChange={e => {
+            onChange={(e) => {
               handleWikiVisitFilterChange(e.target.value)
             }}
           >
