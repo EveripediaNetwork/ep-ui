c32ff49624732bb8aad4698a66e315f7dc9c7141
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2023-11-24 00:44:09 +0530

    Added Maintenance page (#1533)

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest) {
+  const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'true'
+
+  console.log(req.nextUrl.pathname, isMaintenanceMode)
+
+  if (
+    isMaintenanceMode &&
+    req.nextUrl.pathname !== '/maintenance' &&
+    !req.nextUrl.pathname.includes('.')
+  ) {
+    const url = req.nextUrl.clone()
+    url.pathname = '/maintenance'
+    return NextResponse.redirect(url.toString(), { status: 302 })
+  }
+
+  return NextResponse.next()
+}
