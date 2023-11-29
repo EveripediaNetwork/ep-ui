b1f21da4e92cfa9c3d83dc18e5291a7a1fd9a0ce
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-11-29 17:17:44 +0100

    lint fixes

--- src/components/Rank/RankCardItem.tsx
+++ src/components/Rank/RankCardItem.tsx
@@ -50,7 +50,7 @@ const RankingItem = ({
   }`
 
   const dateFounded = item?.events?.find(
-    event => event.type === EventType.CREATED,
+    (event) => event.type === EventType.CREATED,
   )?.date
 
   return (
