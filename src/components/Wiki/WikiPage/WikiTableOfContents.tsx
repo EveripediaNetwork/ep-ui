7a2c48e986ac726fd2538a92e092dbff7f2a078b
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-12-08 15:46:40 +0100

    resolved table of content rendering issue

--- src/components/Wiki/WikiPage/WikiTableOfContents.tsx
+++ src/components/Wiki/WikiPage/WikiTableOfContents.tsx
@@ -18,33 +18,44 @@ interface WikiTableOfContentsProps {
   isAlertAtTop?: boolean
 }
 
-interface Item {
+interface TOCItem {
   level: number
   id: string
   title: string
-  subChildren?: Item[]
+  subChildren?: TOCItem[]
 }
 
-function groupArrayByLevel(inputArray: Item[]): Item[] {
-  const result: Item[] = []
-  const levelMap: Record<number, Item[]> = {}
+function groupArrayByLevel(inputArray: TOCItem[]): TOCItem[] {
+  const result: TOCItem[] = []
+  const levelMap: Record<number, TOCItem[]> = {}
 
-  inputArray.forEach((item) => {
+  inputArray.forEach(item => {
     const { id, title, level } = item
-    const tocItem: Item = { level, id, title }
+    const tocItem: TOCItem = { level, id, title }
 
     if (level === 1) {
       result.push(tocItem)
     } else {
-      const parentLevel = level - 1
-      const parentItems = levelMap[parentLevel]
-      const parentItem = parentItems[parentItems.length - 1]
-      if (!parentItem.subChildren) {
-        parentItem.subChildren = []
+      // Find the correct parent level
+      let parentLevel = level - 1
+      while (!levelMap[parentLevel] && parentLevel > 1) {
+        parentLevel--
       }
-      parentItem.subChildren.push(tocItem)
+
+      // If parent level found, add it to subChildren
+      if (levelMap[parentLevel]) {
+        const parentItems = levelMap[parentLevel]
+        const parentItem = parentItems[parentItems.length - 1]
+
+        if (!parentItem.subChildren) {
+          parentItem.subChildren = []
+        }
+
+        parentItem.subChildren.push(tocItem)
+      }
     }
 
+    // Update the levelMap
     if (!levelMap[level]) {
       levelMap[level] = []
     }
@@ -55,7 +66,7 @@ function groupArrayByLevel(inputArray: Item[]): Item[] {
 }
 
 const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
-  const toc = useAppSelector((state) => state.toc)
+  const toc = useAppSelector(state => state.toc)
   const { asPath, query, push } = useRouter()
 
   const { slug } = query
@@ -87,7 +98,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
     // this function will be called when the heading element is in view
     // hence when the heading element is in view,
     // we will set the activeId to the id of the heading element
-    const callback: IntersectionObserverCallback = (headings) => {
+    const callback: IntersectionObserverCallback = headings => {
       headingElementsRef.current = headings.reduce((map, headingElement) => {
         map[headingElement.target.id] = headingElement
         return map
@@ -95,14 +106,14 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
 
       // get the id of the heading element that is in view
       const visibleHeadings: IntersectionObserverEntry[] = []
-      Object.keys(headingElementsRef.current).forEach((key) => {
+      Object.keys(headingElementsRef.current).forEach(key => {
         const headingElement: IntersectionObserverEntry =
           headingElementsRef.current[key]
         if (headingElement.isIntersecting) visibleHeadings.push(headingElement)
       })
 
       const getIndexFromId = (id: string) =>
-        headingElements.findIndex((heading) => heading.id === id)
+        headingElements.findIndex(heading => heading.id === id)
 
       // setting the activeId to the id of the heading element that is in view
       if (visibleHeadings.length === 1) {
@@ -110,7 +121,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
       } else if (visibleHeadings.length > 1) {
         // if there are multiple heading elements in view then set heading near to top as active
         let closestHeading: IntersectionObserverEntry = visibleHeadings[0]
-        visibleHeadings.forEach((headingElement) => {
+        visibleHeadings.forEach(headingElement => {
           if (
             closestHeading === undefined ||
             getIndexFromId(closestHeading.target.id) >
@@ -127,7 +138,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
     const observer = new IntersectionObserver(callback, {
       rootMargin: '-100px 0px 0px 0px',
     })
-    headingElements.forEach((element) => observer.observe(element))
+    headingElements.forEach(element => observer.observe(element))
 
     return () => observer.disconnect()
   }, [setActiveId, toc])
@@ -135,6 +146,9 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
   useEffect(() => {
     if (!activeId) setActiveId(toc[0]?.id)
   }, [activeId, toc])
+
+  console.log({ toc })
+  console.log(groupArrayByLevel(toc))
   return (
     <>
       {isOpen === isDefaultOpen ? (
@@ -187,7 +201,7 @@ const WikiTableOfContents = ({ isAlertAtTop }: WikiTableOfContentsProps) => {
                   },
                 }}
               >
-                {groupArrayByLevel(toc).map((item) => (
+                {groupArrayByLevel(toc).map(item => (
                   <WikiTableOfContentHeader
                     toc={item}
                     key={item.id}
