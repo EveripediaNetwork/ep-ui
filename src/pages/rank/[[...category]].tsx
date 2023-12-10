2aad78603d8288f6c5be913dd17915ec94942e7b
Aliu Salaudeen <ayokunumi99@gmail.com>
2023-12-10 21:23:40 +0100

    lint fixes

--- src/pages/rank/[[...category]].tsx
+++ src/pages/rank/[[...category]].tsx
@@ -362,7 +362,7 @@ const Rank = ({
                 currentPage={tokensOffset}
                 totalCount={totalTokens}
                 pageSize={LISTING_LIMIT}
-                onPageChange={page => setTokensOffset(page)}
+                onPageChange={(page) => setTokensOffset(page)}
                 maxW={'90%'}
               >
                 <RankTableHead onClickMap={onClickMap} />
@@ -434,7 +434,7 @@ const Rank = ({
                 currentPage={stableCoinOffset}
                 totalCount={totalStableCoins}
                 pageSize={LISTING_LIMIT}
-                onPageChange={page => setStableCoinOffset(page)}
+                onPageChange={(page) => setStableCoinOffset(page)}
                 maxW={'90%'}
               >
                 <RankTableHead onClickMap={onClickMap} />
@@ -505,7 +505,7 @@ const Rank = ({
                 currentPage={aiTokensOffset}
                 totalCount={totalAiTokens}
                 pageSize={LISTING_LIMIT}
-                onPageChange={page => setAiTokensOffset(page)}
+                onPageChange={(page) => setAiTokensOffset(page)}
                 maxW={'90%'}
               >
                 <RankTableHead onClickMap={onClickMap} />
@@ -581,7 +581,7 @@ const Rank = ({
                 currentPage={foundersOffset}
                 totalCount={totalTokens}
                 pageSize={LISTING_LIMIT}
-                onPageChange={page => setFoundersOffset(page)}
+                onPageChange={(page) => setFoundersOffset(page)}
                 maxW={'90%'}
               >
                 <FoundersRankTableHead onClickMap={onClickMap} />
@@ -653,7 +653,7 @@ const Rank = ({
                 currentPage={nftOffset}
                 totalCount={totalNfts}
                 pageSize={LISTING_LIMIT}
-                onPageChange={page => setNftOffset(page)}
+                onPageChange={(page) => setNftOffset(page)}
                 maxW={'90%'}
               >
                 <RankTableHead onClickMap={onClickMap} />
@@ -691,7 +691,7 @@ const Rank = ({
 
 export default Rank
 
-export const getServerSideProps: GetServerSideProps = async ctx => {
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { data: tokensData } = await store.dispatch(
     getCategoryTotal.initiate({ category: 'cryptocurrencies' }),
   )
