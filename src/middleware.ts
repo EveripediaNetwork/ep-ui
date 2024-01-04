7a569dee6e6f69c9d5665c215d9118734873c0cb
Adebesin Cell <adebesintolulope80@gmail.com>
2024-01-04 23:19:18 +0100

    fixes logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { languageData } from './data/LanguageData'
 
 export function middleware(req: NextRequest) {
   const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'true'
 
+  // Check for maintenance mode first
   if (
     isMaintenanceMode &&
     req.nextUrl.pathname !== '/maintenance' &&
@@ -11,6 +13,29 @@ export function middleware(req: NextRequest) {
     const url = req.nextUrl.clone()
     url.pathname = '/maintenance'
     return NextResponse.redirect(url.toString(), { status: 302 })
+  }
+
+  const userLocale = req.headers
+    .get('accept-language')
+    ?.split(',')[0]
+    .split('-')[0]
+
+  const hasLanguagePart = languageData.some((lang) =>
+    req.nextUrl.pathname.startsWith(`/${lang.locale}`),
+  )
+
+  if (!hasLanguagePart) {
+    const isLocaleSupported = languageData.some(
+      (lang) => lang.locale === userLocale,
+    )
+
+    if (!isLocaleSupported) {
+      const defaultLocale =
+        languageData.find((lang) => lang.default)?.locale ?? 'en'
+      const url = req.nextUrl.clone()
+      url.pathname = `/${defaultLocale}${req.nextUrl.pathname}`
+      return NextResponse.redirect(url.toString(), { status: 302 })
+    }
   }
 
   return NextResponse.next()
