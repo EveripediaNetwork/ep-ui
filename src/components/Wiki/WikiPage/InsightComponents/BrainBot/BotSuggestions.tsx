544dd1eedb2858bec47203f51c83d7eea26fbbd9
César Rodríguez <kesarr@gmail.com>
2024-01-05 18:10:02 +0100

    Update BotSuggestions.tsx

--- src/components/Wiki/WikiPage/InsightComponents/BrainBot/BotSuggestions.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/BrainBot/BotSuggestions.tsx
@@ -59,7 +59,7 @@ const BotSuggestions = ({ wiki }: { wiki: Wiki }) => {
         fontSize={'14px'}
         textAlign={'center'}
       >
-        Hi There! I’m the BrainBot. Here are some cool stuff i can do for you.
+        Hi There! Here are some cool stuff i can do for you.
       </Text>
       <VStack marginTop={'12px'}>
         <BrainBotSuggestion
