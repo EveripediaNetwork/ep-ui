bead2d58fb4cd2c58b186512a6c535ede8adc41b
Tolulope Malomo <malomo.tolz@gmail.com>
2024-01-08 20:43:17 +0100

    fix redirection

--- src/utils/checkValidLocale.ts
+++ src/utils/checkValidLocale.ts
@@ -1,19 +1,21 @@
-export const isValidLocale = (locale: string) => {
-  try {
-    // Create a new Intl.DateTimeFormat object with the given locale
-    const formatter = new Intl.DateTimeFormat(locale)
+export const isValidLocale = (locale?: string) => {
+  if (!locale) return false
 
-    // Get the resolved locale from the formatter
-    const resolvedLocale = formatter.resolvedOptions().locale
+  // Create a new Intl.DateTimeFormat object with the given locale
+  const formatter = new Intl.DateTimeFormat(locale)
 
-    // Compare the resolved locale with the input
-    // This check handles cases where the API defaults to a fallback locale
-    return resolvedLocale === locale || resolvedLocale.startsWith(`${locale}-`)
-  } catch (_) {
-    return false
-  }
+  // Get the resolved locale from the formatter
+  const resolvedLocale = formatter.resolvedOptions().locale
+
+  // Compare the resolved locale with the input
+  // This check handles cases where the API defaults to a fallback locale
+  return resolvedLocale === locale || resolvedLocale.startsWith(`${locale}-`)
 }
 
 export const revertToKr = (locale: string) => {
   return locale === 'ko' ? 'kr' : locale
+}
+
+export const revertToKo = (locale: string) => {
+  return locale === 'kr' ? 'ko' : locale
 }
