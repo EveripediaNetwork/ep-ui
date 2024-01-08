2c58fafcb51d1095a8e1ad375ee64a59d271d91a
Adebesin Cell <adebesintolulope80@gmail.com>
2024-01-08 09:26:09 +0100

    handle wrong locale in url to default to english

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server'
+import { isValidLocale } from './utils/checkValidLocale'
+import { languageData } from './data/LanguageData'
 
 export function middleware(req: NextRequest) {
   const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'true'
@@ -12,6 +14,33 @@ export function middleware(req: NextRequest) {
     const url = req.nextUrl.clone()
     url.pathname = '/maintenance'
     return NextResponse.redirect(url.toString(), { status: 302 })
+  }
+
+  const pathname = req.nextUrl.pathname
+  const segments = pathname.split('/').filter(Boolean)
+  const potentialLocale = segments[0]
+
+  const isLocaleValid = isValidLocale(potentialLocale)
+
+  if (isLocaleValid) {
+    const isLocaleSupported = languageData.find(
+      (language) => language.locale === potentialLocale,
+    )
+
+    if (!isLocaleSupported) {
+      const defaultLocale =
+        languageData.find((lang) => lang.default)?.locale || 'en'
+
+      const updatedPathname = pathname.replace(
+        `/${potentialLocale}`,
+        `/${defaultLocale}`,
+      )
+
+      const urlWithLocale = req.nextUrl.clone()
+      urlWithLocale.pathname = updatedPathname
+
+      return NextResponse.redirect(urlWithLocale.toString(), { status: 302 })
+    }
   }
 
   return NextResponse.next()
