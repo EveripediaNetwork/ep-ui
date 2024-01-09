3296610e1ee906c96e0defc1cbc5b6cdfe6efd0a
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-01-09 22:14:03 +0100

    resolving merge conflict issue

--- src/components/Wiki/WikiPage/InsightComponents/BrainBot/BotChatBox.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/BrainBot/BotChatBox.tsx
@@ -11,7 +11,7 @@ const BotChatBox = ({ wiki }: { wiki: Wiki }) => {
   const [chatInput, setChatInput] = useState('')
   const { askQuestion } = useStream()
   const dispatch = useDispatch()
-  const { isLoading } = useAppSelector(state => state.stream)
+  const { isLoading } = useAppSelector((state) => state.stream)
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setChatInput(e.target.value)
@@ -29,7 +29,7 @@ const BotChatBox = ({ wiki }: { wiki: Wiki }) => {
   }
 
   return (
-    <form onSubmit={e => handleSubmit(e)}>
+    <form onSubmit={(e) => handleSubmit(e)}>
       <HStack
         bgColor={'brainBotBg'}
         paddingBlock={'4px'}
