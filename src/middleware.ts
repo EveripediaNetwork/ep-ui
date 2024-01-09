10c7be4726bce16e860376ee94d5a470cbbfc325
Adebesin Cell <adebesintolulope80@gmail.com>
2024-01-09 23:38:44 +0100

    improvement

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,21 +7,23 @@ import {
 
 export function middleware(req: NextRequest) {
   const isMaintenanceMode = process.env.MAINTENANCE_MODE === 'true'
-  const defaultLocale = 'en' // Set default locale
+  const defaultLocale = 'en'
 
-  // Check for maintenance mode first
   if (
     isMaintenanceMode &&
     req.nextUrl.pathname !== '/maintenance' &&
     !req.nextUrl.pathname.includes('.')
   ) {
+    // Set default locale
+
+    // Check for maintenance mode first
     const url = req.nextUrl.clone()
     url.pathname = '/maintenance'
     return NextResponse.redirect(url.toString(), { status: 302 })
   }
 
-  // Skip redirection for static assets and API routes
   if (shouldNotRedirect(req.nextUrl.pathname)) {
+    // Skip redirection for static assets and API routes
     return NextResponse.next()
   }
 
@@ -29,25 +31,26 @@ export function middleware(req: NextRequest) {
   const pathnameParts = req.nextUrl.pathname.split('/')
   let urlLocale = pathnameParts[1]
 
-  // Check if locale is in URL and valid
   if (urlLocale && isValidLocale(urlLocale)) {
-    // If 'ko', revert to 'kr'
     urlLocale = revertToKr(urlLocale)
-    // Set NEXT_LOCALE cookie and return next response
     const response = NextResponse.next()
-    response.cookies.set('NEXT_LOCALE', urlLocale)
+
+    if (req.cookies.get('NEXT_LOCALE')?.value !== urlLocale) {
+      response.cookies.set('NEXT_LOCALE', urlLocale)
+    }
+
     return response
   }
 
   // Get preferred locale from cookie or headers
   const preferredLocale = getPreferredLanguage(req)
 
-  // Check if preferred locale is valid and different from current URL locale
   if (
     preferredLocale &&
     isValidLocale(preferredLocale) &&
     preferredLocale !== urlLocale
   ) {
+    // Redirect to preferred locale if it's valid and different from URL locale
     const url = req.nextUrl.clone()
     url.pathname = `/${revertToKr(preferredLocale)}${url.pathname.substring(
       urlLocale ? urlLocale.length + 1 : 0,
@@ -55,11 +58,16 @@ export function middleware(req: NextRequest) {
     return NextResponse.redirect(url.toString(), { status: 302 })
   }
 
-  // Redirect to default locale if no valid locale in URL
   if (!urlLocale || !isValidLocale(urlLocale)) {
-    const url = req.nextUrl.clone()
-    url.pathname = `/${defaultLocale}${req.nextUrl.pathname}`
-    return NextResponse.redirect(url.toString(), { status: 302 })
+    // Redirect to default locale if no valid locale in URL
+    // Avoid redirecting if already at default locale
+    if (urlLocale !== defaultLocale) {
+      const url = req.nextUrl.clone()
+      url.pathname = `/${defaultLocale}${req.nextUrl.pathname.substring(
+        urlLocale ? urlLocale.length + 1 : 0,
+      )}`
+      return NextResponse.redirect(url.toString(), { status: 302 })
+    }
   }
 
   return NextResponse.next()
