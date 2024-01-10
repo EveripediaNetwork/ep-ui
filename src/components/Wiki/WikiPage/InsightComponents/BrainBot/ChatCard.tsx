06c3a546ca2ef4890cd4a37e7ed337ccab15fd82
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-01-10 11:50:57 +0100

    handling client exception error

--- src/components/Wiki/WikiPage/InsightComponents/BrainBot/ChatCard.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/BrainBot/ChatCard.tsx
@@ -29,7 +29,7 @@ const _paginateContent = (text: string, charsPerPage: number) => {
   const pages = []
   let currentPage = ''
 
-  words.forEach((word) => {
+  words.forEach(word => {
     if ((currentPage + word).length > charsPerPage) {
       pages.push(currentPage.trim())
       currentPage = `${word} `
@@ -102,7 +102,8 @@ const _usePaginateContent = (content: string) => {
 }
 
 const ContentPagination = ({ content, alias, answerSources }: ChartProps) => {
-  const { currentAIMessage } = useAppSelector((state) => state.message)
+  const { currentAIMessage } = useAppSelector(state => state.message)
+  const { isError } = useAppSelector(state => state.stream)
   const _markdownTableRegex = /\|.*\|.*\|/
   const [currentPageIndex, setCurrentPageIndex] = useState(0)
   // const pages = markdownTableRegex.test(content)
@@ -146,6 +147,7 @@ const ContentPagination = ({ content, alias, answerSources }: ChartProps) => {
       </div>
       {alias === 'AI' &&
         !currentAIMessage &&
+        !isError &&
         currentPageIndex === pages.length - 1 && (
           <ChatSources answerSource={answerSource} />
         )}
