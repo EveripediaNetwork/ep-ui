e4f32b3d5205ba46419f40ee6cd5d03eb2540ce7
Tolulope Malomo <malomo.tolz@gmail.com>
2024-02-05 15:57:09 +0100

    fix wiki images

--- src/components/Wiki/WikiPage/CustomRenderers/customImageRender.tsx
+++ src/components/Wiki/WikiPage/CustomRenderers/customImageRender.tsx
@@ -1,5 +1,6 @@
-import { Image } from '@/components/Elements/Image/Image'
+import Image from 'next/image'
 import React from 'react'
+import { cfLoader } from '@/components/Elements/Image/Image'
 
 export const customImageRender = ({
   ...props
@@ -7,13 +8,22 @@ export const customImageRender = ({
   React.ImgHTMLAttributes<HTMLImageElement>,
   HTMLImageElement
 >) => {
+  const { src } = props
   return (
     <Image
-      h="300px"
-      my={2}
-      objectFit="contain"
-      src={props.src || ''}
+      quality={95}
+      loader={
+        typeof src === 'string' && src.startsWith('http') ? cfLoader : undefined
+      }
+      src={src || ''}
       alt="wiki"
+      width={900}
+      height={400}
+      style={{
+        width: 'auto',
+        height: 'auto',
+        position: 'static',
+      }}
     />
   )
 }
