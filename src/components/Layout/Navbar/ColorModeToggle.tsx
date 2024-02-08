9a8f43f925ed0970a60be29f4a8db8e8d0a5bda8
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-02-08 18:44:46 +0100

    done with theme configuration for tailwind

--- src/components/Layout/Navbar/ColorModeToggle.tsx
+++ src/components/Layout/Navbar/ColorModeToggle.tsx
@@ -1,5 +1,5 @@
 import { Flex, Icon, MenuItem, Switch, useColorMode } from '@chakra-ui/react'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { FaSun, FaMoon } from 'react-icons/fa'
 import { useTranslation } from 'next-i18next'
 
@@ -11,6 +11,18 @@ export const ColorModeToggle = ({
   const { colorMode, toggleColorMode } = useColorMode()
   const ColorModeIcon = colorMode === 'light' ? FaMoon : FaSun
   const { t } = useTranslation('common')
+
+  useEffect(() => {
+    console.log({ colorMode })
+    if (colorMode === 'dark') {
+      document.documentElement.classList.add('dark')
+      document.documentElement.classList.remove('light')
+    } else {
+      document.documentElement.classList.add('light')
+      document.documentElement.classList.remove('dark')
+    }
+  }, [colorMode])
+
   return (
     <>
       <MenuItem
