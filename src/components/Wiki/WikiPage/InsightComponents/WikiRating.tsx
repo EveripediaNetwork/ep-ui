24aeb72018f0cbb49e55fff14bfc85f59aa2fb7f
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-02-09 15:19:27 +0530

    Add default states to avg rating

--- src/components/Wiki/WikiPage/InsightComponents/WikiRating.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/WikiRating.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react'
 import { VStack, Text, Box, Divider } from '@chakra-ui/react'
 import { VscSmiley } from 'react-icons/vsc'
+import { RiStarSmileFill } from 'react-icons/ri'
 import WikiAccordion from '../../WikiAccordion'
 import { useTranslation } from 'next-i18next'
 import WikiStarRating from './WikiStarRating'
+import { wikiRatingType } from '@/types/admin'
 
 const WikiRating = ({
   contentId,
@@ -16,8 +18,8 @@ const WikiRating = ({
 
   const { t } = useTranslation('wiki')
   //TODO: get rating from backend
-  const avgRating = 3.5
-  const totalRatings = 100
+  const [avgRating, _setAvgRating] = useState<wikiRatingType | undefined>(1)
+  const [totalRatings, _setTotalRatings] = useState<number | undefined>(2)
 
   return (
     <VStack w="100%" spacing={4} borderRadius={2}>
@@ -30,29 +32,35 @@ const WikiRating = ({
         defaultOpen
       >
         <VStack bgColor="wikiCardItemBg" borderRadius={4} gap="2" p={3}>
-          <VStack alignItems="center">
-            //TODO: translate
-            <Text fontWeight={'semibold'} fontSize="lg" textColor="fadedText">
-              Average Rating
-            </Text>
-            //TODO: add average rating
-            <Box
-              fontSize="xs"
-              rounded="2xl"
-              bgColor="brand.50"
-              _dark={{ bgColor: 'brand.200', textColor: 'brand.800' }}
-              textColor="brand.500"
-              px="3"
-            >
-              {`Based on over ${totalRatings} ratings`}
-            </Box>
-            <WikiStarRating
-              contentId={contentId}
-              userId={userId}
-              setIsRated={setIsRated}
-              avgRating={avgRating}
-            />
-          </VStack>
+          <Text fontWeight={'semibold'} fontSize="lg" textColor="fadedText">
+            Average Rating
+          </Text>
+          <Box
+            fontSize="xs"
+            rounded="2xl"
+            bgColor="brand.50"
+            _dark={{ bgColor: 'brand.200', textColor: 'brand.800' }}
+            textColor="brand.500"
+            px="3"
+          >
+            {totalRatings
+              ? `Based on over ${totalRatings} ratings`
+              : 'No ratings yet, be the first to rate!'}
+          </Box>
+          {totalRatings ? (
+            <VStack alignItems="center">
+              //TODO: translate //TODO: add average rating
+              <WikiStarRating
+                contentId={contentId}
+                userId={userId}
+                setIsRated={setIsRated}
+                avgRating={avgRating}
+                isAvgRating
+              />
+            </VStack>
+          ) : (
+            <RiStarSmileFill size={18} color="#FF1A88" />
+          )}
           <Divider />
           {!isRated ? (
             <VStack>
