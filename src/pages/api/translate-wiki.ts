e5cd170e239685b8a4ac4878946990a251e3dae7
Tolulope Malomo <malomo.tolz@gmail.com>
2024-02-09 17:56:36 +0100

    fix translation

--- src/pages/api/translate-wiki.ts
+++ src/pages/api/translate-wiki.ts
@@ -10,34 +10,35 @@ export default async function handler(
   if (!content || !title)
     return res.status(400).json({ msg: 'Title or content is missing ' })
   const chunks = getWikiChunks(content)
+  console.log('Chunks: ', chunks)
   const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
 
   const [translatedTitle, ...translatedTexts] = await Promise.all(
     chunks.map(async (chunk: string) => {
       return openai.chat.completions.create({
         model: 'gpt-3.5-turbo',
-        temperature: 0.2,
+        temperature: 0,
         messages: [
           {
             role: 'system',
-            content: `You will be provided with a piece of markdown text in English. Your task is to output the text in Korean, preserving the formatting. While outputing the translation, make sure to do the following:
-              1. Do not translate names to Korean, output names the way they are in the original english text
-              1. Any peice of characters that start and end with double asterisks(**) should be immediately fellowed by an empty space( ). 
-              For example, **리플 (Ripple)**은 should be replaced with **리플 (Ripple)** 은
-              Explanation: 
-              the text 리플 (ripple) in **리플 (ripple)**은 starts and begins with double asterisks (**), hence add a space immediately after the ending asterisks before adding the next character.
-              That is, immediately after **리플 (ripple)**, add space before 은
+            content: `A peice of markdown will be provided after the keyword 'START'. Translate the peiece of markdown text to Korean, using the following rules:
+  1. Do not translate names of people or organizations to Korean
+  2. Any peice of characters that start and end with double asterisks(**) should be immediately fellowed by an empty space( ).
+  For example, **리플 (Ripple)**은 should be replaced with **리플 (Ripple)** 은
+  Explanation:
+  the text 리플 (ripple) in **리플 (ripple)**은 starts and begins with double asterisks (**), hence add a space immediately after the ending asterisks before adding the next character.
+  That is, immediately after **리플 (ripple)**, add space before 은
 
-              Example 2
-              The text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 should be replaced with **아컴 인텔리전스 (Arkham Intelligence)** 는
-              Explanation:
-              the text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 starts and begins with double asterisks (**), hence add a space immediately after the ending double asterisks before adding the next character.
-              That is, immediately after **아컴 인텔리전스 (Arkham Intelligence)**, add space before 는
+  Example 2
+  The text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 should be replaced with **아컴 인텔리전스 (Arkham Intelligence)** 는
+  Explanation:
+  the text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 starts and begins with double asterisks (**), hence add a space immediately after the ending double asterisks before adding the next character.
+  That is, immediately after **아컴 인텔리전스 (Arkham Intelligence)**, add space before 는
 
-              Example 4
-              **컨버전스 (Convergence)**는 should be replaced with **컨버전스 (Convergence)** 는
-              
-              VERY IMPORTANT to use the pattern when applicable
+  Example 4
+  **컨버전스 (Convergence)**는 should be replaced with **컨버전스 (Convergence)** 는
+
+  START:
               `,
           },
           {
@@ -48,6 +49,22 @@ export default async function handler(
       })
     }),
   )
+
+  // 1. Do not translate names of people or organizations to Korean
+  // 2. Any peice of characters that start and end with double asterisks(**) should be immediately fellowed by an empty space( ).
+  // For example, **리플 (Ripple)**은 should be replaced with **리플 (Ripple)** 은
+  // Explanation:
+  // the text 리플 (ripple) in **리플 (ripple)**은 starts and begins with double asterisks (**), hence add a space immediately after the ending asterisks before adding the next character.
+  // That is, immediately after **리플 (ripple)**, add space before 은
+
+  // Example 2
+  // The text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 should be replaced with **아컴 인텔리전스 (Arkham Intelligence)** 는
+  // Explanation:
+  // the text 아컴 인텔리전스 (Arkham Intelligence) in **아컴 인텔리전스 (Arkham Intelligence)**는 starts and begins with double asterisks (**), hence add a space immediately after the ending double asterisks before adding the next character.
+  // That is, immediately after **아컴 인텔리전스 (Arkham Intelligence)**, add space before 는
+
+  // Example 4
+  // **컨버전스 (Convergence)**는 should be replaced with **컨버전스 (Convergence)** 는
 
   const translatedContent = [translatedTitle, ...translatedTexts].map(
     (result) => result.choices[0].message.content,
