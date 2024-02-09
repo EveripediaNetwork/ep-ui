5ccf3437bf1d7c9a81d8c42aa000599d73fd0ea4
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-02-09 15:19:37 +0530

    Refactor WikiStarRating component

--- src/components/Wiki/WikiPage/InsightComponents/WikiStarRating.tsx
+++ src/components/Wiki/WikiPage/InsightComponents/WikiStarRating.tsx
@@ -4,30 +4,37 @@ import { wikiRatingType } from '@/types/admin'
 import { RiStarSFill } from 'react-icons/ri'
 import { HStack, IconButton } from '@chakra-ui/react'
 
+type WikiStarRatingProps = {
+  contentId: string
+  userId?: string
+  setIsRated: React.Dispatch<React.SetStateAction<boolean>>
+  avgRating?: number
+  isAvgRating?: boolean
+}
+
 const wikiStarRating = ({
   contentId,
   userId,
   setIsRated,
   avgRating,
-}: {
-  contentId: string
-  userId?: string
-  setIsRated: (isRated: boolean) => void
-  avgRating?: number
-}) => {
+  isAvgRating = false,
+}: WikiStarRatingProps) => {
   const [currHoverRating, setCurrHoverRating] = React.useState(0)
   const [contentFeedback] = useContentFeedbackMutation()
   const sendFeedback = async (rating: wikiRatingType) => {
     setIsRated(true)
     await contentFeedback({ contentId, userId, rating })
   }
-  return avgRating ? (
+  return isAvgRating ? (
     <HStack>
       {[...Array(5)].map((_, i) => {
         const ratingValue = i + 1
         return (
           <IconButton
             key={i}
+            cursor={'default'}
+            fontSize={{ base: 'md', md: 'xl' }}
+            size="xs"
             icon={<RiStarSFill />}
             variant="unstyled"
             color={ratingValue <= (avgRating || 0) ? 'brand.500' : ''}
@@ -42,11 +49,12 @@ const wikiStarRating = ({
         const ratingValue = i + 1
         return (
           <IconButton
-            fontSize={{ base: 'xl', md: '2xl' }}
+            fontSize={{ base: 'xl', md: '3xl' }}
+            size="sm"
             key={i}
             icon={<RiStarSFill />}
             variant="unstyled"
-            color={ratingValue <= (currHoverRating || 0) ? 'brand.500' : ''}
+            color={ratingValue <= (currHoverRating || 0) ? '#FEC84B' : ''}
             aria-label={`rating-${ratingValue}`}
             onMouseOver={() => setCurrHoverRating(i + 1)}
             onClick={() => {
