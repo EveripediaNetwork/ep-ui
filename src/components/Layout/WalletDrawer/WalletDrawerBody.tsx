bd0a63ffffd551a97a920d4535c2edd08de9d252
Adebesin Cell <adebesintolulope80@gmail.com>
2024-02-20 15:54:58 +0100

    fixes link in profile wallet

--- src/components/Layout/WalletDrawer/WalletDrawerBody.tsx
+++ src/components/Layout/WalletDrawer/WalletDrawerBody.tsx
@@ -55,7 +55,7 @@ export const WalletDrawerBody = () => {
 
   useEffect(() => {
     if (walletDetails) {
-      fetchRateAndCalculateTotalBalance(walletDetails).then((result) => {
+      fetchRateAndCalculateTotalBalance(walletDetails).then(result => {
         dispatch(updateTotalBalance(calculateTotalBalance(result)))
         dispatch(updateBalanceBreakdown(result))
         setTotalBalanceIsLoading(false)
