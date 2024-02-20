08ee0c26b0d07c8a01a64180dcf7e33d0e886773
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-02-20 11:31:45 +0100

    working on homepage event section

---
+++ src/components/Landing/EventOverview.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const EventOverview = () => {
+  return (
+    <div className="max-w-[1296px] mx-auto py-24">
+      <h2 className="text-4xl font-semibold">
+        Blockchain and Cryptocurrency Events
+      </h2>
+      <div className="grid grid-cols-2 gap-14">
+        <div className="flex flex-col">
+          <p>
+            Learn from the industry experts on crypto trends, explore investment
+            opportunities, network with potential partners, connect with
+            like-minded individuals, and cultivate relationships for future
+            collaborations at global blockchain and crypto events and
+            conferences.
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EventOverview
