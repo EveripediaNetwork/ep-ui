cda5d17ca19f2ce900123f6bdf5fab44e3c2ebfc
Sid <58448956+SidharthK2@users.noreply.github.com>
2024-02-23 15:29:02 +0530

    Revert "SEO for wiki ratings"

--- src/components/SEO/Wiki.tsx
+++ src/components/SEO/Wiki.tsx
@@ -1,8 +1,7 @@
 import { env } from '@/env.mjs'
-import { NextSeo } from 'next-seo'
-import * as DOMPurify from 'dompurify'
+import { ArticleJsonLd, NextSeo } from 'next-seo'
 import React from 'react'
-import Head from 'next/head'
+
 interface WikiHeaderProps {
   slug: string
   title: string
@@ -27,65 +26,36 @@ export const WikiHeader = ({
   noindex = false,
   avgRating,
   totalRatings,
-}: WikiHeaderProps) => {
-  const combinedStructuredData = JSON.stringify({
-    '@context': 'https://schema.org',
-    '@type': 'Article',
-    '@id': `${env.NEXT_PUBLIC_DOMAIN}/wiki/${slug}`,
-    name: title,
-    description: description,
-    image: [mainImage],
-    datePublished: datePublished || '',
-    dateModified: dateModified || '',
-    author: {
-      '@type': 'Person',
-      name: author,
-    },
-    publisher: {
-      '@type': 'Organization',
-      name: 'IQWiki',
-      logo: `${env.NEXT_PUBLIC_DOMAIN}/images/icons/favicon.ico`,
-    },
-    mainEntityOfPage: {
-      '@type': 'WebPage',
-      '@id': `${env.NEXT_PUBLIC_DOMAIN}/wiki/${slug}`,
-    },
-    mediaObject: {
-      '@type': 'MediaObject',
-      contentUrl: `${env.NEXT_PUBLIC_DOMAIN}/wiki/${slug}`,
-      name: title,
-      aggregateRating: {
-        '@type': 'AggregateRating',
+}: WikiHeaderProps) => (
+  <>
+    <ArticleJsonLd
+      url={`${env.NEXT_PUBLIC_DOMAIN}/wiki/${slug}`}
+      title={title}
+      images={[mainImage]}
+      datePublished={datePublished || ''}
+      dateModified={dateModified || ''}
+      authorName={author}
+      publisherName="IQWiki"
+      publisherLogo={`${env.NEXT_PUBLIC_DOMAIN}/images/icons/favicon.ico`}
+      description={description}
+      aggregateRating={{
         ratingValue: avgRating?.toString() || '0',
         reviewCount: totalRatings?.toString() || '0',
-      },
-    },
-  })
-  return (
-    <>
-      <NextSeo
-        title={title}
-        description={description}
-        noindex={noindex}
-        openGraph={{
-          title,
-          description,
-          images: [
-            {
-              url: `${env.NEXT_PUBLIC_DOMAIN}/_next/image?url=${mainImage}&w=1200&q=95`,
-            },
-          ],
-        }}
-      />
-      <Head>
-        <script
-          type="application/ld+json"
-          // biome-ignore lint: reason=nextjs-no-xss
-          dangerouslySetInnerHTML={{
-            __html: DOMPurify.sanitize(combinedStructuredData),
-          }}
-        />
-      </Head>
-    </>
-  )
-}
+      }}
+    />
+    <NextSeo
+      title={title}
+      description={description}
+      noindex={noindex}
+      openGraph={{
+        title,
+        description,
+        images: [
+          {
+            url: `${env.NEXT_PUBLIC_DOMAIN}/_next/image?url=${mainImage}&w=1200&q=95`,
+          },
+        ],
+      }}
+    />
+  </>
+)
