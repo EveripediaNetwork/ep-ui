bee620005c8d705da588a22b512e8f4429ed05f7
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-02-26 19:37:12 +0530

    Add Binance and Upbit icons

---
+++ src/components/Icons/binance.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Icon, IconProps } from '@chakra-ui/react'
+
+const Binance = (props: IconProps) => {
+  return (
+    <Icon viewBox="0 0 32 31" {...props}>
+      <g clip-path="url(#clip0_31447_124672)">
+        <path
+          d="M3.43212 11.6279L0 15.0184L3.43212 18.4088L6.86425 15.0184L3.43212 11.6279ZM15.2271 6.78095L21.1245 12.6069L24.5567 9.21636L15.2271 0L5.89745 9.21636L9.32964 12.6069L15.2271 6.78095ZM27.022 11.6279L23.59 15.0184L27.022 18.4088L30.4542 15.0184L27.022 11.6279ZM15.2271 23.2797L9.32964 17.4538L5.89745 20.8442L15.2271 30.0606L24.5567 20.8442L21.1245 17.4538L15.2271 23.2797ZM15.2271 18.4327L18.6592 15.0422L15.2271 11.6518L11.7949 15.0422L15.2271 18.4327Z"
+          fill="#F0B90B"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_31447_124672">
+          <rect width="31" height="31" fill="white" />
+        </clipPath>
+      </defs>
+    </Icon>
+  )
+}
+
+export default Binance
