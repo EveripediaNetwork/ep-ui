132e9392b0ccf527bad73948ad43ab03dd3cb35e
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-03-04 23:10:36 +0100

    added contact input for suggestion modal

--- src/components/Event/SuggestEventModal.tsx
+++ src/components/Event/SuggestEventModal.tsx
@@ -8,28 +8,57 @@ import {
 } from '../ui/dialog'
 import { Button } from '../ui/button'
 import { Textarea } from '../ui/textarea'
+import { Input } from '../ui/input'
 
 const SuggestEventModal = () => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const formData = new FormData(e.currentTarget)
+    const email = formData.get('email')
+    const suggestions = formData.get('suggestions')
+
+    console.log({ email, suggestions })
+  }
+
   return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle className="text-base">Suggest Event</DialogTitle>
-        <DialogDescription className="text-xs">
-          Didn’t find your event of interest? Suggest an event you would be
-          excited to attend and we’ll provide you with the rest of the details.
-        </DialogDescription>
-      </DialogHeader>
-      <Textarea
-        placeholder="Details"
-        className="resize-none border border-gray300 dark:border-alpha-300"
-        rows={8}
-      />
-      <DialogFooter>
-        <Button type="submit" className="bg-brand-500 dark:bg-brand-800 px-10">
-          Submit
-        </Button>
-      </DialogFooter>
-    </DialogContent>
+    <>
+      <DialogContent>
+        <form onSubmit={handleSubmit}>
+          <DialogHeader className="mb-3">
+            <DialogTitle className="text-base dark:text-alpha-800">
+              Suggest Event
+            </DialogTitle>
+            <DialogDescription className="text-xs dark:text-alpha-800">
+              Didn’t find your event of interest? Suggest an event you would be
+              excited to attend and we’ll provide you with the rest of the
+              details.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid w-full items-center gap-1">
+            <label htmlFor="email" className="text-sm dark:text-alpha-800">
+              Email (Optional)
+            </label>
+            <Input name="email" type="email" placeholder="mabel@braindao.com" />
+          </div>
+          <Textarea
+            placeholder="write your wiki suggestions here..."
+            name="suggestions"
+            className="resize-none mt-4 border border-gray300 dark:border-alpha-300"
+            rows={8}
+          />
+          <DialogFooter className="mt-5 sm:justify-start">
+            <Button
+              type="submit"
+              onClick={() => console.log('clicked')}
+              className="bg-brand-500 dark:bg-brand-800 py-1 h-auto px-20"
+            >
+              Submit
+            </Button>
+          </DialogFooter>
+        </form>
+      </DialogContent>
+    </>
   )
 }
 
