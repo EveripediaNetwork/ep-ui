7220958de0c6220cb8ef838e0c06a19ae26430e7
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-03-06 17:04:00 +0530

    Refactor useStream hook

--- src/hooks/useStream/index.ts
+++ src/hooks/useStream/index.ts
@@ -1,4 +1,7 @@
-import { generateEventsSchema, generateOutputSchema } from './schema'
+import {
+  //  generateEventsSchema,
+  generateOutputSchema,
+} from './schema'
 import { useDispatch } from 'react-redux'
 import { setIsError, setIsLoading } from '@/store/slices/stream-slice'
 import {
@@ -9,11 +12,7 @@ import {
 } from '@/store/slices/chatbot-slice'
 import { randomUUID } from 'crypto'
 import { useAppSelector } from '@/store/hook'
-import {
-  EventStreamContentType,
-  fetchEventSource,
-} from '@fortaine/fetch-event-source'
-import { env } from '@/env.mjs'
+import axios from 'axios'
 
 const useStream = () => {
   const dispatch = useDispatch()
@@ -26,7 +25,7 @@ const useStream = () => {
     question: string
     query?: string
   }) => {
-    const ctrl = new AbortController()
+    // const ctrl = new AbortController()
     if (!question) return
 
     if (question === 'Ask me about crypto') {
@@ -36,71 +35,36 @@ const useStream = () => {
         ),
       )
     } else {
-      try {
-        const requestObject = {
-          enableStream: true,
-          search: query ? query : question,
-          language: 'en',
-          isChat: true,
-        }
-        dispatch(setCurrentMessage(question))
-        dispatch(setIsLoading(true))
-        dispatch(setIsError(false))
-        await fetchEventSource('https://iqgpt.com/api/generate', {
-          method: 'POST',
-          signal: ctrl.signal,
-          headers: {
-            'Content-Type': 'application/json',
-            'x-auth-key': `${env.NEXT_PUBLIC_BOT_API_KEY}`,
-          },
-          body: JSON.stringify(requestObject),
-          openWhenHidden: true,
-          async onopen(response) {
-            if (response.status === 500) {
-              throw new Error('Something Went Wrong')
-            }
-            if (
-              response.ok &&
-              response.headers.get('content-type') === EventStreamContentType
-            ) {
-              return
-            }
-          },
-          onmessage: (event) => {
-            if (event.event === generateEventsSchema.Enum.FINAL_OUTPUT) {
-              const { chat, answer, answerSources, messageId } =
-                generateOutputSchema.parse(JSON.parse(event.data))
-
-              if (chat && !currentChatId) {
-                dispatch(setCurrentChatId(chat.id))
-              }
-
-              dispatch(
-                addMessage({
-                  id: String(messageId) || randomUUID(),
-                  answer:
-                    answer ?? 'Sorry, I could not find an answer to that.',
-                  search: question,
-                  answerSources,
-                }),
-              )
-            }
-          },
-          onclose() {
-            ctrl.abort()
-          },
-          onerror(err) {
-            setIsError(true)
-            console.log(err)
-            throw new Error(err)
-          },
+      dispatch(setCurrentMessage(question))
+      dispatch(setIsLoading(true))
+      dispatch(setIsError(false))
+      await axios
+        .post('/api/generate', {
+          question: query ? query : question,
         })
-      } catch (err) {
-        console.log(err)
-        dispatch(setIsError(true))
-      } finally {
-        dispatch(setIsLoading(false))
-      }
+        .then((res) => {
+          const { chat, answer, answerSources, messageId } =
+            generateOutputSchema.parse(res.data)
+          if (chat && !currentChatId) {
+            dispatch(setCurrentChatId(chat.id))
+          }
+          dispatch(
+            addMessage({
+              id: String(messageId) || randomUUID(),
+              answer: answer ?? 'Sorry, I could not find an answer to that.',
+              search: question,
+              answerSources,
+            }),
+          )
+        })
+        .catch((err) => {
+          setIsError(true)
+          console.log(err)
+          throw new Error(err)
+        })
+        .finally(() => {
+          dispatch(setIsLoading(false))
+        })
     }
   }
 
