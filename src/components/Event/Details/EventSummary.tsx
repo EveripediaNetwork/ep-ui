1d70aacf9612c066d99d34ba18293417030a6065
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-03-10 22:41:56 +0100

    dome with working on tags routing

--- src/components/Event/Details/EventSummary.tsx
+++ src/components/Event/Details/EventSummary.tsx
@@ -49,22 +49,22 @@ const EventSummary = ({ event }: { event: Wiki }) => {
           Register/Get Tickets
         </Link>
       )}
-      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
+      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
         <span className="col-span-1">Location</span>
-        <span className="max-w-[163px] md:max-w-full lg:max-w-[119px] xl:max-w-full">
+        <span className="max-w-[163px] md:max-w-full lg:max-w-[119px] xl:col-span-2 xl:max-w-full">
           Le Carrousel du Louvre (Paris, France)
         </span>
       </span>
-      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
+      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
         <span className="col-span-1">Date</span>
-        <span>
+        <span className="xl:col-span-2">
           {event.events && new Date(event.events?.[0].date).toDateString()}
         </span>
       </span>
       {social_profiles.length > 0 && (
-        <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
+        <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 text-xs lg:text-[10px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
           <span className="col-span-1">Social profiles</span>
-          <span className="flex text-2xl md:text-xl xl:text-2xl items-center gap-1">
+          <span className="flex text-2xl xl:col-span-2 md:text-xl xl:text-2xl items-center gap-1">
             {social_profiles.map((socials) => (
               <Link key={socials.id} href={`${socials.value}`} target="_blank">
                 {socials.id === 'twitter_profile' ? (
@@ -81,16 +81,17 @@ const EventSummary = ({ event }: { event: Wiki }) => {
           </span>
         </span>
       )}
-      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 text-xs lg:text-[8px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
+      <span className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 text-xs lg:text-[8px] xl:text-xs font-medium rounded-lg bg-gray100 dark:bg-gray700 items-center px-4 lg:px-2 xl:px-4 py-3">
         <span className="col-span-1">Tags</span>
-        <span className="flex flex-1 max-w-[209px] md:col-span-2 lg:col-span-1 md:max-w-full lg:max-w-[150px] xl:max-w-[219px] gap-1 xl:gap-2 flex-wrap">
+        <span className="flex flex-1 max-w-[209px] md:col-span-2 lg:col-span-1 xl:col-span-2 md:max-w-full lg:max-w-[150px] xl:max-w-[219px] gap-1 xl:gap-2 flex-wrap">
           {event.tags.map((tag) => (
-            <span
+            <Link
+              href={`/events?eventType=${tag.id}`}
               key={tag.id}
               className="rounded-full px-2 py-1 border border-gray300 dark:border-alpha-300"
             >
               {tag.id}
-            </span>
+            </Link>
           ))}
         </span>
       </span>
