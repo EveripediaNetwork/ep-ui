da8329e07919246ae7dc8022251d981089e57705
Adebesin Cell <adebesintolulope80@gmail.com>
2024-03-20 18:47:28 +0100

    Fixes useAddress to keep injected connector state

--- src/components/CreateWiki/CreateWikiTopBar/WikiPublish/WikiPublishButton.tsx
+++ src/components/CreateWiki/CreateWikiTopBar/WikiPublish/WikiPublishButton.tsx
@@ -44,7 +44,7 @@ const NetworkErrorNotification = dynamic(
 )
 
 export const WikiPublishButton = () => {
-  const wiki = useAppSelector(state => state.wiki)
+  const wiki = useAppSelector((state) => state.wiki)
   const [submittingWiki, setSubmittingWiki] = useBoolean()
   const { address: userAddress, isConnected: isUserConnected } = useAddress()
   const { userCanEdit } = useWhiteListValidator(userAddress)
@@ -130,15 +130,16 @@ export const WikiPublishButton = () => {
       getDetectedProvider()
     } else {
       getConnectedChain(detectedProvider)
-      detectedProvider.on('chainChanged', newlyConnectedChain =>
+      detectedProvider.on('chainChanged', (newlyConnectedChain) =>
         setConnectedChainId(newlyConnectedChain),
       )
     }
 
     return () => {
       if (detectedProvider) {
-        detectedProvider.removeListener('chainChanged', newlyConnectedChain =>
-          setConnectedChainId(newlyConnectedChain),
+        detectedProvider.removeListener(
+          'chainChanged',
+          (newlyConnectedChain) => setConnectedChainId(newlyConnectedChain),
         )
       }
     }
@@ -247,7 +248,7 @@ export const WikiPublishButton = () => {
         ...wiki,
         user: { id: userAddress },
         content: sanitizeContentToPublish(String(wiki.content)),
-        metadata: wiki.metadata.filter(m => m.value),
+        metadata: wiki.metadata.filter((m) => m.value),
       }
 
       console.log(finalWiki)
--- src/hooks/useAddress.ts
+++ src/hooks/useAddress.ts
@@ -18,6 +18,7 @@ export const useAddress = () => {
 
     if (isInjectorConnected) {
       setIsConnected(isInjectorConnected)
+      return
     }
 
     setIsConnected(isWalletConnected && isConnectorConnected)
