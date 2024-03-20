3a7eec7b6d972d132fb0e30d96128432c1d6da39
Adebesin Cell <adebesintolulope80@gmail.com>
2024-03-20 20:19:52 +0100

    fixes formatting

--- src/components/Event/TrendingEvent.tsx
+++ src/components/Event/TrendingEvent.tsx
@@ -26,7 +26,7 @@ export const TrendingEventsCard = ({ events }: { events: TEvents[] }) => {
         }}
       >
         <CarouselContent className="">
-          {events.map(event => (
+          {events.map((event) => (
             <CarouselItem
               className="h-[350px] xl:h-[300px] overflow-hidden basis-[80%] md:basis-[40%] xl:basis-[309px] rounded-xl relative ml-4"
               key={event.id}
