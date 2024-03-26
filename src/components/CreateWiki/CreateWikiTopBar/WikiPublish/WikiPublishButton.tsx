197f09051f8f6551b073487e8ab754c1f41d4ef5
Adebesin Cell <adebesintolulope80@gmail.com>
2024-03-26 17:34:05 +0100

    fixes formatting

--- src/components/CreateWiki/CreateWikiTopBar/WikiPublish/WikiPublishButton.tsx
+++ src/components/CreateWiki/CreateWikiTopBar/WikiPublish/WikiPublishButton.tsx
@@ -45,7 +45,7 @@ const NetworkErrorNotification = dynamic(
 )
 
 export const WikiPublishButton = () => {
-  const wiki = useAppSelector(state => state.wiki)
+  const wiki = useAppSelector((state) => state.wiki)
   const [submittingWiki, setSubmittingWiki] = useBoolean()
   const { address: userAddress, isConnected: isUserConnected } = useAccount()
   const { userCanEdit } = useWhiteListValidator()
@@ -138,15 +138,16 @@ export const WikiPublishButton = () => {
       getDetectedProvider()
     } else {
       getConnectedChain(detectedProvider)
-      detectedProvider.on('chainChanged', newlyConnectedChain =>
+      detectedProvider.on('chainChanged', (newlyConnectedChain) =>
         setConnectedChainId(newlyConnectedChain),
       )
     }
 
     return () => {
       if (detectedProvider) {
-        detectedProvider.removeListener('chainChanged', newlyConnectedChain =>
-          setConnectedChainId(newlyConnectedChain),
+        detectedProvider.removeListener(
+          'chainChanged',
+          (newlyConnectedChain) => setConnectedChainId(newlyConnectedChain),
         )
       }
     }
@@ -252,7 +253,7 @@ export const WikiPublishButton = () => {
         ...wiki,
         user: { id: userAddress },
         content: sanitizeContentToPublish(String(wiki.content)),
-        metadata: wiki.metadata.filter(m => m.value),
+        metadata: wiki.metadata.filter((m) => m.value),
       }
 
       if (finalWiki.id === CreateNewWikiSlug)
