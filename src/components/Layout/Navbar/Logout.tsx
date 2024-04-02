6f286fa001a09c120d7946051d29a157846dfe6c
Tolulope Malomo <malomo.tolz@gmail.com>
2024-04-02 16:34:24 +0100

    fix login issue

--- src/components/Layout/Navbar/Logout.tsx
+++ src/components/Layout/Navbar/Logout.tsx
@@ -8,6 +8,9 @@ import { useTranslation } from 'next-i18next'
 import { useAddress } from '@/hooks/useAddress'
 import { deleteCookie } from 'cookies-next'
 import { cookieNames } from '@/types/cookies'
+import { disconnect } from '@wagmi/core'
+import { wagmiConfig } from '@/config/wagmi'
+import { WagmiWrapper } from '../WagmiWrapper'
 
 export const LogOutBtn = ({ isInMobileMenu }: { isInMobileMenu: boolean }) => {
   const { address: isUserConnected } = useAddress()
@@ -18,43 +21,41 @@ export const LogOutBtn = ({ isInMobileMenu }: { isInMobileMenu: boolean }) => {
   const handleLogOut = async () => {
     dispatch(setStateToDefault())
     deleteCookie(cookieNames.Enum['x-auth-token'])
-    for (const key of Object.keys(localStorage)) {
-      if (key.startsWith('wagmi') && key !== 'wagmi.store') {
-        localStorage.removeItem(key)
-      }
-    }
+    await disconnect(wagmiConfig)
     router.push(router.asPath)
   }
 
   return (
-    <Button
-      minH={{ base: '35px', md: '48px' }}
-      px={isInMobileMenu ? 0 : 3}
-      bgColor="transparent"
-      sx={{ '&:hover, &:focus, &:active': { bgColor: 'subMenuHoverBg' } }}
-      onClick={isUserConnected ? handleLogOut : undefined}
-      cursor={isUserConnected ? 'pointer' : 'not-allowed'}
-      display={isUserConnected ? 'flex' : 'none'}
-      w="full"
-    >
-      <Icon
-        fontSize="4xl"
-        color="linkColor"
-        fontWeight={600}
-        as={RiLogoutBoxRFill}
-        pr={3}
-      />
-      <Flex
-        ml={isInMobileMenu ? 2 : 'unset'}
-        fontSize="md"
-        fontWeight="semibold"
-        color="linkColor"
-        flex="auto"
+    <WagmiWrapper>
+      <Button
+        minH={{ base: '35px', md: '48px' }}
+        px={isInMobileMenu ? 0 : 3}
+        bgColor="transparent"
+        sx={{ '&:hover, &:focus, &:active': { bgColor: 'subMenuHoverBg' } }}
+        onClick={isUserConnected ? handleLogOut : undefined}
+        cursor={isUserConnected ? 'pointer' : 'not-allowed'}
+        display={isUserConnected ? 'flex' : 'none'}
+        w="full"
       >
-        <span style={isInMobileMenu ? { fontSize: 18 } : {}}>
-          {t('Logout')}
-        </span>
-      </Flex>
-    </Button>
+        <Icon
+          fontSize="4xl"
+          color="linkColor"
+          fontWeight={600}
+          as={RiLogoutBoxRFill}
+          pr={3}
+        />
+        <Flex
+          ml={isInMobileMenu ? 2 : 'unset'}
+          fontSize="md"
+          fontWeight="semibold"
+          color="linkColor"
+          flex="auto"
+        >
+          <span style={isInMobileMenu ? { fontSize: 18 } : {}}>
+            {t('Logout')}
+          </span>
+        </Flex>
+      </Button>
+    </WagmiWrapper>
   )
 }
