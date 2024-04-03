783370899ba51932ae282fd424d77fe0c4f1a51d
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-04-03 11:28:13 +0100

    Add cmcTokenData interface to cmcTokenDetails/index.ts

--- src/services/cmcTokenDetails/index.ts
+++ src/services/cmcTokenDetails/index.ts
@@ -1,6 +1,54 @@
 import { HYDRATE } from 'next-redux-wrapper'
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+interface cmcTokenData {
+  [tokenName: string]: {
+    id: number
+    name: string
+    symbol: string
+    slug: string
+    num_market_pairs: number
+    date_added: string
+    tags: string[]
+    max_supply: number
+    circulating_supply: number
+    total_supply: number
+    platform: {
+      id: number
+      name: string
+      symbol: string
+      slug: string
+      token_address: string
+    }
+    is_active: number
+    infinite_supply: boolean
+    cmc_rank: number
+    is_fiat: number
+    self_reported_circulating_supply: number
+    self_reported_market_cap: number
+    tvl_ratio: null
+    last_updated: string
+    quote: {
+      USD: {
+        price: number
+        volume_24h: number
+        volume_change_24h: number
+        percent_change_1h: number
+        percent_change_24h: number
+        percent_change_7d: number
+        percent_change_30d: number
+        percent_change_60d: number
+        percent_change_90d: number
+        market_cap: number
+        market_cap_dominance: number
+        fully_diluted_market_cap: number
+        tvl: null
+        last_updated: string
+      }
+    }
+  }
+}
+
 export const cmcTokenDataApi = createApi({
   reducerPath: 'cmcTokenDataApi',
   extractRehydrationInfo(action, { reducerPath }) {
@@ -15,62 +63,12 @@ export const cmcTokenDataApi = createApi({
   refetchOnMountOrArgChange: 60,
   refetchOnFocus: true,
   endpoints: (builder) => ({
-    getCmcTokenData: builder.query<
-      {
-        [tokenName: string]: {
-          id: number
-          name: string
-          symbol: string
-          slug: string
-          num_market_pairs: number
-          date_added: string
-          tags: string[]
-          max_supply: number
-          circulating_supply: number
-          total_supply: number
-          platform: {
-            id: number
-            name: string
-            symbol: string
-            slug: string
-            token_address: string
-          }
-          is_active: number
-          infinite_supply: boolean
-          cmc_rank: number
-          is_fiat: number
-          self_reported_circulating_supply: number
-          self_reported_market_cap: number
-          tvl_ratio: null
-          last_updated: string
-          quote: {
-            USD: {
-              price: number
-              volume_24h: number
-              volume_change_24h: number
-              percent_change_1h: number
-              percent_change_24h: number
-              percent_change_7d: number
-              percent_change_30d: number
-              percent_change_60d: number
-              percent_change_90d: number
-              market_cap: number
-              market_cap_dominance: number
-              fully_diluted_market_cap: number
-              tvl: null
-              last_updated: string
-            }
-          }
-        }
-      },
-      string
-    >({
+    getCmcTokenData: builder.query<cmcTokenData, string>({
       query: (tokenName) => `?tokenName=${tokenName}`,
       transformResponse: (response: any) => {
         if (response?.response?.data) {
           return response.response.data
         }
-
         return undefined
       },
     }),
