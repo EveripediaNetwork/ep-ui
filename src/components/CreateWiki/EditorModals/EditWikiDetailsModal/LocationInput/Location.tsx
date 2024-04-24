e63c839a669fa86cf40b6853630aea50135ddac0
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-04-24 10:35:06 +0100

    updating location data logic to resolve edge cases

--- src/components/CreateWiki/EditorModals/EditWikiDetailsModal/LocationInput/Location.tsx
+++ src/components/CreateWiki/EditorModals/EditWikiDetailsModal/LocationInput/Location.tsx
@@ -12,14 +12,16 @@ const Location = () => {
   const parsedLocation = locationString ? JSON.parse(locationString) : ''
   const location = Array.isArray(parsedLocation)
     ? parsedLocation
-    : [parsedLocation]
+    : parsedLocation
 
   const removeLocation = (year?: string) => {
     let newLocation = []
     if (Array.isArray(parsedLocation)) {
-      newLocation = location.filter((item) => item?.year !== year)
+      newLocation = location.filter(
+        (item: { year: string | undefined }) => item?.year !== year,
+      )
     } else {
-      newLocation = location.filter((item) => item?.year)
+      newLocation = location.filter((item: { year: any }) => item?.year)
     }
     dispatch({
       type: 'wiki/updateMetadata',
@@ -33,77 +35,79 @@ const Location = () => {
   return (
     <Flex gap={2} flexWrap={'wrap'}>
       {location.length > 0 &&
-        location.map((item, index) => (
-          <Flex
-            gap="2"
-            alignItems="center"
-            borderRadius="md"
-            bg="gray.200"
-            px="2"
-            _dark={{
-              bg: 'whiteAlpha.200',
-            }}
-            w="max-content"
-            key={item?.year || index}
-          >
-            <Flex alignItems="center" gap="1" flexShrink={0}>
-              <Icon as={VscLocation} w="16px" h="16px" />
-            </Flex>
-            <Button
-              w="max-content"
-              display="flex"
-              gap={2}
-              color="black"
-              _active={{
-                bg: 'gray.100',
-              }}
-              _hover={{
-                bg: 'gray.100',
-              }}
+        location.map(
+          (item: { year: string | undefined; country: any }, index: any) => (
+            <Flex
+              gap="2"
+              alignItems="center"
+              borderRadius="md"
+              bg="gray.200"
+              px="2"
               _dark={{
-                color: 'white',
-                bg: 'whiteAlpha.100',
+                bg: 'whiteAlpha.200',
               }}
-              borderLeftRadius="0"
-              size="xs"
-              px={2}
+              w="max-content"
+              key={item?.year || index}
             >
-              <Text
-                fontWeight="normal"
-                fontSize="xs"
-                noOfLines={1}
-                display="inline-block"
-                overflowX="hidden"
-                textOverflow="ellipsis"
-                whiteSpace="nowrap"
-                maxWidth="150px"
+              <Flex alignItems="center" gap="1" flexShrink={0}>
+                <Icon as={VscLocation} w="16px" h="16px" />
+              </Flex>
+              <Button
+                w="max-content"
+                display="flex"
+                gap={2}
+                color="black"
+                _active={{
+                  bg: 'gray.100',
+                }}
+                _hover={{
+                  bg: 'gray.100',
+                }}
+                _dark={{
+                  color: 'white',
+                  bg: 'whiteAlpha.100',
+                }}
+                borderLeftRadius="0"
+                size="xs"
+                px={2}
               >
-                {item?.year
-                  ? `${item?.country} - ${item?.year}`
-                  : `${item?.country}`}
-              </Text>
-            </Button>
-            <Center
-              flexShrink={0}
-              boxSize={4}
-              fontSize="xs"
-              fontWeight="bold"
-              lineHeight="none"
-              color="red.100"
-              bg="red.400"
-              _hover={{
-                bg: 'red.500',
-              }}
-              rounded="full"
-              onClick={(e) => {
-                e.stopPropagation()
-                removeLocation(item?.year)
-              }}
-            >
-              <Icon as={RiCloseLine} w="16px" h="16px" />
-            </Center>
-          </Flex>
-        ))}
+                <Text
+                  fontWeight="normal"
+                  fontSize="xs"
+                  noOfLines={1}
+                  display="inline-block"
+                  overflowX="hidden"
+                  textOverflow="ellipsis"
+                  whiteSpace="nowrap"
+                  maxWidth="150px"
+                >
+                  {item?.year
+                    ? `${item?.country} - ${item?.year}`
+                    : `${item?.country}`}
+                </Text>
+              </Button>
+              <Center
+                flexShrink={0}
+                boxSize={4}
+                fontSize="xs"
+                fontWeight="bold"
+                lineHeight="none"
+                color="red.100"
+                bg="red.400"
+                _hover={{
+                  bg: 'red.500',
+                }}
+                rounded="full"
+                onClick={(e) => {
+                  e.stopPropagation()
+                  removeLocation(item?.year)
+                }}
+              >
+                <Icon as={RiCloseLine} w="16px" h="16px" />
+              </Center>
+            </Flex>
+          ),
+        )}
     </Flex>
   )
 }
