c29b6f42350d6fa9e069a85442b857855a0d87d5
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-04-29 17:57:42 +0100

    resolve location rendering on edit wiki details modal

--- src/components/CreateWiki/EditorModals/EditWikiDetailsModal/LocationInput/Location.tsx
+++ src/components/CreateWiki/EditorModals/EditWikiDetailsModal/LocationInput/Location.tsx
@@ -4,15 +4,86 @@ import { MData } from '@everipedia/iq-utils'
 import React from 'react'
 import { RiCloseLine } from 'react-icons/ri'
 import { VscLocation } from 'react-icons/vsc'
+import { TLocationType } from './location.type'
+
+const LocationItem = (props: TLocationType) => {
+  const { year, country, removeLocation } = props
+  return (
+    <Flex
+      gap="2"
+      alignItems="center"
+      borderRadius="md"
+      bg="gray.200"
+      px="2"
+      _dark={{
+        bg: 'whiteAlpha.200',
+      }}
+      w="max-content"
+    >
+      <Flex alignItems="center" gap="1" flexShrink={0}>
+        <Icon as={VscLocation} w="16px" h="16px" />
+      </Flex>
+      <Button
+        w="max-content"
+        display="flex"
+        gap={2}
+        color="black"
+        _active={{
+          bg: 'gray.100',
+        }}
+        _hover={{
+          bg: 'gray.100',
+        }}
+        _dark={{
+          color: 'white',
+          bg: 'whiteAlpha.100',
+        }}
+        borderLeftRadius="0"
+        size="xs"
+        px={2}
+      >
+        <Text
+          fontWeight="normal"
+          fontSize="xs"
+          noOfLines={1}
+          display="inline-block"
+          overflowX="hidden"
+          textOverflow="ellipsis"
+          whiteSpace="nowrap"
+          maxWidth="150px"
+        >
+          {`${country} - ${year}`}
+        </Text>
+      </Button>
+      <Center
+        flexShrink={0}
+        boxSize={4}
+        fontSize="xs"
+        fontWeight="bold"
+        lineHeight="none"
+        color="red.100"
+        bg="red.400"
+        _hover={{
+          bg: 'red.500',
+        }}
+        rounded="full"
+        onClick={(e) => {
+          e.stopPropagation()
+          removeLocation(year)
+        }}
+      >
+        <Icon as={RiCloseLine} w="16px" h="16px" />
+      </Center>
+    </Flex>
+  )
+}
 
 const Location = () => {
   const dispatch = useAppDispatch()
   const metadata = useAppSelector((state) => state.wiki.metadata)
   const locationString = metadata.find((m: MData) => m.id === 'location')?.value
   const parsedLocation = locationString ? JSON.parse(locationString) : ''
-  const location = Array.isArray(parsedLocation)
-    ? parsedLocation
-    : parsedLocation
+  const location = parsedLocation
 
   const removeLocation = (year?: string) => {
     let newLocation = []
@@ -21,7 +92,7 @@ const Location = () => {
         (item: { year: string | undefined }) => item?.year !== year,
       )
     } else {
-      newLocation = location.filter((item: { year: any }) => item?.year)
+      newLocation = []
     }
     dispatch({
       type: 'wiki/updateMetadata',
@@ -34,80 +105,24 @@ const Location = () => {
 
   return (
     <Flex gap={2} flexWrap={'wrap'}>
-      {location.length > 0 &&
+      {location.length > 0 ? (
         location.map(
           (item: { year: string | undefined; country: any }, index: any) => (
-            <Flex
-              gap="2"
-              alignItems="center"
-              borderRadius="md"
-              bg="gray.200"
-              px="2"
-              _dark={{
-                bg: 'whiteAlpha.200',
-              }}
-              w="max-content"
-              key={item?.year || index}
-            >
-              <Flex alignItems="center" gap="1" flexShrink={0}>
-                <Icon as={VscLocation} w="16px" h="16px" />
-              </Flex>
-              <Button
-                w="max-content"
-                display="flex"
-                gap={2}
-                color="black"
-                _active={{
-                  bg: 'gray.100',
-                }}
-                _hover={{
-                  bg: 'gray.100',
-                }}
-                _dark={{
-                  color: 'white',
-                  bg: 'whiteAlpha.100',
-                }}
-                borderLeftRadius="0"
-                size="xs"
-                px={2}
-              >
-                <Text
-                  fontWeight="normal"
-                  fontSize="xs"
-                  noOfLines={1}
-                  display="inline-block"
-                  overflowX="hidden"
-                  textOverflow="ellipsis"
-                  whiteSpace="nowrap"
-                  maxWidth="150px"
-                >
-                  {item?.year
-                    ? `${item?.country} - ${item?.year}`
-                    : `${item?.country}`}
-                </Text>
-              </Button>
-              <Center
-                flexShrink={0}
-                boxSize={4}
-                fontSize="xs"
-                fontWeight="bold"
-                lineHeight="none"
-                color="red.100"
-                bg="red.400"
-                _hover={{
-                  bg: 'red.500',
-                }}
-                rounded="full"
-                onClick={(e) => {
-                  e.stopPropagation()
-                  removeLocation(item?.year)
-                }}
-              >
-                <Icon as={RiCloseLine} w="16px" h="16px" />
-              </Center>
-            </Flex>
+            <LocationItem
+              key={item.year || index}
+              year={item.year}
+              country={item.country}
+              removeLocation={removeLocation}
+            />
           ),
-        )}
+        )
+      ) : (
+        <LocationItem
+          year={location.year}
+          country={location.country}
+          removeLocation={removeLocation}
+        />
+      )}
     </Flex>
   )
 }
