ea5e098613d8bc7e55125091d92576311e5a1069
Adebesin Cell <adebesintolulope80@gmail.com>
2024-05-03 18:58:34 +0100

    fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import { Flex } from '@chakra-ui/react'
-import {
-  getPromotedWikis,
-  getTrendingWikis,
-  getWikis,
-  wikiApi,
-} from '@/services/wikis'
+import { getPromotedWikis, getTrendingWikis, getWikis } from '@/services/wikis'
 import { store } from '@/store/store'
-import { getTags, tagsApi } from '@/services/tags'
-import { editorApi, getLeaderboard } from '@/services/editor'
+import { getTags } from '@/services/tags'
+import { getLeaderboard } from '@/services/editor'
 import { sortLeaderboards } from '@/utils/DataTransform/leaderboard.utils'
-import { nftLisitngAPI } from '@/services/nftlisting'
 import {
   getAiTokenRanking,
   getNFTRanking,
   getTokenRanking,
   getStableCoinRanking,
   getFoundersRanking,
-  rankingAPI,
 } from '@/services/ranking'
 import { Hero } from '@/components/Landing/Hero'
 import { DayRangeType, getDateRange } from '@/utils/HomepageUtils/getDate'
@@ -67,12 +60,11 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const { startDay: weekStartDay, endDay: weekEndDay } = getDateRange({
     rangeType: DayRangeType.LAST_WEEK,
   })
-
   const { startDay: monthStartDay, endDay: monthEndDay } = getDateRange({
     rangeType: DayRangeType.LAST_MONTH,
   })
 
-  console.log({
+  console.log('Date Ranges', {
     todayStartDay,
     todayEndDay,
     weekStartDay,
@@ -81,146 +73,155 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
     monthEndDay,
   })
 
-  const { data: promotedWikis, error: promotedWikisError } =
-    await store.dispatch(getPromotedWikis.initiate())
-  const { data: recent, error: recentError } = await store.dispatch(
-    getWikis.initiate(),
-  )
-  const { data: leaderboard, error: leaderboardError } = await store.dispatch(
-    getLeaderboard.initiate(),
-  )
-  const { data: tagsData, error: tagsDataError } = await store.dispatch(
-    getTags.initiate({
-      startDate: Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 30,
-      endDate: Math.floor(Date.now() / 1000),
-    }),
-  )
+  try {
+    const results = await Promise.all([
+      store.dispatch(getPromotedWikis.initiate()),
+      store.dispatch(getWikis.initiate()),
+      store.dispatch(getLeaderboard.initiate()),
+      store.dispatch(
+        getTags.initiate({
+          startDate: Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 30,
+          endDate: Math.floor(Date.now() / 1000),
+        }),
+      ),
+      store.dispatch(
+        getNFTRanking.initiate({
+          kind: 'NFT',
+          limit: RANKING_LIST_LIMIT,
+          offset: 1,
+        }),
+      ),
+      store.dispatch(
+        getTokenRanking.initiate({
+          kind: 'TOKEN',
+          limit: RANKING_LIST_LIMIT,
+          offset: 1,
+        }),
+      ),
+      store.dispatch(
+        getFoundersRanking.initiate({
+          kind: 'TOKEN',
+          limit: RANKING_LIST_LIMIT,
+          offset: 1,
+          founders: true,
+        }),
+      ),
+      store.dispatch(
+        getAiTokenRanking.initiate({
+          kind: 'TOKEN',
+          limit: RANKING_LIST_LIMIT,
+          offset: 1,
+          category: 'AI',
+        }),
+      ),
+      store.dispatch(
+        getStableCoinRanking.initiate({
+          kind: 'TOKEN',
+          limit: RANKING_LIST_LIMIT,
+          offset: 1,
+          category: 'STABLE_COINS',
+        }),
+      ),
+      store.dispatch(
+        getTrendingWikis.initiate({
+          amount: TRENDING_WIKIS_AMOUNT,
+          startDay: todayStartDay,
+          endDay: todayEndDay,
+        }),
+      ),
+      store.dispatch(
+        getTrendingWikis.initiate({
+          amount: TRENDING_WIKIS_AMOUNT,
+          startDay: weekStartDay,
+          endDay: weekEndDay,
+        }),
+      ),
+      store.dispatch(
+        getTrendingWikis.initiate({
+          amount: TRENDING_WIKIS_AMOUNT,
+          startDay: monthStartDay,
+          endDay: monthEndDay,
+        }),
+      ),
+    ])
 
-  const { data: NFTsList, error: NFTsListError } = await store.dispatch(
-    getNFTRanking.initiate({
-      kind: 'NFT',
-      limit: RANKING_LIST_LIMIT,
-      offset: 1,
-    }),
-  )
+    const [
+      promotedWikis,
+      recent,
+      leaderboard,
+      tagsData,
+      NFTsList,
+      TokensList,
+      foundersData,
+      aiTokensList,
+      stableCoinsList,
+      todayTrending,
+      weekTrending,
+      monthTrending,
+    ] = results.map(r => r.data)
+    const errors = results.map(r => r.error).filter(e => e)
 
-  const { data: TokensList, error: TokensListError } = await store.dispatch(
-    getTokenRanking.initiate({
-      kind: 'TOKEN',
-      limit: RANKING_LIST_LIMIT,
-      offset: 1,
-    }),
-  )
-  const { data: foundersData, error: foundersDataError } = await store.dispatch(
-    getFoundersRanking.initiate({
-      kind: 'TOKEN',
-      limit: RANKING_LIST_LIMIT,
-      offset: 1,
-      founders: true,
-    }),
-  )
-  const { data: aiTokensList, error: aiTokensListError } = await store.dispatch(
-    getAiTokenRanking.initiate({
-      kind: 'TOKEN',
-      limit: RANKING_LIST_LIMIT,
-      offset: 1,
-      category: 'AI',
-    }),
-  )
-  const { data: stableCoinsList, error: stableCoinsListError } =
-    await store.dispatch(
-      getStableCoinRanking.initiate({
-        kind: 'TOKEN',
-        limit: RANKING_LIST_LIMIT,
-        offset: 1,
-        category: 'STABLE_COINS',
-      }),
-    )
+    console.log('Retrieved Data', {
+      promotedWikis,
+      recent,
+      leaderboard,
+      tagsData,
+      NFTsList,
+      TokensList,
+      foundersData,
+      aiTokensList,
+      stableCoinsList,
+      todayTrending,
+      weekTrending,
+      monthTrending,
+    })
 
-  const { data: todayTrending, error: todayTrendingError } =
-    await store.dispatch(
-      getTrendingWikis.initiate({
-        amount: TRENDING_WIKIS_AMOUNT,
-        startDay: todayStartDay,
-        endDay: todayEndDay,
-      }),
-    )
+    if (errors.length > 0) {
+      console.error(
+        'Errors encountered:',
+        errors.map(e => `${e}-${e?.message}`),
+      )
+    }
 
-  const { data: weekTrending, error: weekTrendingError } = await store.dispatch(
-    getTrendingWikis.initiate({
-      amount: TRENDING_WIKIS_AMOUNT,
-      startDay: weekStartDay,
-      endDay: weekEndDay,
-    }),
-  )
+    //@ts-ignore
+    const sortedleaderboards = leaderboard ? sortLeaderboards(leaderboard) : []
+    const rankings = {
+      NFTsListing: NFTsList ?? [],
+      aiTokensListing: aiTokensList ?? [],
+      TokensListing: TokensList ?? [],
+      stableCoinsListing: stableCoinsList ?? [],
+      foundersListing: foundersData ?? [],
+    }
 
-  const { data: monthTrending, error: monthTrendingError } =
-    await store.dispatch(
-      getTrendingWikis.initiate({
-        amount: TRENDING_WIKIS_AMOUNT,
-        startDay: monthStartDay,
-        endDay: monthEndDay,
-      }),
-    )
-
-  await Promise.all([
-    store.dispatch(wikiApi.util.getRunningQueriesThunk()),
-    store.dispatch(tagsApi.util.getRunningQueriesThunk()),
-    store.dispatch(editorApi.util.getRunningQueriesThunk()),
-    store.dispatch(nftLisitngAPI.util.getRunningQueriesThunk()),
-    store.dispatch(rankingAPI.util.getRunningQueriesThunk()),
-  ])
-
-  console.error(
-    'Errors encountered:',
-    [
-      promotedWikisError,
-      recentError,
-      leaderboardError,
-      tagsDataError,
-      NFTsListError,
-      TokensListError,
-      foundersDataError,
-      aiTokensListError,
-      stableCoinsListError,
-      todayTrendingError,
-      weekTrendingError,
-      monthTrendingError,
-    ]
-      .filter((e) => e)
-      .map((e) => `${e}-${e?.message}`),
-  )
-
-  const sortedleaderboards = leaderboard ? sortLeaderboards(leaderboard) : []
-  const rankings = {
-    NFTsListing: NFTsList ?? [],
-    aiTokensListing: aiTokensList ?? [],
-    TokensListing: TokensList ?? [],
-    stableCoinsListing: stableCoinsList ?? [],
-    foundersListing: foundersData ?? [],
-  }
-
-  return {
-    props: {
-      ...(await serverSideTranslations(locale ?? 'en', [
-        'common',
-        'home',
-        'category',
-        'rank',
-        'wiki',
-        'event',
-      ])),
-      promotedWikis: promotedWikis ?? [],
-      recentWikis: recent ?? [],
-      popularTags: tagsData ?? [],
-      leaderboards: sortedleaderboards,
-      rankings: rankings,
-      trending: {
-        todayTrending: todayTrending ?? [],
-        weekTrending: weekTrending ?? [],
-        monthTrending: monthTrending ?? [],
+    return {
+      props: {
+        ...(await serverSideTranslations(locale ?? 'en', [
+          'common',
+          'home',
+          'category',
+          'rank',
+          'wiki',
+          'event',
+        ])),
+        promotedWikis: promotedWikis ?? [],
+        recentWikis: recent ?? [],
+        popularTags: tagsData ?? [],
+        leaderboards: sortedleaderboards,
+        rankings: rankings,
+        trending: {
+          todayTrending: todayTrending ?? [],
+          weekTrending: weekTrending ?? [],
+          monthTrending: monthTrending ?? [],
+        },
       },
-    },
+    }
+  } catch (error) {
+    console.error('An unexpected error occurred:', error)
+    return {
+      props: {
+        error: 'Failed to load data',
+      },
+    }
   }
 }
 
