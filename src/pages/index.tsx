847ec3d8170fd3f0158a4dd1f7edfdf014b523fe
Adebesin Cell <adebesintolulope80@gmail.com>
2024-05-03 19:36:54 +0100

    fixes homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,43 +1,70 @@
 import React from 'react'
-import { Flex } from '@chakra-ui/react'
-import { getPromotedWikis, getTrendingWikis, getWikis } from '@/services/wikis'
+import { Box, Flex } from '@chakra-ui/react'
+import dynamic from 'next/dynamic'
+import {
+  getPromotedWikis,
+  getTrendingWikis,
+  getWikis,
+  wikiApi,
+  PromotedWikisBuilder,
+} from '@/services/wikis'
 import { store } from '@/store/store'
-import { getTags } from '@/services/tags'
-import { getLeaderboard } from '@/services/editor'
+import { Wiki } from '@everipedia/iq-utils'
+import TrendingWikis from '@/components/Landing/TrendingWikis'
+const CategoriesList = dynamic(
+  () => import('@/components/Landing/CategoriesList'),
+)
+import { getTags, tagsApi } from '@/services/tags'
+const DiscoverMore = dynamic(() => import('@/components/Landing/DiscoverMore'))
+const LeaderBoard = dynamic(() => import('@/components/Landing/Leaderboard'))
+import { editorApi, getLeaderboard, LeaderBoardType } from '@/services/editor'
 import { sortLeaderboards } from '@/utils/DataTransform/leaderboard.utils'
+import { RankCardType } from '@/types/RankDataTypes'
+const RankingList = dynamic(() => import('@/components/Landing/RankingList'))
+import { nftLisitngAPI } from '@/services/nftlisting'
 import {
   getAiTokenRanking,
   getNFTRanking,
   getTokenRanking,
   getStableCoinRanking,
   getFoundersRanking,
+  rankingAPI,
 } from '@/services/ranking'
 import { Hero } from '@/components/Landing/Hero'
 import { DayRangeType, getDateRange } from '@/utils/HomepageUtils/getDate'
+import { TrendingData } from '@/types/Home'
+const AboutIqgpt = dynamic(() => import('@/components/Landing/AboutIqgpt'))
 import { GetServerSideProps } from 'next'
-// import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import { IQBar } from '@/components/Landing/IQBar'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import EventOverview from '@/components/Landing/EventOverview'
+import { IQBar } from '@/components/Landing/IQBar'
 
 const RANKING_LIST_LIMIT = 10
 const TRENDING_WIKIS_AMOUNT = 5
 
-// interface HomePageProps {
-//   promotedWikis: Wiki[]
-//   recentWikis: Wiki[]
-//   popularTags: { id: string }[]
-//   leaderboards: LeaderBoardType[]
-//   rankings: {
-//     NFTsListing: RankCardType[]
-//     aiTokensListing: RankCardType[]
-//     TokensListing: RankCardType[]
-//     stableCoinsListing: RankCardType[]
-//     foundersListing: RankCardType[]
-//   }
-//   trending: TrendingData
-// }
+interface HomePageProps {
+  promotedWikis: Wiki[]
+  recentWikis: Wiki[]
+  popularTags: { id: string }[]
+  leaderboards: LeaderBoardType[]
+  rankings: {
+    NFTsListing: RankCardType[]
+    aiTokensListing: RankCardType[]
+    TokensListing: RankCardType[]
+    stableCoinsListing: RankCardType[]
+    foundersListing: RankCardType[]
+  }
+  trending: TrendingData
+}
 
-const Index = () => {
+export const Index = ({
+  promotedWikis,
+  recentWikis,
+  popularTags,
+  leaderboards,
+  rankings,
+  trending,
+}: HomePageProps) => {
   return (
     <Flex
       _dark={{
@@ -49,6 +76,23 @@ const Index = () => {
     >
       <Hero />
       <IQBar />
+      <Box
+        mt={{ base: '-20', md: '-15', xl: '-10' }}
+        px={0}
+        className="container"
+      >
+        <TrendingWikis
+          trending={trending}
+          recent={recentWikis?.slice(0, 5)}
+          featuredWikis={promotedWikis}
+        />
+        <RankingList listingLimit={RANKING_LIST_LIMIT} rankings={rankings} />
+        <AboutIqgpt />
+        <CategoriesList />
+        <EventOverview />
+      </Box>
+      {leaderboards.length > 0 && <LeaderBoard leaderboards={leaderboards} />}
+      <DiscoverMore tagsData={popularTags} />
     </Flex>
   )
 }
@@ -60,159 +104,139 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => {
   const { startDay: weekStartDay, endDay: weekEndDay } = getDateRange({
     rangeType: DayRangeType.LAST_WEEK,
   })
+
   const { startDay: monthStartDay, endDay: monthEndDay } = getDateRange({
     rangeType: DayRangeType.LAST_MONTH,
   })
 
-  console.log('Date Ranges', {
-    todayStartDay,
-    todayEndDay,
-    weekStartDay,
-    weekEndDay,
-    monthStartDay,
-    monthEndDay,
-  })
-
-  // Initialize an array to store potential errors for frontend handling
-  //@ts-ignore
-  const errors = []
-
-  // Helper function to fetch data and handle errors
-  //@ts-ignore
-  const fetchData = async (dispatchFunction, name) => {
-    try {
-      const result = await store.dispatch(dispatchFunction)
-      if (result.error) throw result.error
-      return result.data
-    } catch (error) {
-      //@ts-ignore
-      console.error(`Error fetching ${name}:`, error.message)
-      //@ts-ignore
-      errors.push({ name, message: error.message })
-      return [] // Default empty state for lists, adjust if different data structure is expected
-    }
-  }
-
-  // Perform API calls
-  const promotedWikis = await fetchData(
-    getPromotedWikis.initiate(),
-    'promoted wikis',
+  const { data: promotedWikis, error: promotedWikisError } =
+    await store.dispatch(getPromotedWikis.initiate())
+  const { data: recent, error: recentError } = await store.dispatch(
+    getWikis.initiate(),
   )
-  const recent = await fetchData(getWikis.initiate(), 'recent wikis')
-  const leaderboard = await fetchData(getLeaderboard.initiate(), 'leaderboard')
-  const tagsData = await fetchData(
+  const { data: leaderboard } = await store.dispatch(getLeaderboard.initiate())
+  const { data: tagsData, error: tagsDataError } = await store.dispatch(
     getTags.initiate({
       startDate: Math.floor(Date.now() / 1000) - 60 * 60 * 24 * 30,
       endDate: Math.floor(Date.now() / 1000),
     }),
-    'tags',
   )
-  const NFTsList = await fetchData(
+
+  const { data: NFTsList } = await store.dispatch(
     getNFTRanking.initiate({
       kind: 'NFT',
       limit: RANKING_LIST_LIMIT,
       offset: 1,
     }),
-    'NFT rankings',
   )
-  const TokensList = await fetchData(
+
+  const { data: TokensList } = await store.dispatch(
     getTokenRanking.initiate({
       kind: 'TOKEN',
       limit: RANKING_LIST_LIMIT,
       offset: 1,
     }),
-    'token rankings',
   )
-  const foundersData = await fetchData(
+  const { data: foundersData } = await store.dispatch(
     getFoundersRanking.initiate({
       kind: 'TOKEN',
       limit: RANKING_LIST_LIMIT,
       offset: 1,
       founders: true,
     }),
-    'founders rankings',
   )
-  const aiTokensList = await fetchData(
+  const { data: aiTokensList } = await store.dispatch(
     getAiTokenRanking.initiate({
       kind: 'TOKEN',
       limit: RANKING_LIST_LIMIT,
       offset: 1,
       category: 'AI',
     }),
-    'AI token rankings',
   )
-  const stableCoinsList = await fetchData(
+  const { data: stableCoinsList } = await store.dispatch(
     getStableCoinRanking.initiate({
       kind: 'TOKEN',
       limit: RANKING_LIST_LIMIT,
       offset: 1,
       category: 'STABLE_COINS',
     }),
-    'stable coin rankings',
   )
-  const todayTrending = await fetchData(
+
+  const { data: todayTrending } = await store.dispatch(
     getTrendingWikis.initiate({
       amount: TRENDING_WIKIS_AMOUNT,
       startDay: todayStartDay,
       endDay: todayEndDay,
     }),
-    'today trending wikis',
   )
-  const weekTrending = await fetchData(
+
+  const { data: weekTrending } = await store.dispatch(
     getTrendingWikis.initiate({
       amount: TRENDING_WIKIS_AMOUNT,
       startDay: weekStartDay,
       endDay: weekEndDay,
     }),
-    'week trending wikis',
   )
-  const monthTrending = await fetchData(
+
+  const { data: monthTrending } = await store.dispatch(
     getTrendingWikis.initiate({
       amount: TRENDING_WIKIS_AMOUNT,
       startDay: monthStartDay,
       endDay: monthEndDay,
     }),
-    'month trending wikis',
   )
 
-  // Process data for returning
-  const sortedLeaderboards = leaderboard.length
-    ? sortLeaderboards(leaderboard)
-    : []
-  const rankings = {
-    NFTsListing: NFTsList,
-    aiTokensListing: aiTokensList,
-    TokensListing: TokensList,
-    stableCoinsListing: stableCoinsList,
-    foundersListing: foundersData,
+  await Promise.all([
+    store.dispatch(wikiApi.util.getRunningQueriesThunk()),
+    store.dispatch(tagsApi.util.getRunningQueriesThunk()),
+    store.dispatch(editorApi.util.getRunningQueriesThunk()),
+    store.dispatch(wikiApi.util.getRunningQueriesThunk()),
+    store.dispatch(nftLisitngAPI.util.getRunningQueriesThunk()),
+    store.dispatch(rankingAPI.util.getRunningQueriesThunk()),
+  ])
+
+  if (promotedWikisError || tagsDataError || recentError) {
+    throw new Error(
+      `Error fetching data. the error is: ${[
+        JSON.stringify(tagsDataError?.message),
+        JSON.stringify(promotedWikisError?.message),
+        JSON.stringify(tagsDataError?.message),
+        JSON.stringify(recentError?.message),
+      ].join(' ')}
+      }`,
+    )
   }
-  const trending = {
-    todayTrending,
-    weekTrending,
-    monthTrending,
+  let sortedPromotedWikis: PromotedWikisBuilder[] = []
+  if (promotedWikis?.length) {
+    sortedPromotedWikis = [...promotedWikis]
+    sortedPromotedWikis?.sort((a, b) => a.promoted - b.promoted)
   }
 
-  // Translation setup, include errors if any
-  const translations = await serverSideTranslations(locale ?? 'en', [
-    'common',
-    'home',
-    'category',
-    'rank',
-    'wiki',
-    'event',
-  ])
+  const sortedleaderboards = sortLeaderboards(leaderboard)
+  const rankings = {
+    NFTsListing: NFTsList ?? [],
+    aiTokensListing: aiTokensList ?? [],
+    TokensListing: TokensList ?? [],
+    stableCoinsListing: stableCoinsList ?? [],
+    foundersListing: foundersData ?? [],
+  }
 
   return {
     props: {
-      ...translations,
-      promotedWikis,
-      recent,
-      popularTags: tagsData,
-      leaderboards: sortedLeaderboards,
-      rankings,
-      trending,
-      //@ts-ignore
-      errors: errors.length ? errors : null, // Pass errors to frontend if any
+      ...(await serverSideTranslations(locale ?? 'en', [
+        'common',
+        'home',
+        'category',
+        'rank',
+        'wiki',
+        'event',
+      ])),
+      promotedWikis: sortedPromotedWikis ?? [],
+      recentWikis: recent ?? [],
+      popularTags: tagsData ?? [],
+      leaderboards: sortedleaderboards ?? [],
+      rankings: rankings,
+      trending: { todayTrending, weekTrending, monthTrending },
     },
   }
 }
