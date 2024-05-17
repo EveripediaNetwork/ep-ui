fc89162e9c1d98cd7fa03f57e826909db9353d6f
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-05-17 18:02:48 +0100

    Fix null value bug in blogEntries prop

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -59,7 +59,7 @@ export const getStaticProps: GetStaticProps = async ({ locale }) => {
   return {
     props: {
       ...(await serverSideTranslations(locale ?? 'en', ['blog', 'common'])),
-      blogEntries,
+      blogEntries: blogEntries ?? [],
     },
     revalidate: 60 * 5,
   }
