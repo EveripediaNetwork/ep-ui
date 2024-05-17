86c21d74b089fea928288c73cb62b8dcfacdf7c1
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-05-17 17:36:12 +0100

    Refactor getProvider.ts to use iqChain

--- src/utils/WalletUtils/getProvider.ts
+++ src/utils/WalletUtils/getProvider.ts
@@ -1,8 +1,12 @@
 import config from '@/config'
 import { createPublicClient, http } from 'viem'
 import { mainnet } from 'viem/chains'
+import { iqChain } from '@/config/wagmi'
+
+const chain = config.isProduction ? mainnet : iqChain
+const transport = config.isProduction ? http(config.ensRPC) : http()
 
 export const provider: any = createPublicClient({
-  chain: mainnet,
-  transport: http(config.ensRPC),
+  chain,
+  transport,
 })
