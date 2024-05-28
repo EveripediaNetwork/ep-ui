8620fefcd7c877746a824b56f7eedd1e4fb4b1c4
Kelechi <okekekelechi96@gmail.com>
2024-05-28 19:47:05 +0100

    added a different error status and include console.warn

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -19,6 +19,7 @@ export default async function handler(
     await res.revalidate(path)
     return res.json({ revalidated: true, path })
   } catch (err) {
-    return res.status(500).json({ path: path, error: JSON.stringify(err) })
+    console.warn(JSON.stringify(err))
+    return res.status(400).json({ path: path, error: JSON.stringify(err) })
   }
 }
