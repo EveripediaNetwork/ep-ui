f50797b15c302514563fe83eac4d492eb1b975c6
Srujan Gurram <srujangs8@gmail.com>
2024-06-20 13:35:29 +0530

    Add async rewrites for PostHog API requests and skipTrailingSlashRedirect to support trailing slashes

--- src/components/Layout/CSPostHogProvider.tsx
+++ src/components/Layout/CSPostHogProvider.tsx
@@ -6,12 +6,9 @@ import { useEffect } from 'react'
 
 if (typeof window !== 'undefined') {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-    loaded: (posthog) => {
-      if (process.env.NODE_ENV === 'development') posthog.debug() // debug mode in development
-    },
+    api_host: '/ingest',
+    ui_host: 'https://us.posthog.com',
+    person_profiles: 'identified_only',
   })
 }
 
