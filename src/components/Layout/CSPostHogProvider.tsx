5eb4a7647919af923d0f877e920cf059d1589893
Srujan Gurram <srujangs8@gmail.com>
2024-06-21 15:49:10 +0530

    Remove 'person_profiles' setting from PostHog initialization

--- src/components/Layout/CSPostHogProvider.tsx
+++ src/components/Layout/CSPostHogProvider.tsx
@@ -9,7 +9,6 @@ if (typeof window !== 'undefined') {
   posthog.init(env.NEXT_PUBLIC_POSTHOG_KEY, {
     api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
-    person_profiles: 'identified_only',
   })
 }
 
