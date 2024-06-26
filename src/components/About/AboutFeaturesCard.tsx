e0f464f6dd2685ee984424adfa4d8e2e01cdeff9
Damola18 <olutokeezekiel18@gmail.com>
2024-06-26 17:34:25 +0100

    fixes

--- src/components/About/AboutFeaturesCard.tsx
+++ src/components/About/AboutFeaturesCard.tsx
@@ -20,18 +20,23 @@ const AboutFeaturesCard = ({
   >
     <MagicCard className="flex w-full cursor-pointer flex-col text-left overflow-hidden bg-white">
       <Icon as={icon} mb={4} w="50px" h="50px" />
-      <Heading fontWeight={700} fontSize={{ base: '18px', lg: '20px' }} mb={4} color='gray.800' _dark={{color: 'white'}}>
+      <Heading
+        fontWeight={700}
+        fontSize={{ base: '18px', lg: '20px' }}
+        mb={4}
+        color="gray.800"
+        _dark={{ color: 'white' }}
+      >
         {title}
       </Heading>
       <Text
         fontWeight={500}
         fontSize={{ base: '14px', md: '16px', lg: '18px' }}
-        color='gray.600'
-        _dark={{color: 'white'}}
+        color="gray.600"
+        _dark={{ color: 'white' }}
       >
         {content}
       </Text>
-    
     </MagicCard>
   </MagicContainer>
 )
