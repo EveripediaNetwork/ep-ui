7e69690a17642336d03eeb7006b36166a0af2fe3
Damola18 <olutokeezekiel18@gmail.com>
2024-06-27 13:40:10 +0100

    add border beam component

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,11 +81,17 @@ module.exports = {
               'rotate(360deg) translateY(calc(var(--radius) * 1px)) rotate(-360deg)',
           },
         },
+        'border-beam': {
+          '100%': {
+            'offset-distance': '100%',
+          },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         orbit: 'orbit calc(var(--duration)*1s) linear infinite',
+        "border-beam": "border-beam calc(var(--duration)*1s) infinite linear",
       },
       backgroundImage: {
         'custom-gradient':
