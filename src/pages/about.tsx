f88a0705a2f8d5d44349640b448948bcce2be93b
Damola18 <olutokeezekiel18@gmail.com>
2024-06-28 17:18:18 +0100

    update page wrapper

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Box } from '@chakra-ui/react'
 import AboutHero from '@/components/About/AboutHero'
 import AboutFeatures from '@/components/About/AboutFeatures'
 import AboutAsSeenIn from '@/components/About/AboutAsSeenIn'
@@ -13,16 +12,12 @@ import { GetServerSideProps } from 'next'
 const About = () => (
   <>
     <AboutHeader />
-    <Box px={{ base: 6, lg: 16 }} py={{ base: '10', lg: 15 }}>
-      <AboutHero />
-      <AboutFeatures />
-      <AboutAsSeenIn />
-      <AboutAiIntegration />
-    </Box>
+    <AboutHero />
+    <AboutFeatures />
+    <AboutAsSeenIn />
+    <AboutAiIntegration />
     <AboutOurTeam />
-    <Box px={{ base: 6, lg: 8 }} py={{ base: '10', lg: 15 }}>
-      <OurHistory />
-    </Box>
+    <OurHistory />
   </>
 )
 
