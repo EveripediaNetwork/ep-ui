70903ea891ab95fa65daf385c220ff2b304f062a
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-07-09 11:43:22 +0100

    Refactor useAddress hook to use wagmi

--- src/hooks/useAddress.ts
+++ src/hooks/useAddress.ts
@@ -1,7 +1,31 @@
 import { useAccount } from 'wagmi'
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { setToken } from '@/store/slices/user-slice'
+import { cookieNames } from '@/types/cookies'
+import { verify } from '@everipedia/web3-signer'
+import { getCookie } from 'cookies-next'
 
 export const useAddress = () => {
+  const dispatch = useDispatch()
   const { address, isConnected } = useAccount()
 
-  return { address, isConnected }
+  useEffect(() => {
+    const encodedToken = getCookie(cookieNames.Enum['x-auth-token']) as string
+
+    if (encodedToken) {
+      const decodedToken = decodeURI(encodedToken)
+
+      try {
+        verify(decodedToken)
+        dispatch(setToken(decodedToken))
+      } catch (error) {
+        console.error('Error verifying token:', error)
+      }
+    }
+  }, [dispatch])
+
+  const simpleAddress = address ? address.toString() : null
+
+  return { address: simpleAddress, isConnected }
 }
