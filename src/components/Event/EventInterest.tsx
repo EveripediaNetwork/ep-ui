c5628a3f13a885f1e5812624d71056519a6bfb07
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-07-30 23:09:18 +0100

    done with event interest translations

--- src/components/Event/EventInterest.tsx
+++ src/components/Event/EventInterest.tsx
@@ -1,6 +1,7 @@
 import { EventInterestData } from '@/components/Event/event.data'
 import { TEvents, getEventsByTags } from '@/services/event'
 import { store } from '@/store/store'
+import { useTranslation } from 'next-i18next'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 
@@ -14,14 +15,15 @@ const EventInterest = ({
   setIsLoading: Function
 }) => {
   const router = useRouter()
+  const { t } = useTranslation('event')
   const { query, pathname } = router
   const [selectedTags, setSelectedTags] = useState<string[]>([])
 
   const toggleTag = (tag: string) => {
-    setSelectedTags((prevSelected) => {
+    setSelectedTags(prevSelected => {
       let updatedTags: string[] = [...prevSelected]
       if (prevSelected.includes(tag)) {
-        updatedTags = prevSelected.filter((t) => t !== tag)
+        updatedTags = prevSelected.filter(t => t !== tag)
       } else {
         updatedTags = [...prevSelected, tag].slice(0, 4)
       }
@@ -31,8 +33,8 @@ const EventInterest = ({
         currentQueryParams.tags = updatedTags
         setIsLoading(true)
         filterEventsByTags(updatedTags)
-          .then((res) => setEventData(res))
-          .catch((err) => console.log(err))
+          .then(res => setEventData(res))
+          .catch(err => console.log(err))
           .finally(() => setIsLoading(false))
       } else {
         delete currentQueryParams.tags
@@ -72,14 +74,12 @@ const EventInterest = ({
     <div className="flex flex-col xl:flex-row justify-between max-w-[1296px] mx-auto mt-6 md:mt-[30px] rounded-xl border dark:border-alpha-300 border-gray200 bg-white dark:bg-gray700 py-3 md:py-5 px-4 xl:px-[32px] xl:gap-32">
       <div className="xl:max-w-[200px] flex flex-col gap-2 xl:gap-3 w-full">
         <h3 className="text-xl font-semibold text-gray800 dark:text-alpha-900">
-          Interests
+          {t('eventsInterestHeading')}
         </h3>
-        <span className="text-sm">
-          Get event suggestion based on your interests.
-        </span>
+        <span className="text-sm">{t('eventsInterestDescription')}</span>
       </div>
       <div className="flex flex-wrap gap-3 mt-3 xl:mt-0">
-        {EventInterestData.map((interest) => {
+        {EventInterestData.map(interest => {
           return (
             <button
               type="button"
@@ -91,7 +91,7 @@ const EventInterest = ({
               }`}
               key={interest}
             >
-              {interest}
+              {t(`interests.${interest}`)}
             </button>
           )
         })}
