7981077d8affe8eb0408c75a3dc809384d0f0cfb
Kelechi <okekekelechi96@gmail.com>
2024-08-01 14:58:22 +0100

    adjusted padding top for hero section

--- src/components/Landing/DiscoverMore.tsx
+++ src/components/Landing/DiscoverMore.tsx
@@ -10,12 +10,12 @@ const DiscoverMore = ({ tagsData }: DiscoverMoreProps) => {
 
   return (
     <div className="bg-gray-100 dark:bg-alpha-50">
-      <div className="container mx-auto py-10 lg:py-20 relative px-4 lg:px-8 2xl:px-0 ">
+      <div className="container mx-auto py-10 lg:py-20 relative px-4 lg:px-8 2xl:px-0">
         <h1 className="font-bold text-2xl">{t('DiscoverMoreHeading')}</h1>
         <div className="flex flex-wrap mt-8 gap-4">
           {tagsData?.map((tag) => (
             <Link
-              className="rounded-full text-xs lg:text-2xl border-2 border-gray-300 px-4 py-1 text-gray-600 dark:border-gray-700 dark:text-alpha-900"
+              className="rounded-full text-sm lg:text-2xl border-2 border-gray-300 px-4 py-1 text-gray-600 dark:border-gray-700 dark:text-alpha-900"
               key={tag.id}
               href={`/tags/${tag.id}`}
             >
